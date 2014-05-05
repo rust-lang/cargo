ff85524210e7863a8fafa10ebe8a307d53b7a8a1
Yehuda Katz <wycats@gmail.com>
2014-05-05 11:33:04 -0700

    Start breaking up cargo-compile

--- src/bin/cargo-compile.rs
+++ src/bin/cargo-compile.rs
@@ -2,11 +2,32 @@
 #![allow(deprecated_owned_vector)]
 
 extern crate cargo;
+extern crate hammer;
+extern crate serialize;
 
 use cargo::ops::cargo_compile::compile;
+use cargo::{CargoResult,ToCargoError};
+use hammer::{FlagDecoder,FlagConfig,FlagConfiguration,HammerError};
+use serialize::Decodable;
+
+#[deriving(Eq,Clone,Decodable,Encodable)]
+pub struct Options {
+    manifest_path: ~str
+}
+
+impl FlagConfig for Options {}
+
+fn flags<T: FlagConfig + Decodable<FlagDecoder, HammerError>>() -> CargoResult<T> {
+    let mut decoder = FlagDecoder::new::<T>(std::os::args().tail());
+    Decodable::decode(&mut decoder).to_cargo_error(|e: HammerError| e.message, 1)
+}
+
+fn execute() -> CargoResult<()> {
+    compile(try!(flags::<Options>()).manifest_path.as_slice())
+}
 
 fn main() {
-    match compile() {
+    match execute() {
         Err(io_error) => fail!("{}", io_error),
         Ok(_) => return
     }
