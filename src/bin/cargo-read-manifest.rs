d28ce723510ad979a20ef313d877f57b0160985c
Yehuda Katz <wycats@gmail.com>
2014-05-07 16:35:55 -0700

    Get cargo-read-manifest working again

--- src/bin/cargo-read-manifest.rs
+++ src/bin/cargo-read-manifest.rs
@@ -5,7 +5,22 @@ extern crate cargo;
 extern crate serialize;
 extern crate hammer;
 
+use hammer::FlagConfig;
+use cargo::{execute_main_without_stdin,CLIResult};
+use cargo::core::Package;
+use cargo::ops::cargo_read_manifest::read_manifest;
+
+#[deriving(Eq,Clone,Decodable)]
+struct Options {
+    manifest_path: ~str
+}
+
+impl FlagConfig for Options {}
+
 fn main() {
-    // Standalone cargo-read-manifest will go here
-    unimplemented!();
+    execute_main_without_stdin(execute);
+}
+
+fn execute(options: Options) -> CLIResult<Option<Package>> {
+    read_manifest(options.manifest_path.as_slice()).map(|m| Some(m))
 }
--- src/cargo/ops/cargo_read_manifest.rs
+++ src/cargo/ops/cargo_read_manifest.rs
@@ -1,14 +1,14 @@
 use toml;
 use toml::from_toml;
 use core;
-use core::manifest::{SerializedManifest};
+use core::manifest::{TomlManifest};
 use core::errors::{CLIError,CLIResult,ToResult};
 
 pub fn read_manifest(manifest_path: &str) -> CLIResult<core::Package> {
     let root = try!(toml::parse_from_file(manifest_path.clone()).to_result(|err|
         CLIError::new(format!("Cargo.toml was not valid Toml: {}", manifest_path), Some(err.to_str()), 1)));
 
-    let toml_manifest = try!(from_toml::<SerializedManifest>(root.clone()).to_result(|err: toml::Error|
+    let toml_manifest = try!(from_toml::<TomlManifest>(root.clone()).to_result(|err: toml::Error|
         CLIError::new(format!("Cargo.toml was not in the right format: {}", manifest_path), Some(err.to_str()), 1)));
 
     toml_manifest.to_package(manifest_path.as_slice()).to_result(|err|
