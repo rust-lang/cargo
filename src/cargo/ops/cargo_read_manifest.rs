7f5a2cebb52f6e1c3a79f968b6b0b281764b76c8
Yehuda Katz <wycats@gmail.com>
2014-05-09 16:57:13 -0700

    More tests for error cases in compile
    Also some refactoring of the error structure. More cleanup work around
    human-readable and CLI errors is still required.

--- src/cargo/ops/cargo_read_manifest.rs
+++ src/cargo/ops/cargo_read_manifest.rs
@@ -2,23 +2,26 @@ use toml;
 use toml::from_toml;
 use core::Package;
 use core::manifest::{TomlManifest};
-use util::{other_error,CargoResult,CargoError};
+use util::{toml_error,human_error,CargoResult,CargoError};
 
 pub fn read_manifest(path: &str) -> CargoResult<Package> {
-    let root = try!(parse_from_file(path));
-    let toml = try!(load_toml(path, root));
-    toml.to_package(path)
+    let root = try!(parse_from_file(path).map_err(|err: CargoError|
+        human_error(format!("Cargo.toml is not valid Toml"), format!("path={}", path), err)));
+
+    let toml = try!(load_toml(root).map_err(|err: CargoError|
+        human_error(format!("Cargo.toml is not a valid Cargo manifest"), format!("path={}", path), err)));
+
+    Ok(toml.to_package(path))
 }
 
 fn parse_from_file(path: &str) -> CargoResult<toml::Value> {
-    toml::parse_from_file(path.clone()).map_err(|err| to_cargo_err(path, err))
+    toml::parse_from_file(path.clone()).map_err(to_cargo_err)
 }
 
-fn load_toml(path: &str, root: toml::Value) -> CargoResult<TomlManifest> {
-    from_toml::<TomlManifest>(root).map_err(|err| to_cargo_err(path, err))
+fn load_toml(root: toml::Value) -> CargoResult<TomlManifest> {
+    from_toml::<TomlManifest>(root).map_err(to_cargo_err)
 }
 
-fn to_cargo_err(path: &str, err: toml::Error) -> CargoError {
-    other_error("Cargo.toml is not valid Toml")
-        .with_detail(format!("path={}; err={}", path, err.to_str()))
+fn to_cargo_err(err: toml::Error) -> CargoError {
+    toml_error("Problem loading manifest", err)
 }
--- src/cargo/util/mod.rs
+++ src/cargo/util/mod.rs
@@ -1,5 +1,5 @@
 pub use self::process_builder::{process,ProcessBuilder};
-pub use self::result::{CargoError,CargoResult,Wrap,Require,other_error};
+pub use self::result::{CargoError,CargoResult,Wrap,Require,ToCLI,other_error,human_error,toml_error};
 
 pub mod graph;
 pub mod process_builder;
