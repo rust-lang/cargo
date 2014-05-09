deb2b70a26bdca0334a783f729e15773ae0b354b
Carl Lerche <me@carllerche.com>
2014-05-09 10:29:49 -0700

    Switch important_paths to using result::CargoResult

--- src/cargo/util/important_paths.rs
+++ src/cargo/util/important_paths.rs
@@ -1,4 +1,4 @@
-use core::errors::{CargoResult,CargoError,MissingManifest};
+use util::{other_error,CargoResult,CargoError};
 
 pub fn find_project(pwd: Path, file: ~str) -> CargoResult<Path> {
     let mut current = pwd.clone();
@@ -11,5 +11,10 @@ pub fn find_project(pwd: Path, file: ~str) -> CargoResult<Path> {
         if !current.pop() { break; }
     }
 
-    Err(CargoError::internal(MissingManifest(pwd, file)))
+    Err(manifest_missing_err(&pwd, file.as_slice()))
+}
+
+fn manifest_missing_err(pwd: &Path, file: &str) -> CargoError {
+    other_error("manifest not found")
+        .with_detail(format!("pwd={}; file={}", pwd.display(), file))
 }
