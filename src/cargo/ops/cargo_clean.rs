3f110840b3f5274a54646f350328e1b7f5990f34
Alex Crichton <alex@alexcrichton.com>
2014-07-22 08:06:36 -0700

    Add a cargo-doc command

--- src/cargo/ops/cargo_clean.rs
+++ src/cargo/ops/cargo_clean.rs
@@ -1,25 +1,30 @@
 use std::io::fs::{rmdir_recursive};
-use core::{SourceId};
+
+use core::source::Source;
+use sources::PathSource;
 use util::{CargoResult, human, ChainError};
-use ops::{read_manifest};
-use std::io::{File};
-use util::toml::{project_layout};
 
 /// Cleans the project from build artifacts.
 
 pub fn clean(manifest_path: &Path) -> CargoResult<()> {
-    let mut file = try!(File::open(manifest_path));
-    let data = try!(file.read_to_end());
-    let layout = project_layout(&manifest_path.dir_path());
-    let (manifest, _) = try!(read_manifest(data.as_slice(), 
-                                           layout, 
-                                           &SourceId::for_path(manifest_path)));
+    let mut src = PathSource::for_path(&manifest_path.dir_path());
+    try!(src.update());
+    let root = try!(src.get_root_package());
+    let manifest = root.get_manifest();
 
     let build_dir = manifest.get_target_dir();
-
     if build_dir.exists() {
-      rmdir_recursive(build_dir).chain_error(|| human("Could not remove build directory"))
-    } else {
-      Ok(())
+        try!(rmdir_recursive(build_dir).chain_error(|| {
+            human("Could not remove build directory")
+        }))
     }
+
+    let doc_dir = manifest.get_doc_dir();
+    if doc_dir.exists() {
+        try!(rmdir_recursive(doc_dir).chain_error(|| {
+            human("Could not remove documentation directory")
+        }))
+    }
+
+    Ok(())
 }
