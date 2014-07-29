e97f46562d026920d55d38d171dc574a9668c439
Alex Crichton <alex@alexcrichton.com>
2014-07-28 20:09:07 -0700

    Don't blow away target/doc
    The existing rustdoc behavior for working with an already-present directory is
    good enough, so there's no need for cargo to help out by blowing things away.
    This also makes the copy-back portion easier by just not having it.
    
    Closes #284

--- src/cargo/ops/cargo_doc.rs
+++ src/cargo/ops/cargo_doc.rs
@@ -1,9 +1,5 @@
-use std::io::fs;
-
 use ops;
 use util::CargoResult;
-use core::source::Source;
-use sources::PathSource;
 
 pub struct DocOptions<'a> {
     pub all: bool,
@@ -12,11 +8,6 @@ pub struct DocOptions<'a> {
 
 pub fn doc(manifest_path: &Path,
            options: &mut DocOptions) -> CargoResult<()> {
-    let mut src = PathSource::for_path(&manifest_path.dir_path());
-    try!(src.update());
-    let root = try!(src.get_root_package());
-    let output = root.get_manifest().get_target_dir().join("doc");
-    let _ = fs::rmdir_recursive(&output);
     try!(ops::compile(manifest_path, &mut options.compile_opts));
     Ok(())
 }
