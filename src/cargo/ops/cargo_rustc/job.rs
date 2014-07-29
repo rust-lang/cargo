79768eb0d9496ba2156a839c3a1c0758e51fd5a8
Alex Crichton <alex@alexcrichton.com>
2014-07-29 10:20:42 -0700

    Prepare for per-target fingerprints
    This commit refactors all related infrastructure for the cargo_rustc module to
    move the granularity of fingerprints from packages to targets. This involved
    heavily modifying the `JobQueue` structure to understand a finer-grained target
    for dirtiness propagation and dependency management, and then dealing with
    fallout in the main module.
    
    The fingerprint module has been refactored to support a per-target fingerprint.
    A fallout of this change is that each build step has its own fingerprint,
    including the custom build command step. This will be implemented in a future
    commit.
    
    As fallout of this reorganization, we are now exploiting the maximal parallelism
    within packages themselves whereas before we were only partially parallelizing.
    No new features were added as part of this commit, so I just ensured that all
    the tests ran.

--- src/cargo/ops/cargo_rustc/job.rs
+++ src/cargo/ops/cargo_rustc/job.rs
@@ -1,57 +1,22 @@
-use util::CargoResult;
-use std::sync::{Arc, Mutex};
+use util::{CargoResult, Fresh, Dirty, Freshness};
 
-pub struct Job {
-    work: proc():Send -> CargoResult<Vec<Job>>,
-}
+pub struct Job { dirty: Work, fresh: Work }
+
+pub type Work = proc():Send -> CargoResult<()>;
 
 impl Job {
     /// Create a new job representing a unit of work.
-    pub fn new(work: proc():Send -> CargoResult<Vec<Job>>) -> Job {
-        Job { work: work }
-    }
-
-    /// Creates a new job which will execute all of `jobs` and then return the
-    /// work `after` if they all succeed sequentially.
-    pub fn all(jobs: Vec<Job>, after: Vec<Job>) -> Job {
-        Job::new(proc() {
-            for job in jobs.move_iter() {
-                try!(job.run());
-            }
-            Ok(after)
-        })
-    }
-
-    /// Maps a list of jobs to a new list of jobs which will run `after` once
-    /// all the jobs have completed.
-    pub fn after(jobs: Vec<Job>, after: Job) -> Vec<Job> {
-        if jobs.len() == 0 { return vec![after] }
-
-        struct State { job: Option<Job>, remaining: uint }
-
-        let lock = Arc::new(Mutex::new(State {
-            job: Some(after),
-            remaining: jobs.len(),
-        }));
-
-        jobs.move_iter().map(|job| {
-            let my_lock = lock.clone();
-            Job::new(proc() {
-                try!(job.run());
-                let mut state = my_lock.lock();
-                state.remaining -= 1;
-                Ok(if state.remaining == 0 {
-                    vec![state.job.take().unwrap()]
-                } else {
-                    Vec::new()
-                })
-            })
-        }).collect()
+    pub fn new(dirty: proc():Send -> CargoResult<()>,
+               fresh: proc():Send -> CargoResult<()>) -> Job {
+        Job { dirty: dirty, fresh: fresh }
     }
 
     /// Consumes this job by running it, returning the result of the
     /// computation.
-    pub fn run(self) -> CargoResult<Vec<Job>> {
-        (self.work)()
+    pub fn run(self, fresh: Freshness) -> CargoResult<()> {
+        match fresh {
+            Fresh => (self.fresh)(),
+            Dirty => (self.dirty)(),
+        }
     }
 }
