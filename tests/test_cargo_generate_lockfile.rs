2f2769b4e239964e7f88c2cd8746ac3b1925df9c
Alex Crichton <alex@alexcrichton.com>
2014-08-04 10:50:32 -0700

    Update the lockfile when deps are modified
    Closes #314

--- tests/test_cargo_generate_lockfile.rs
+++ tests/test_cargo_generate_lockfile.rs
@@ -33,3 +33,64 @@ test!(ignores_carriage_return {
                 execs().with_status(0));
     assert_eq!(lockfile.stat().assert().modified, mtime);
 })
+
+test!(adding_and_removing_packages {
+    let p = project("foo")
+        .file("Cargo.toml", r#"
+            [package]
+            name = "foo"
+            authors = []
+            version = "0.0.1"
+        "#)
+        .file("src/main.rs", "fn main() {}");
+
+    assert_that(p.cargo_process("cargo-generate-lockfile"),
+                execs().with_status(0));
+
+    let lockfile = p.root().join("Cargo.lock");
+    let toml = p.root().join("Cargo.toml");
+    let lock1 = File::open(&lockfile).read_to_string().assert();
+
+    // add a dep
+    File::create(&toml).write_str(r#"
+        [package]
+        name = "foo"
+        authors = []
+        version = "0.0.1"
+
+        [dependencies]
+        bar = "0.5.0"
+    "#).assert();
+    assert_that(p.process(cargo_dir().join("cargo-generate-lockfile")),
+                execs().with_status(0));
+    let lock2 = File::open(&lockfile).read_to_string().assert();
+    assert!(lock1 != lock2);
+
+    // change the dep
+    File::create(&toml).write_str(r#"
+        [package]
+        name = "foo"
+        authors = []
+        version = "0.0.1"
+
+        [dependencies]
+        bar = "0.2.0"
+    "#).assert();
+    assert_that(p.process(cargo_dir().join("cargo-generate-lockfile")),
+                execs().with_status(0));
+    let lock3 = File::open(&lockfile).read_to_string().assert();
+    assert!(lock1 != lock3);
+    assert!(lock2 != lock3);
+
+    // remove the dep
+    File::create(&toml).write_str(r#"
+        [package]
+        name = "foo"
+        authors = []
+        version = "0.0.1"
+    "#).assert();
+    assert_that(p.process(cargo_dir().join("cargo-generate-lockfile")),
+                execs().with_status(0));
+    let lock4 = File::open(&lockfile).read_to_string().assert();
+    assert_eq!(lock1, lock4);
+})
