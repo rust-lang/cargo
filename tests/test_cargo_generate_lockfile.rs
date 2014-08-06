730ce191b367882e1c6547393ce246dc147e5ca6
Alex Crichton <alex@alexcrichton.com>
2014-08-06 16:10:40 -0700

    Unconditionally write Cargo.lock
    Otherwise `cargo-update` ends up not doing much because all sources are
    considered equal regardless of their precision.

--- tests/test_cargo_generate_lockfile.rs
+++ tests/test_cargo_generate_lockfile.rs
@@ -27,11 +27,8 @@ test!(ignores_carriage_return {
     let lock = lock.assert();
     let lock = lock.as_slice().replace("\n", "\r\n");
     File::create(&lockfile).write_str(lock.as_slice()).assert();
-    lockfile.move_into_the_past().assert();
-    let mtime = lockfile.stat().assert().modified;
     assert_that(p.process(cargo_dir().join("cargo-build")),
                 execs().with_status(0));
-    assert_eq!(lockfile.stat().assert().modified, mtime);
 })
 
 test!(adding_and_removing_packages {
