d9cafd853cf497e4bc161790108adb665418184f
Alex Crichton <alex@alexcrichton.com>
2014-08-05 20:54:32 -0700

    Lift the requirement that cargo is built with make
    Cargo should be able to build with cargo so others can depend on the cargo
    library. This means that the one requirement of the CFG_VERSION env var will not
    be available. This alters `version()` to prioritize CFG_VERSION but fall back to
    the cargo-specified variables.

--- src/cargo/lib.rs
+++ src/cargo/lib.rs
@@ -200,7 +200,14 @@ fn handle_cause(err: &CargoError, shell: &mut MultiShell) {
 }
 
 pub fn version() -> String {
-    format!("cargo {}", env!("CFG_VERSION"))
+    format!("cargo {}", match option_env!("CFG_VERSION") {
+        Some(s) => s.to_string(),
+        None => format!("{}.{}.{}{}",
+                        env!("CARGO_PKG_VERSION_MAJOR"),
+                        env!("CARGO_PKG_VERSION_MINOR"),
+                        env!("CARGO_PKG_VERSION_PATCH"),
+                        option_env!("CARGO_PKG_VERSION_PRE").unwrap_or(""))
+    })
 }
 
 fn flags_from_args<T: FlagParser>(args: &[String],
