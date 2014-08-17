rust-lang/cargo

commit 0c834d7b2d45d8fa4bb29bec682df2d6379ff39a

Ensure TARGET is always present for build commands

src/cargo/ops/cargo_rustc/context.rs
@@ -4,7 +4,7 @@ use semver::Version;
 
 use core::{SourceMap, Package, PackageId, PackageSet, Resolve, Target};
 use util;
-use util::{CargoResult, ChainError, internal, Config, profile};
+use util::{CargoResult, ChainError, internal, Config, profile, Require};
 
 use super::{Kind, KindPlugin, KindTarget, Compilation};
 use super::layout::{Layout, LayoutProxy};
@@ -27,6 +27,7 @@ pub struct Context<'a, 'b> {
     env: &'a str,
     host: Layout,
     target: Option<Layout>,
+    target_triple: String,
     host_dylib: (String, String),
     package_set: &'a PackageSet,
     target_dylib: (String, String),
@@ -47,8 +48,10 @@ impl<'a, 'b> Context<'a, 'b> {
             let (dylib, _) = try!(Context::filename_parts(None));
             dylib
         };
+        let (rustc_version, target_triple) = try!(Context::rustc_version());
         Ok(Context {
-            rustc_version: try!(Context::rustc_version()),
+            rustc_version: rustc_version,
+            target_triple: target_triple,
             env: env,
             host: host,
             target: target,
@@ -65,11 +68,26 @@ impl<'a, 'b> Context<'a, 'b> {
         })
     }
 
-    /// Run `rustc` to figure out what its current version string is
-    fn rustc_version() -> CargoResult<String> {
+    /// Run `rustc` to figure out what its current version string is.
+    ///
+    /// The second element of the tuple returned is the target triple that rustc
+    /// is a host for.
+    fn rustc_version() -> CargoResult<(String, String)> {
         let output = try!(util::process("rustc").arg("-v").arg("verbose")
                                .exec_with_output());
-        Ok(String::from_utf8(output.output).unwrap())
+        let output = try!(String::from_utf8(output.output).map_err(|_| {
+            internal("rustc -v didn't return utf8 output")
+        }));
+        let triple = {
+            let triple = output.as_slice().lines().filter(|l| {
+                l.starts_with("host: ")
+            }).map(|l| l.slice_from(6)).next();
+            let triple = try!(triple.require(|| {
+                internal("rustc -v didn't have a line for `host:`")
+            }));
+            triple.to_string()
+        };
+        Ok((output, triple))
     }
 
     /// Run `rustc` to discover the dylib prefix/suffix for the target
@@ -203,6 +221,11 @@ impl<'a, 'b> Context<'a, 'b> {
     fn dylib(&self, kind: Kind) -> (&str, &str) {
         let pair = if kind == KindPlugin {&self.host_dylib} else {&self.target_dylib};
         (pair.ref0().as_slice(), pair.ref1().as_slice())
+    }
+
+    /// Return the target triple which this context is targeting.
+    pub fn target_triple(&self) -> &str {
+        self.target_triple.as_slice()
     }
 
     /// Return the exact filename of the target.

src/cargo/ops/cargo_rustc/mod.rs
@@ -167,7 +167,7 @@ fn compile_custom(pkg: &Package, cmd: &str,
     let mut p = process(cmd.next().unwrap(), pkg, cx)
                      .env("OUT_DIR", Some(&output))
                      .env("DEPS_DIR", Some(&output))
-                     .env("TARGET", cx.config.target());
+                     .env("TARGET", Some(cx.target_triple()));
     for arg in cmd {
         p = p.arg(arg);
     }

tests/test_cargo_bench.rs
@@ -482,7 +482,7 @@ test!(lib_bin_same_name {
             version = "0.0.1"
             authors = []
 
-            [[lib]]
+            [lib]
             name = "foo"
             [[bin]]
             name = "foo"
@@ -591,7 +591,7 @@ test!(lib_with_standard_name2 {
             version = "0.0.1"
             authors = []
 
-            [[lib]]
+            [lib]
             name = "syntax"
             bench = false
             doctest = false
@@ -666,7 +666,7 @@ test!(bench_dylib {
             version = "0.0.1"
             authors = []
 
-            [[lib]]
+            [lib]
             name = "foo"
             crate_type = ["dylib"]
 
@@ -695,7 +695,7 @@ test!(bench_dylib {
             version = "0.0.1"
             authors = []
 
-            [[lib]]
+            [lib]
             name = "bar"
             crate_type = ["dylib"]
         "#)

commit 51de050e6c67ca9bcfe7e85822e8a3a936378497

If --target is specified, pass that to build commands
0c834d7b accidentally broke this by favoring rustc's target triple over the
actual target triple.

src/cargo/ops/cargo_rustc/context.rs
@@ -48,7 +48,9 @@ impl<'a, 'b> Context<'a, 'b> {
             let (dylib, _) = try!(Context::filename_parts(None));
             dylib
         };
-        let (rustc_version, target_triple) = try!(Context::rustc_version());
+        let (rustc_version, rustc_host) = try!(Context::rustc_version());
+        let target_triple = config.target().map(|s| s.to_string());
+        let target_triple = target_triple.unwrap_or(rustc_host);
         Ok(Context {
             rustc_version: rustc_version,
             target_triple: target_triple,

tests/test_cargo_compile_git_deps.rs
@@ -919,7 +919,7 @@ test!(dep_with_changed_submodule {
 
     let mut file = File::create(&git_project.root().join(".gitmodules"));
     file.write_str(format!("[submodule \"src\"]\n\tpath = src\n\turl={}",
-                           git_project3.url()).as_slice());
+                           git_project3.url()).as_slice()).assert();
 
     git_project.process("git").args(["submodule", "sync"]).exec_with_output().assert();
     git_project.process("git").args(["fetch"]).cwd(git_project.root().join("src"))

tests/test_cargo_cross_compile.rs
@@ -32,9 +32,31 @@ fn alternate() -> &'static str {
 test!(simple_cross {
     if disabled() { return }
 
+    let mut build = project("builder");
+    build = build
+        .file("Cargo.toml", r#"
+            [project]
+            name = "foo"
+            version = "0.5.0"
+            authors = ["wycats@example.com"]
+        "#)
+        .file("src/main.rs", format!(r#"
+            fn main() {{
+                assert_eq!(std::os::getenv("TARGET").unwrap().as_slice(), "{}");
+            }}
+        "#, alternate()).as_slice());
+    assert_that(build.cargo_process("cargo-build"),
+                execs().with_status(0));
+
     let p = project("foo")
-        .file("Cargo.toml", basic_bin_manifest("foo").as_slice())
-        .file("src/foo.rs", r#"
+        .file("Cargo.toml", format!(r#"
+            [package]
+            name = "foo"
+            version = "0.0.0"
+            authors = []
+            build = '{}'
+        "#, build.bin("foo").display()))
+        .file("src/main.rs", r#"
             use std::os;
             fn main() {
                 assert_eq!(os::consts::ARCH, "x86");