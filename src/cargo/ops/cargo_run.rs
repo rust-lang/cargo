0e91bedf8bd35e1788d96fc0debc741f4887dbf1
Alex Crichton <alex@alexcrichton.com>
2014-08-22 09:53:56 -0700

    Fix `cargo run` for renamed bin targets

--- src/cargo/ops/cargo_run.rs
+++ src/cargo/ops/cargo_run.rs
@@ -1,23 +1,30 @@
 use std::os;
 
 use ops;
-use util::{CargoResult, human, process, ProcessError};
+use util::{CargoResult, human, process, ProcessError, Require};
 use core::source::Source;
 use sources::PathSource;
 
 pub fn run(manifest_path: &Path,
            options: &mut ops::CompileOptions,
            args: &[String]) -> CargoResult<Option<ProcessError>> {
-    if !manifest_path.dir_path().join("src").join("main.rs").exists() {
-        return Err(human("`src/main.rs` must be present for `cargo run`"))
-    }
-
     let mut src = try!(PathSource::for_path(&manifest_path.dir_path()));
     try!(src.update());
     let root = try!(src.get_root_package());
+    let mut bins = root.get_manifest().get_targets().iter().filter(|a| {
+        a.is_bin() && a.get_profile().is_compile()
+    });
+    let bin = try!(bins.next().require(|| {
+        human("a bin target must be available for `cargo run`")
+    }));
+    match bins.next() {
+        Some(..) => return Err(human("`cargo run` requires that a project only \
+                                      have one executable")),
+        None => {}
+    }
 
     let compile = try!(ops::compile(manifest_path, options));
-    let exe = manifest_path.dir_path().join("target").join(root.get_name());
+    let exe = manifest_path.dir_path().join("target").join(bin.get_name());
     let exe = match exe.path_relative_from(&os::getcwd()) {
         Some(path) => path,
         None => exe,
