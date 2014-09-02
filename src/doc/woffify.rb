70446f9e45c37a1b18126769f135c173a2b54e55
Alex Crichton <alex@alexcrichton.com>
2014-09-02 10:30:20 -0700

    Don't use bundled fonts

--- src/doc/woffify.rb
+++
@@ -1,9 +0,0 @@
-require "fileutils"
-
-FileUtils.rm Dir["source/fonts/**/*.woff"]
-
-Dir["source/fonts/**/*.ttf"].each do |file|
-  out = file.sub(/\.ttf$/, ".woff").gsub(' ', "-")
-  puts "Converting `#{file}` to `#{out}`"
-  `~/npm/bin/ttf2woff "#{file}" "#{out}"`
-end
