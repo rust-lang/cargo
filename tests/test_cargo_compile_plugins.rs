0c8f6dba56dbf9464fea7e5d3e0988e61275b330
Alex Crichton <alex@alexcrichton.com>
2014-09-21 15:15:00 -0700

    Allow plugins to have native dylib dependencies
    Ensure that the dynamic linker search path contains the location of the output
    directories for these dependencies when compiling with plugins.
    
    Closes #597

--- tests/test_cargo_compile_plugins.rs
+++ tests/test_cargo_compile_plugins.rs
@@ -1,3 +1,6 @@
+use std::io::fs;
+use std::os;
+
 use support::{project, execs, cargo_dir};
 use hamcrest::assert_that;
 
@@ -76,5 +79,91 @@ test!(plugin_to_the_max {
     assert_that(foo.cargo_process("build"),
                 execs().with_status(0));
     assert_that(foo.process(cargo_dir().join("cargo")).arg("doc"),
+                execs().with_status(0));
+})
+
+test!(plugin_with_dynamic_native_dependency {
+    let build = project("build")
+        .file("Cargo.toml", r#"
+            [package]
+            name = "build"
+            version = "0.0.1"
+            authors = []
+
+            [lib]
+            name = "build"
+            crate-type = ["dylib"]
+        "#)
+        .file("src/main.rs", r#"
+            use std::io::fs;
+            use std::os;
+
+            fn main() {
+                let src = Path::new(os::getenv("SRC").unwrap());
+                let dst = Path::new(os::getenv("OUT_DIR").unwrap());
+                let dst = dst.join(src.filename().unwrap());
+                fs::rename(&src, &dst).unwrap();
+            }
+        "#)
+        .file("src/lib.rs", r#"
+            #[no_mangle]
+            pub extern fn foo() {}
+        "#);
+    assert_that(build.cargo_process("build"),
+                execs().with_status(0).with_stderr(""));
+    let src = build.root().join("target");
+    let lib = fs::readdir(&src).unwrap().into_iter().find(|lib| {
+        let lib = lib.filename_str().unwrap();
+        lib.starts_with(os::consts::DLL_PREFIX) &&
+            lib.ends_with(os::consts::DLL_SUFFIX)
+    }).unwrap();
+    let libname = lib.filename_str().unwrap();
+    let libname = libname.slice(os::consts::DLL_PREFIX.len(),
+                                libname.len() - os::consts::DLL_SUFFIX.len());
+
+    let foo = project("foo")
+        .file("Cargo.toml", r#"
+            [package]
+            name = "foo"
+            version = "0.0.1"
+            authors = []
+
+            [dependencies.bar]
+            path = "bar"
+        "#)
+        .file("src/main.rs", r#"
+            #![feature(phase)]
+            #[phase(plugin)] extern crate bar;
+
+            fn main() {}
+        "#)
+        .file("bar/Cargo.toml", format!(r#"
+            [package]
+            name = "bar"
+            version = "0.0.1"
+            authors = []
+            build = '{}'
+
+            [lib]
+            name = "bar"
+            plugin = true
+        "#, build.bin("build").display()))
+        .file("bar/src/lib.rs", format!(r#"
+            #![feature(plugin_registrar)]
+
+            extern crate rustc;
+
+            use rustc::plugin::Registry;
+
+            #[link(name = "{}")]
+            extern {{ fn foo(); }}
+
+            #[plugin_registrar]
+            pub fn bar(_reg: &mut Registry) {{
+                unsafe {{ foo() }}
+            }}
+        "#, libname));
+
+    assert_that(foo.cargo_process("build").env("SRC", Some(lib.as_vec())),
                 execs().with_status(0));
 })
