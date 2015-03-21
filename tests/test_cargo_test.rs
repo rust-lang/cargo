1a452fb151bf077c9c1c2817f943b934aeb89b13
Alex Crichton <alex@alexcrichton.com>
2015-03-20 17:48:55 -0700

    Fix test for `--test` filtering
    The flag now only applies to integration tests, not all targets with the
    specified name.

--- tests/test_cargo_test.rs
+++ tests/test_cargo_test.rs
@@ -924,13 +924,6 @@ test test_b ... ok
 
 test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured
 
-{running} target[..]b-[..]
-
-running 1 test
-test test_b ... ok
-
-test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured
-
 ",
        compiling = COMPILING,
        running = RUNNING,
