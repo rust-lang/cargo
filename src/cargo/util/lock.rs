a2c6a515b28174c96fdf8eadda56382146c27e4f
Sebastian Thiel <byronimo@gmail.com>
2015-06-27 23:13:36 +0200

    Added locks for Registry methods
    Namely these are:
    
    * `open(...)`
    * `do_update(...)`
    * `download_package(...)`
    
    The general intention is to add an additional check for the originally
    desired result and to verify it in case it already exists. That way,
    duplication of work can be prevented (for example in case of
    `download_package(...)`).

--- src/cargo/util/lock.rs
+++ src/cargo/util/lock.rs
@@ -2,8 +2,9 @@ use file_lock::{FileLock, LockKind, AccessMode, ParseError};
 use file_lock::flock::Error as FileLockError;
 
 use std::path::PathBuf;
+use std::fs;
 
-use util::{Config, CargoError, CargoResult};
+use util::{Config, CargoError, CargoResult, human};
 
 impl From<FileLockError> for Box<CargoError> {
     fn from(t: FileLockError) -> Self {
@@ -26,7 +27,7 @@ pub struct CargoLock<'cfg> {
 }
 
 impl<'cfg> CargoLock<'cfg> {
-    pub fn new_exclusive(path: PathBuf, config: &'cfg Config) -> CargoLock<'cfg> {
+    pub fn new(path: PathBuf, config: &'cfg Config) -> CargoLock<'cfg> {
         CargoLock {
             config: config,
             inner: FileLock::new(path, AccessMode::Write)
@@ -41,12 +42,25 @@ impl<'cfg> CargoLock<'cfg> {
     }
 
     pub fn lock(&mut self) -> CargoResult<()> {
-        let kind: LockKind = try!(try!(self.config.get_string("build.lock-kind"))
+        const CONFIG_KEY: &'static str = "build.lock-kind";
+        let kind_string = try!(self.config.get_string(CONFIG_KEY))
                                                   .map(|t| t.0)
                                                   .unwrap_or_else(|| LockKind::NonBlocking
                                                                                 .as_ref()
-                                                                                .to_owned())
-                                .parse());
+                                                                                .to_owned());
+        let kind: LockKind = match kind_string.parse() {
+            Ok(kind) => kind,
+            Err(_) => return Err(human(format!("Failed to parse value '{}' at configuration key \
+                                               '{}'. Must be one of '{}' and '{}'", 
+                                               kind_string, CONFIG_KEY,
+                                               LockKind::NonBlocking.as_ref(), 
+                                               LockKind::Blocking.as_ref())))
+        };
+        // NOTE(ST): This could fail if cargo is run concurrently for the first time
+        // The only way to prevent it would be to take a lock in a directory which exists.
+        // This is why we don't try! here, but hope the directory exists when we 
+        // try to create the lock file
+        fs::create_dir_all(self.inner.path().parent().unwrap()).ok();
         Ok(try!(self.inner.any_lock(kind)))
     }
 }
