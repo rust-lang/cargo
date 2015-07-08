b337c5ee832098c81d96df3bbf38bd2843734f44
Sebastian Thiel <byronimo@gmail.com>
2015-07-08 10:46:42 +0200

    Improved error handling in CargoLock.lock()
    * just fail if the directory creation fails. This could happen if there
      is a race, but such a race would only possibly occour if cargo
      has never been invoked before on a particular CARGO_HOME
    * use try! + chain_error + human instead of more complex error handling

--- src/cargo/util/lock.rs
+++ src/cargo/util/lock.rs
@@ -3,9 +3,8 @@ use file_lock::filename::Lock as FileLock;
 
 use std::path::PathBuf;
 use std::fs;
-use std::thread::sleep_ms;
 
-use util::{CargoResult, caused_human};
+use util::{CargoResult, ChainError, human};
 
 pub use file_lock::filename::Kind as LockKind;
 
@@ -21,26 +20,12 @@ impl CargoLock {
     }
 
     pub fn lock(&mut self, kind: LockKind) -> CargoResult<()> {
-        // NOTE(ST): This could fail if cargo is run concurrently for the first time
-        // The only way to prevent it would be to take a lock in a directory which exists.
-        // This is why we don't try! here, but hope the directory exists when we 
-        // try to create the lock file
         {
             let lock_dir = self.inner.path().parent().unwrap();
-            if let Err(_) = fs::create_dir_all(lock_dir) {
-                // We might compete to create one or more directories here
-                // Give the competing process some time to finish. Then we will
-                // retry, hoping it the creation works (maybe just because the )
-                // directory is available already.
-                // TODO(ST): magic numbers, especially in sleep, will fail at some point ... .
-                sleep_ms(100);
-                if let Err(io_err) = fs::create_dir_all(lock_dir) {
-                    // Fail permanently if it still didn't work ... 
-                    return Err(caused_human(format!("Failed to create parent directory of \
-                                                     lock-file at '{}'", 
-                                                     lock_dir.display()), io_err));
-                }
-            }
+            try!(fs::create_dir_all(lock_dir).chain_error(|| { 
+                human(format!("Failed to create parent directory of lock-file at '{}'",
+                              lock_dir.display()))
+            }));
         }
         debug!("About to acquire file lock: '{}'", self.inner.path().display());
         Ok(try!(self.inner.any_lock(kind)))
