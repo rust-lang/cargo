aabe237ff4d8b878931ce03f1639fa695f9eb317
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-08-29 12:08:12 -0400

    Add tests for different arguments given to read-manifest
    Right now, only passing --manifest-path an absolute path to the
    parent directory of a Cargo.toml file works, but I think the other
    tests should work as well for consistency with other commands that
    optionally take --manifest-path.

--- tests/tests.rs
+++ tests/tests.rs
@@ -51,6 +51,7 @@ mod test_cargo_new;
 mod test_cargo_package;
 mod test_cargo_profiles;
 mod test_cargo_publish;
+mod test_cargo_read_manifest;
 mod test_cargo_registry;
 mod test_cargo_run;
 mod test_cargo_rustc;
