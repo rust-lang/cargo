70976106f25ed05be34ebcb2734340536271fc0a
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-09-19 20:41:40 -0400

    Extract a function for getting resolved packages from a registry
    This code was happening in both cargo fetch and cargo compile and had a
    slightly different error message in each ;)

--- src/cargo/ops/cargo_fetch.rs
+++ src/cargo/ops/cargo_fetch.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use core::registry::PackageRegistry;
-use core::{Package, PackageId};
+use core::{Package, PackageId, Resolve};
 use ops;
 use util::{CargoResult, Config, human, ChainError};
 
@@ -10,10 +10,14 @@ pub fn fetch(manifest_path: &Path, config: &Config) -> CargoResult<()> {
     let package = try!(Package::for_path(manifest_path, config));
     let mut registry = PackageRegistry::new(config);
     let resolve = try!(ops::resolve_pkg(&mut registry, &package));
-
-    let ids: Vec<PackageId> = resolve.iter().cloned().collect();
-    try!(registry.get(&ids).chain_error(|| {
-        human("unable to get packages from source")
-    }));
+    let _ = get_resolved_packages(&resolve, &mut registry);
     Ok(())
+}
+
+pub fn get_resolved_packages(resolve: &Resolve, registry: &mut PackageRegistry)
+                             -> CargoResult<Vec<Package>> {
+    let ids: Vec<PackageId> = resolve.iter().cloned().collect();
+    registry.get(&ids).chain_error(|| {
+        human("Unable to get packages from source")
+    })
 }
