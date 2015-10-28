3aaba548cbff6c7d1706629422f1120b2ef83589
Marvin Löbel <loebel.marvin@gmail.com>
2015-10-28 10:17:32 +0100

    Made cwd setting in process builder optional

--- src/cargo/util/rustc.rs
+++ src/cargo/util/rustc.rs
@@ -15,8 +15,8 @@ impl Rustc {
     /// If successful this function returns a description of the compiler along
     /// with a list of its capabilities.
     pub fn new<P: AsRef<Path>>(path: P, cwd: &Path) -> CargoResult<Rustc> {
-        let mut cmd = try!(util::process(path.as_ref(), cwd));
-        cmd.arg("-vV");
+        let mut cmd = util::process(path.as_ref());
+        cmd.cwd(cwd).arg("-vV");
 
         let mut ret = Rustc::blank();
         let mut first = cmd.clone();
