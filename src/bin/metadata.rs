1fd9f16e573a7efa91afb416eebe156c6e5152ac
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-01-23 14:54:35 +0300

    metadata: add version to format

--- src/bin/metadata.rs
+++ src/bin/metadata.rs
@@ -11,14 +11,15 @@ use cargo::util::{CliResult, Config};
 
 #[derive(RustcDecodable)]
 struct Options {
+    flag_color: Option<String>,
     flag_features: Vec<String>,
+    flag_format_version: u32,
     flag_manifest_path: Option<String>,
     flag_no_default_features: bool,
     flag_output_format: String,
     flag_output_path: Option<String>,
-    flag_verbose: bool,
     flag_quiet: bool,
-    flag_color: Option<String>,
+    flag_verbose: bool,
 }
 
 pub const USAGE: &'static str = "
@@ -29,16 +30,18 @@ Usage:
     cargo metadata [options]
 
 Options:
-    -h, --help               Print this message
-    -o, --output-path PATH   Path the output is written to, otherwise stdout is used
-    -f, --output-format FMT  Output format [default: toml]
-                             Valid values: toml, json
-    --features FEATURES      Space-separated list of features
-    --no-default-features    Do not include the `default` feature
-    --manifest-path PATH     Path to the manifest
-    -v, --verbose            Use verbose output
-    -q, --quiet              No output printed to stdout
-    --color WHEN             Coloring: auto, always, never
+    -h, --help                 Print this message
+    -o, --output-path PATH     Path the output is written to, otherwise stdout is used
+    -f, --output-format FMT    Output format [default: toml]
+                               Valid values: toml, json
+    --features FEATURES        Space-separated list of features
+    --no-default-features      Do not include the `default` feature
+    --manifest-path PATH       Path to the manifest
+    --format-version VERSION   Format version [default: 1]
+                               Valid values: 1
+    -v, --verbose              Use verbose output
+    -q, --quiet                No output printed to stdout
+    --color WHEN               Coloring: auto, always, never
 ";
 
 pub fn execute(options: Options, config: &Config) -> CliResult<Option<()>> {
@@ -57,6 +60,7 @@ pub fn execute(options: Options, config: &Config) -> CliResult<Option<()>> {
         no_default_features: options.flag_no_default_features,
         output_format: options.flag_output_format,
         output_to: output_to,
+        version: options.flag_format_version,
     };
 
     try!(output_metadata(options, config));
