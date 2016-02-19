c0247431290955082f2df8056a70804aa51636d9
Alex Crichton <alex@alexcrichton.com>
2016-02-19 13:37:55 -0800

    Remove SourceSet
    This isn't used anywhere

--- src/cargo/core/mod.rs
+++ src/cargo/core/mod.rs
@@ -6,7 +6,7 @@ pub use self::package_id_spec::PackageIdSpec;
 pub use self::registry::Registry;
 pub use self::resolver::Resolve;
 pub use self::shell::{Shell, MultiShell, ShellConfig, Verbosity, ColorConfig};
-pub use self::source::{Source, SourceId, SourceMap, SourceSet, GitReference};
+pub use self::source::{Source, SourceId, SourceMap, GitReference};
 pub use self::summary::Summary;
 
 pub mod source;
