6f433b7ce72ca42c3ee0e608cc025c2f04d4ce2f
Amanieu d'Antras <amanieu@gmail.com>
2016-02-23 20:50:26 +0000

    Ignore *.bk files generated by rustfmt in cargo-generated .gitignore

--- src/cargo/ops/cargo_new.rs
+++ src/cargo/ops/cargo_new.rs
@@ -316,7 +316,7 @@ fn mk(config: &Config, opts: &MkOptions) -> CargoResult<()> {
     let path = opts.path;
     let name = opts.name;
     let cfg = try!(global_config(config));
-    let mut ignore = "target\n".to_string();
+    let mut ignore = "target\n*.bk\n".to_string();
     let in_existing_vcs_repo = existing_vcs_repo(path.parent().unwrap(), config.cwd());
     if !opts.bin {
         ignore.push_str("Cargo.lock\n");
