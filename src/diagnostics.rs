a1245c324e68be1a506943a68448908fcb1947b5
Pascal Hertleif <killercup@gmail.com>
2016-07-07 12:04:15 +0200

    Make Serde Deserialize Our Nice Diagnostics

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -5,74 +5,74 @@
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Diagnostic {
     /// The primary error message.
-    message: String,
-    code: Option<DiagnosticCode>,
+    pub message: String,
+    pub code: Option<DiagnosticCode>,
     /// "error: internal compiler error", "error", "warning", "note", "help".
-    level: String,
-    spans: Vec<DiagnosticSpan>,
+    pub level: String,
+    pub spans: Vec<DiagnosticSpan>,
     /// Associated diagnostic messages.
-    children: Vec<Diagnostic>,
+    pub children: Vec<Diagnostic>,
     /// The message as rustc would render it. Currently this is only
     /// `Some` for "suggestions", but eventually it will include all
     /// snippets.
-    rendered: Option<String>,
+    pub rendered: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct DiagnosticSpan {
-    file_name: String,
-    byte_start: u32,
-    byte_end: u32,
+pub struct DiagnosticSpan {
+    pub file_name: String,
+    pub byte_start: u32,
+    pub byte_end: u32,
     /// 1-based.
-    line_start: usize,
-    line_end: usize,
+    pub line_start: usize,
+    pub line_end: usize,
     /// 1-based, character offset.
-    column_start: usize,
-    column_end: usize,
+    pub column_start: usize,
+    pub column_end: usize,
     /// Is this a "primary" span -- meaning the point, or one of the points,
     /// where the error occurred?
-    is_primary: bool,
+    pub is_primary: bool,
     /// Source text from the start of line_start to the end of line_end.
-    text: Vec<DiagnosticSpanLine>,
+    pub text: Vec<DiagnosticSpanLine>,
     /// Label that should be placed at this location (if any)
-    label: Option<String>,
+    pub label: Option<String>,
     /// If we are suggesting a replacement, this will contain text
     /// that should be sliced in atop this span. You may prefer to
     /// load the fully rendered version from the parent `Diagnostic`,
     /// however.
-    suggested_replacement: Option<String>,
+    pub suggested_replacement: Option<String>,
     /// Macro invocations that created the code at this span, if any.
-    expansion: Option<Box<DiagnosticSpanMacroExpansion>>,
+    #[serde(bound="")]
+    pub expansion: Option<Box<DiagnosticSpanMacroExpansion>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct DiagnosticSpanLine {
-    text: String,
+pub struct DiagnosticSpanLine {
+    pub text: String,
 
     /// 1-based, character offset in self.text.
-    highlight_start: usize,
-
-    highlight_end: usize,
+    pub highlight_start: usize,
+    pub highlight_end: usize,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct DiagnosticSpanMacroExpansion {
+pub struct DiagnosticSpanMacroExpansion {
     /// span where macro was applied to generate this code; note that
     /// this may itself derive from a macro (if
     /// `span.expansion.is_some()`)
-    span: DiagnosticSpan,
+    pub span: DiagnosticSpan,
 
     /// name of macro that was applied (e.g., "foo!" or "#[derive(Eq)]")
-    macro_decl_name: String,
+    pub macro_decl_name: String,
 
     /// span where macro was defined (if known)
-    def_site_span: Option<DiagnosticSpan>,
+    pub def_site_span: Option<DiagnosticSpan>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct DiagnosticCode {
+pub struct DiagnosticCode {
     /// The code itself.
-    code: String,
+    pub code: String,
     /// An explanation for the code.
-    explanation: Option<String>,
+    pub explanation: Option<String>,
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,15 +1,16 @@
 use std::fs::File;
+use std::io::Read;
 use serde_json;
 
-fn not_empty(s: &str) -> bool {
+fn not_empty(s: &&str) -> bool {
     s.trim().len() > 0
 }
 
 #[test]
 fn clippy() {
-    let file = File::open("tests/fixtures/clippy.json");
+    let mut file = File::open("tests/fixtures/clippy.json").unwrap();
     let mut buffer = String::new();
-    file.read_to_string(&mut buffer);
+    file.read_to_string(&mut buffer).unwrap();
 
     for line in buffer.lines().filter(not_empty) {
         let deserialized: ::diagnostics::Diagnostic = serde_json::from_str(&line).unwrap();
