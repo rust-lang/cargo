0a952925c8e2c39ce18eedf66eb33f08b437f2d0
Andy Russell <arussell123@gmail.com>
2016-10-20 23:57:51 -0400

    avoid panic when virtual workspace has no members

--- tests/workspaces.rs
+++ tests/workspaces.rs
@@ -655,6 +655,29 @@ fn virtual_build() {
     let p = project("foo")
         .file("Cargo.toml", r#"
             [workspace]
+            members = ["bar"]
+        "#)
+        .file("bar/Cargo.toml", r#"
+            [project]
+            name = "bar"
+            version = "0.1.0"
+            authors = []
+        "#)
+        .file("bar/src/main.rs", "fn main() {}");
+    p.build();
+    assert_that(p.cargo("build"),
+                execs().with_status(101)
+                       .with_stderr("\
+error: manifest path `[..]` is a virtual manifest, but this command \
+requires running against an actual package in this workspace
+"));
+}
+
+#[test]
+fn virtual_build_no_members() {
+    let p = project("foo")
+        .file("Cargo.toml", r#"
+            [workspace]
         "#)
         .file("bar/Cargo.toml", r#"
             [project]
