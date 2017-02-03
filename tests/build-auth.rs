e3d816208338e80c549fd63bb26c124ea9376913
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-02-03 16:38:13 +0100

    ensure libressl compatibility
    Update libgit2-sys, openssl-sys and openssl
    as the versions currently in Cargo.lock are
    not compatible with libressl.
    Also update tests accordingly
    
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- tests/build-auth.rs
+++ tests/build-auth.rs
@@ -41,7 +41,7 @@ fn http_auth_offered() {
         assert_eq!(req, vec![
             "GET /foo/bar/info/refs?service=git-upload-pack HTTP/1.1",
             "Accept: */*",
-            "User-Agent: git/1.0 (libgit2 0.24.0)",
+            "User-Agent: git/2.0 (libgit2 0.25.0)",
         ].into_iter().map(|s| s.to_string()).collect());
         drop(conn);
 
@@ -56,7 +56,7 @@ fn http_auth_offered() {
             "GET /foo/bar/info/refs?service=git-upload-pack HTTP/1.1",
             "Authorization: Basic Zm9vOmJhcg==",
             "Accept: */*",
-            "User-Agent: git/1.0 (libgit2 0.24.0)",
+            "User-Agent: git/2.0 (libgit2 0.25.0)",
         ].into_iter().map(|s| s.to_string()).collect());
     });
 
