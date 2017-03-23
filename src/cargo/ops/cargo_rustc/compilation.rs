d70ca2108f88b0deaf7a6793aba3554abccee770
Anton Larin <edding.default@gmail.com>
2017-03-23 17:26:40 +0300

    Include package props with env vars into target metadata
    Previously, when changing package properties with corresponding
    environment variables (such as authors, which has CARGO_PKG_AUTHORS),
    it didn't invalidate the build, even though there could have been
    a dependency on such variables in the source code.
    
    This commit includes such properties (there are 3 of them in total:
    authors, description and homepage) in the target metadata.
    
    Fixes #3696.

--- src/cargo/ops/cargo_rustc/compilation.rs
+++ src/cargo/ops/cargo_rustc/compilation.rs
@@ -146,6 +146,10 @@ impl<'cfg> Compilation<'cfg> {
         let cargo_exe = self.config.cargo_exe()?;
         cmd.env(::CARGO_ENV, cargo_exe);
 
+        // When adding new environment variables depending on
+        // crate properties which might require rebuild upon change
+        // consider adding the corresponding properties to the hash
+        // in Context::target_metadata()
         cmd.env("CARGO_MANIFEST_DIR", pkg.root())
            .env("CARGO_PKG_VERSION_MAJOR", &pkg.version().major.to_string())
            .env("CARGO_PKG_VERSION_MINOR", &pkg.version().minor.to_string())
