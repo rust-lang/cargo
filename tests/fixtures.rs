a3fa6c1d50eb09c64f552637f4feadf069c7c1eb
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-05-15 13:29:11 +0200

    Rewrite the scripts in duct

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -1,53 +1,47 @@
-use std::process::{Command, Stdio};
-use std::fs::File;
-use std::io::{Read, Write};
+#[macro_use]
+extern crate duct;
 
 #[test]
 fn fixtures() {
     // ignore if this fails
-    let _ = Command::new("cargo").arg("install").arg("clippy").status().unwrap();
+    let _ = cmd!("cargo", "install", "clippy").run();
     let root_dir = std::env::current_dir().unwrap();
     println!("root: {:?}", root_dir);
     for fixture in std::fs::read_dir(root_dir.join("tests/crates")).unwrap() {
         let fixture = fixture.unwrap();
         let fixture_path = fixture.path();
         // FIXME: don't expect the crate to be in the `ui` subdir
-        std::env::set_current_dir(fixture_path.join("ui")).unwrap();
+        let dir = fixture_path.join("ui");
         for entry in std::fs::read_dir(root_dir.join("tests/tests").join(fixture_path.file_name().unwrap())).unwrap() {
             let test = entry.unwrap().path();
             println!("{:?}", test);
-            assert!(Command::new("git").arg("checkout").arg(".").status().unwrap().success());
-            assert!(Command::new("cargo").arg("clean").status().unwrap().success());
-            let mut input = Vec::new();
-            File::open(test.join("input.txt")).unwrap().read_to_end(&mut input).unwrap();
+            assert!(cmd!("git", "checkout", ".").dir(&dir).run().unwrap().status.success());
+            assert!(cmd!("cargo", "clean").dir(&dir).run().unwrap().status.success());
             let manifest = format!("{:?}", root_dir.join("Cargo.toml"));
-            let mut cmd = Command::new("cargo")
-                .arg("run")
-                .arg(format!("--manifest-path={}", &manifest[1..manifest.len() - 1]))
-                .arg("--")
-                .arg("--clippy")
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .spawn()
+            cmd!("cargo",
+                "run",
+                format!("--manifest-path={}", &manifest[1..manifest.len() - 1]),
+                "--",
+                "--clippy"
+                )
+                .dir(&dir)
+                .stdin(test.join("input.txt"))
+                .stdout(dir.join("output.txt"))
+                .run()
                 .unwrap();
-            cmd.stdin.as_mut().unwrap().write(&input).unwrap();
-            let output = cmd.wait_with_output().unwrap();
-            File::create("output.txt").unwrap().write_all(&output.stdout).unwrap();
+
             if std::env::var("APPLY_RUSTFIX").is_ok() {
-                std::fs::copy("output.txt", test.join("output.txt")).unwrap();
-                let diff = Command::new("git").arg("diff").output().unwrap();
-                File::create(test.join("diff.diff")).unwrap().write_all(&diff.stdout).unwrap();
+                std::fs::copy(dir.join("output.txt"), test.join("output.txt")).unwrap();
+                cmd!("git", "diff").dir(&dir).stdout(test.join("diff.diff")).run().unwrap();
             } else {
-                let diff = Command::new("git").arg("diff").output().unwrap();
-                File::create("diff.diff").unwrap().write_all(&diff.stdout).unwrap();
-                if !Command::new("diff").arg("-q").arg("diff.diff").arg(test.join("diff.diff")).status().unwrap().success() {
+                cmd!("git", "diff").dir(&dir).stdout(dir.join("diff.diff")).run().unwrap();
+                if !cmd!("diff", "-q", dir.join("diff.diff"), test.join("diff.diff")).dir(&dir).unchecked().run().unwrap().status.success() {
                     panic!("Unexpected changes applied by rustfix");
                 }
-                if !Command::new("diff").arg("-q").arg("output.txt").arg(test.join("output.txt")).status().unwrap().success() {
+                if !cmd!("diff", "-q", dir.join("output.txt"), test.join("output.txt")).dir(&dir).unchecked().run().unwrap().status.success() {
                     panic!("Unexpected output by rustfix");
                 }
             }
         }
-        std::env::set_current_dir(&root_dir).unwrap();
     }
 }
