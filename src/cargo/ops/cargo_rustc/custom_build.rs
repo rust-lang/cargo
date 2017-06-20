rust-lang/cargo

commit a4c0c06275889ea4231a4fccd1180e6ab5907428

Coalesce config.rustc and config.rustc_info

src/cargo/ops/cargo_rustc/custom_build.rs
@@ -113,7 +113,7 @@ fn build_work<'a, 'cfg>(cx: &mut Context<'a, 'cfg>, unit: &Unit<'a>)
      .env("OPT_LEVEL", &profile.opt_level.to_string())
      .env("PROFILE", if cx.build_config.release {"release"} else {"debug"})
      .env("HOST", cx.host_triple())
-     .env("RUSTC", &*try!(cx.config.rustc()))
+     .env("RUSTC", &try!(cx.config.rustc()).path)
      .env("RUSTDOC", &*try!(cx.config.rustdoc()));
 
      if let Some(links) = unit.pkg.manifest().links(){

src/bin/cargo.rs
@@ -16,7 +16,6 @@ use cargo::core::shell::Verbosity;
 use cargo::execute_main_without_stdin;
 use cargo::util::{self, CliResult, lev_distance, Config, human, CargoResult};
 use cargo::util::CliError;
-use cargo::util::process_builder::process;
 
 #[derive(RustcDecodable)]
 pub struct Flags {
@@ -150,9 +149,8 @@ fn execute(flags: Flags, config: &Config) -> CliResult<Option<()>> {
     }
 
     if let Some(ref code) = flags.flag_explain {
-        try!(process(&*try!(config.rustc()))
-                 .arg("--explain").arg(code).exec()
-                 .map_err(human));
+        let mut procss = try!(config.rustc()).process();
+        try!(procss.arg("--explain").arg(code).exec().map_err(human));
         return Ok(None)
     }
 

src/cargo/ops/cargo_clean.rs
@@ -33,7 +33,7 @@ pub fn clean(ws: &Workspace, opts: &CleanOptions) -> CargoResult<()> {
     let packages = ops::get_resolved_packages(&resolve, registry);
 
     let profiles = try!(ws.current()).manifest().profiles();
-    let host_triple = try!(opts.config.rustc_info()).host.clone();
+    let host_triple = try!(opts.config.rustc()).host.clone();
     let mut cx = try!(Context::new(ws, &resolve, &packages, opts.config,
                                    BuildConfig {
                                        host_triple: host_triple,

src/cargo/ops/cargo_compile.rs
@@ -443,7 +443,7 @@ fn scrape_build_config(config: &Config,
     let cfg_target = try!(config.get_string("build.target")).map(|s| s.val);
     let target = target.or(cfg_target);
     let mut base = ops::BuildConfig {
-        host_triple: try!(config.rustc_info()).host.clone(),
+        host_triple: try!(config.rustc()).host.clone(),
         requested_target: target.clone(),
         jobs: jobs,
         ..Default::default()

src/cargo/ops/cargo_rustc/context.rs
@@ -8,7 +8,7 @@ use std::sync::Arc;
 use core::{Package, PackageId, PackageSet, Resolve, Target, Profile};
 use core::{TargetKind, Profiles, Metadata, Dependency, Workspace};
 use core::dependency::Kind as DepKind;
-use util::{self, CargoResult, ChainError, internal, Config, profile, Cfg, human};
+use util::{CargoResult, ChainError, internal, Config, profile, Cfg, human};
 
 use super::TargetConfig;
 use super::custom_build::{BuildState, BuildScripts};
@@ -166,7 +166,7 @@ impl<'a, 'cfg> Context<'a, 'cfg> {
                                       &self.build_config,
                                       kind,
                                       "RUSTFLAGS"));
-        let mut process = util::process(&*try!(self.config.rustc()));
+        let mut process = try!(self.config.rustc()).process();
         process.arg("-")
                .arg("--crate-name").arg("_")
                .arg("--print=file-names")

src/cargo/ops/cargo_rustc/engine.rs
@@ -41,7 +41,7 @@ impl CommandPrototype {
         Ok(CommandPrototype {
             builder: {
                 let mut p = match ty {
-                    CommandType::Rustc => process(&*try!(config.rustc())),
+                    CommandType::Rustc => try!(config.rustc()).process(),
                     CommandType::Rustdoc => process(&*try!(config.rustdoc())),
                     CommandType::Target(ref s) |
                     CommandType::Host(ref s) => process(s),

src/cargo/ops/cargo_rustc/fingerprint.rs
@@ -352,7 +352,7 @@ fn calculate<'a, 'cfg>(cx: &mut Context<'a, 'cfg>, unit: &Unit<'a>)
         try!(cx.rustflags_args(unit))
     };
     let fingerprint = Arc::new(Fingerprint {
-        rustc: util::hash_u64(&try!(cx.config.rustc_info()).verbose_version),
+        rustc: util::hash_u64(&try!(cx.config.rustc()).verbose_version),
         target: util::hash_u64(&unit.target),
         profile: util::hash_u64(&unit.profile),
         features: format!("{:?}", features),

src/cargo/ops/cargo_rustc/mod.rs
@@ -204,7 +204,7 @@ fn rustc(cx: &mut Context, unit: &Unit) -> CargoResult<Work> {
 
     let name = unit.pkg.name().to_string();
     if !cx.show_warnings(unit.pkg.package_id()) {
-        if try!(cx.config.rustc_info()).cap_lints {
+        if try!(cx.config.rustc()).cap_lints {
             rustc.arg("--cap-lints").arg("allow");
         } else {
             rustc.arg("-Awarnings");

commit dedcb20db28422a1bac36d96f80867d580dd32ff

use full path to rustdoc as for rustc
If user is using system-wide rust/cargo and rustup'ed one and tries to run
`cargo test`, rustc is executed through /usr/bin/rustc, but rustdoc
is executed without full path which leads to:

error[E0514]: found crate `XXX` compiled by an incompatible version of rustc

Fixes: a4c0c06275889ea4231a4fccd1180e6ab5907428
Reported-by: Martin Sehnoutka <msehnout@rehdat.com>
Signed-off-by: Igor Gnatenko <ignatenkobrain@fedoraproject.org>

src/cargo/ops/cargo_rustc/custom_build.rs
@@ -123,7 +123,7 @@ fn build_work<'a, 'cfg>(cx: &mut Context<'a, 'cfg>, unit: &Unit<'a>)
        .env("PROFILE", if cx.build_config.release { "release" } else { "debug" })
        .env("HOST", cx.host_triple())
        .env("RUSTC", &cx.config.rustc()?.path)
-       .env("RUSTDOC", &*cx.config.rustdoc()?)
+       .env("RUSTDOC", &cx.config.rustdoc()?.path)
        .inherit_jobserver(&cx.jobserver);
 
     if let Some(links) = unit.pkg.manifest().links() {