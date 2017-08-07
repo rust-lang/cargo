c5c8446e1ee837b33e694af05156fbe3ecdfab15
Behnam Esfahbod <behnam@zwnj.org>
2017-08-07 09:31:32 -0700

    [cargo_new] Hint to use `cargo init` on existing dir
    The `new` command always expects a non-existing path. The `init` command
    always expects an existing path. Therefore, it makes sense to hint to
    use `init` if the pre-condition to `new` is not satisfied.
    
    Fixes <https://github.com/rust-lang/cargo/issues/4366>

--- tests/new.rs
+++ tests/new.rs
@@ -111,7 +111,8 @@ fn existing() {
     fs::create_dir(&dst).unwrap();
     assert_that(cargo_process("new").arg("foo"),
                 execs().with_status(101)
-                       .with_stderr(format!("[ERROR] destination `{}` already exists\n",
+                       .with_stderr(format!("[ERROR] destination `{}` already exists\n\n\
+                                            Use `cargo init` to initialize the directory",
                                             dst.display())));
 }
 
