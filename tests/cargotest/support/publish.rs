2af2dfe7344b2cedbd4e03e403c7a8a12c31fa84
Chris Swindle <christopher.swindle@metaswitch.com>
2017-10-31 06:28:09 +0000

    Switch over to use --registry for publish list.

--- tests/cargotest/support/publish.rs
+++ tests/cargotest/support/publish.rs
@@ -10,10 +10,21 @@ use url::Url;
 pub fn setup() -> Repository {
     let config = paths::root().join(".cargo/config");
     t!(fs::create_dir_all(config.parent().unwrap()));
-    t!(t!(File::create(&config)).write_all(br#"
+    t!(t!(File::create(&config)).write_all(format!(r#"
         [registry]
             token = "api-token"
+
+        [registries.alternative]
+        index = "{registry}"
+    "#, registry = registry().to_string()).as_bytes()));
+
+    let credentials = paths::root().join("home/.cargo/credentials");
+    t!(fs::create_dir_all(credentials.parent().unwrap()));
+    t!(t!(File::create(&credentials)).write_all(br#"
+        [alternative]
+        token = "api-token"
     "#));
+
     t!(fs::create_dir_all(&upload_path().join("api/v1/crates")));
 
     repo(&registry_path())
