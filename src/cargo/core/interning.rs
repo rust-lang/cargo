4af9245f99b3be32b7defdc54a5564f00b799a11
Eh2406 <YeomanYaacov@gmail.com>
2018-03-07 17:19:53 -0500

    use `Deref` instead of an explicit function

--- src/cargo/core/interning.rs
+++ src/cargo/core/interning.rs
@@ -1,10 +1,10 @@
-use std::fmt;
 use std::sync::RwLock;
 use std::collections::HashSet;
 use std::slice;
 use std::str;
 use std::mem;
 use std::cmp::Ordering;
+use std::ops::Deref;
 
 pub fn leek(s: String) -> &'static str {
     let boxed = s.into_boxed_str();
@@ -38,23 +38,23 @@ impl InternedString {
         cache.insert(s);
         InternedString { ptr: s.as_ptr(), len: s.len() }
     }
-    pub fn to_inner(&self) -> &'static str {
-        unsafe {
-            let slice = slice::from_raw_parts(self.ptr, self.len);
-            str::from_utf8_unchecked(slice)
-        }
-    }
 }
 
-impl fmt::Debug for InternedString {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "InternedString {{ {} }}", self.to_inner())
+impl Deref for InternedString {
+    type Target = str;
+
+    fn deref(&self) -> &'static str {
+        unsafe {
+            let slice = slice::from_raw_parts(self.ptr, self.len);
+            &str::from_utf8_unchecked(slice)
+        }
     }
 }
 
 impl Ord for InternedString {
     fn cmp(&self, other: &InternedString) -> Ordering {
-        self.to_inner().cmp(&other.to_inner())
+        let str: &str = &*self;
+        str.cmp(&*other)
     }
 }
 
