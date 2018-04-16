09b3d19df0fec53d6a65f8f11e8d00cd8838d2a9
Dale Wijnand <dale.wijnand@gmail.com>
2018-04-17 00:19:42 +0200

    Introduce autoXXX keys for target auto-discovery
    In Rust 2015 absence of the configuration makes it default to not
    include auto-discovered targets (i.e false), with a warnings message.
    
    In Rust 2018 absence makes it default to include auto-discovered
    targets (i.e true).
    
    Fixes #5330

--- src/cargo/util/toml/targets.rs
+++ src/cargo/util/toml/targets.rs
@@ -14,7 +14,7 @@ use std::path::{Path, PathBuf};
 use std::fs::{self, DirEntry};
 use std::collections::HashSet;
 
-use core::{compiler, Target};
+use core::{compiler, Edition, Target};
 use util::errors::CargoResult;
 use super::{LibKind, PathValue, StringOrBool, TomlBenchTarget, TomlBinTarget, TomlExampleTarget,
             TomlLibTarget, TomlManifest, TomlTarget, TomlTestTarget};
@@ -23,6 +23,7 @@ pub fn targets(
     manifest: &TomlManifest,
     package_name: &str,
     package_root: &Path,
+    edition: Edition,
     custom_build: &Option<StringOrBool>,
     warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
@@ -38,10 +39,18 @@ pub fn targets(
         has_lib = false;
     }
 
+    let package = manifest
+        .package
+        .as_ref()
+        .or_else(|| manifest.project.as_ref())
+        .ok_or_else(|| format_err!("manifest has no `package` (or `project`)"))?;
+
     targets.extend(clean_bins(
         manifest.bin.as_ref(),
         package_root,
         package_name,
+        edition,
+        package.autobins,
         warnings,
         errors,
         has_lib,
@@ -50,14 +59,26 @@ pub fn targets(
     targets.extend(clean_examples(
         manifest.example.as_ref(),
         package_root,
+        edition,
+        package.autoexamples,
+        warnings,
         errors,
     )?);
 
-    targets.extend(clean_tests(manifest.test.as_ref(), package_root, errors)?);
+    targets.extend(clean_tests(
+        manifest.test.as_ref(),
+        package_root,
+        edition,
+        package.autotests,
+        warnings,
+        errors,
+    )?);
 
     targets.extend(clean_benches(
         manifest.bench.as_ref(),
         package_root,
+        edition,
+        package.autobenches,
         warnings,
         errors,
     )?);
@@ -163,6 +184,8 @@ fn clean_bins(
     toml_bins: Option<&Vec<TomlBinTarget>>,
     package_root: &Path,
     package_name: &str,
+    edition: Edition,
+    autodiscover: Option<bool>,
     warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
     has_lib: bool,
@@ -172,6 +195,13 @@ fn clean_bins(
     let bins = toml_targets_and_inferred(
         toml_bins,
         &inferred,
+        package_root,
+        autodiscover,
+        edition,
+        warnings,
+        "binary",
+        "bin",
+        "autobins",
     );
 
     for bin in &bins {
@@ -254,6 +284,9 @@ fn clean_bins(
 fn clean_examples(
     toml_examples: Option<&Vec<TomlExampleTarget>>,
     package_root: &Path,
+    edition: Edition,
+    autodiscover: Option<bool>,
+    warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
 ) -> CargoResult<Vec<Target>> {
     let inferred = infer_from_directory(&package_root.join("examples"));
@@ -264,7 +297,11 @@ fn clean_examples(
         toml_examples,
         &inferred,
         package_root,
+        edition,
+        autodiscover,
+        warnings,
         errors,
+        "autoexamples",
     )?;
 
     let mut result = Vec::new();
@@ -290,11 +327,25 @@ fn clean_examples(
 fn clean_tests(
     toml_tests: Option<&Vec<TomlTestTarget>>,
     package_root: &Path,
+    edition: Edition,
+    autodiscover: Option<bool>,
+    warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
 ) -> CargoResult<Vec<Target>> {
     let inferred = infer_from_directory(&package_root.join("tests"));
 
-    let targets = clean_targets("test", "test", toml_tests, &inferred, package_root, errors)?;
+    let targets = clean_targets(
+        "test",
+        "test",
+        toml_tests,
+        &inferred,
+        package_root,
+        edition,
+        autodiscover,
+        warnings,
+        errors,
+        "autotests",
+    )?;
 
     let mut result = Vec::new();
     for (path, toml) in targets {
@@ -308,6 +359,8 @@ fn clean_tests(
 fn clean_benches(
     toml_benches: Option<&Vec<TomlBenchTarget>>,
     package_root: &Path,
+    edition: Edition,
+    autodiscover: Option<bool>,
     warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
 ) -> CargoResult<Vec<Target>> {
@@ -336,8 +389,12 @@ fn clean_benches(
             toml_benches,
             &inferred,
             package_root,
+            edition,
+            autodiscover,
+            warnings,
             errors,
             &mut legacy_bench_path,
+            "autobenches",
         )?
     };
 
@@ -359,7 +416,11 @@ fn clean_targets(
     toml_targets: Option<&Vec<TomlTarget>>,
     inferred: &[(String, PathBuf)],
     package_root: &Path,
+    edition: Edition,
+    autodiscover: Option<bool>,
+    warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
+    autodiscover_flag_name: &str,
 ) -> CargoResult<Vec<(PathBuf, TomlTarget)>> {
     clean_targets_with_legacy_path(
         target_kind_human,
@@ -367,8 +428,12 @@ fn clean_targets(
         toml_targets,
         inferred,
         package_root,
+        edition,
+        autodiscover,
+        warnings,
         errors,
         &mut |_| None,
+        autodiscover_flag_name,
     )
 }
 
@@ -378,12 +443,23 @@ fn clean_targets_with_legacy_path(
     toml_targets: Option<&Vec<TomlTarget>>,
     inferred: &[(String, PathBuf)],
     package_root: &Path,
+    edition: Edition,
+    autodiscover: Option<bool>,
+    warnings: &mut Vec<String>,
     errors: &mut Vec<String>,
     legacy_path: &mut FnMut(&TomlTarget) -> Option<PathBuf>,
+    autodiscover_flag_name: &str,
 ) -> CargoResult<Vec<(PathBuf, TomlTarget)>> {
     let toml_targets = toml_targets_and_inferred(
         toml_targets,
         inferred,
+        package_root,
+        autodiscover,
+        edition,
+        warnings,
+        target_kind_human,
+        target_kind,
+        autodiscover_flag_name,
     );
 
     for target in &toml_targets {
@@ -473,10 +549,81 @@ fn is_not_dotfile(entry: &DirEntry) -> bool {
 fn toml_targets_and_inferred(
     toml_targets: Option<&Vec<TomlTarget>>,
     inferred: &[(String, PathBuf)],
+    package_root: &Path,
+    autodiscover: Option<bool>,
+    edition: Edition,
+    warnings: &mut Vec<String>,
+    target_kind_human: &str,
+    target_kind: &str,
+    autodiscover_flag_name: &str,
 ) -> Vec<TomlTarget> {
+    let inferred_targets = inferred_to_toml_targets(inferred);
     match toml_targets {
-        None => inferred_to_toml_targets(inferred),
-        Some(targets) => targets.clone(),
+        None => inferred_targets,
+        Some(targets) => {
+            let mut targets = targets.clone();
+
+            let target_path =
+                |target: &TomlTarget| target.path.clone().map(|p| package_root.join(p.0));
+
+            let mut seen_names = HashSet::new();
+            let mut seen_paths = HashSet::new();
+            for target in targets.iter() {
+                seen_names.insert(target.name.clone());
+                seen_paths.insert(target_path(target));
+            }
+
+            let mut rem_targets = vec![];
+            for target in inferred_targets {
+                if !seen_names.contains(&target.name) && !seen_paths.contains(&target_path(&target))
+                {
+                    rem_targets.push(target);
+                }
+            }
+
+            let autodiscover = match autodiscover {
+                Some(autodiscover) => autodiscover,
+                None => match edition {
+                    Edition::Edition2018 => true,
+                    Edition::Edition2015 => {
+                        if !rem_targets.is_empty() {
+                            let mut rem_targets_str = String::new();
+                            for t in rem_targets.iter() {
+                                if let Some(p) = t.path.clone() {
+                                    rem_targets_str.push_str(&format!("* {:?}\n", p.0))
+                                }
+                            }
+                            warnings.push(format!(
+                                "\
+An explicit [[{section}]] section is specified in Cargo.toml which currently disables Cargo from \
+automatically inferring other {target_kind_human} targets. This inference behavior will change in \
+the Rust 2018 edition and the following files will be included as a {target_kind_human} target:
+
+{rem_targets_str}
+This is likely to break cargo build or cargo test as these files may not be ready to be compiled \
+as a {target_kind_human} target today. You can future-proof yourself and disable this warning by \
+adding {autodiscover_flag_name} = false to your [package] section. You may also move the files to \
+a location where Cargo would not automatically infer them to be a target, such as in subfolders.
+
+For more information on this warning you can consult https://github.com/rust-lang/cargo/issues/5330\
+                            ",
+                                section = target_kind,
+                                target_kind_human = target_kind_human,
+                                rem_targets_str = rem_targets_str,
+                                autodiscover_flag_name = autodiscover_flag_name,
+                            ));
+                        };
+                        false
+                    }
+                },
+            };
+
+            if autodiscover {
+                targets.append(&mut rem_targets);
+            }
+
+            targets
+        }
     }
 }
 
