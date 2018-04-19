97b971a77541c69626062ea6096888ecdee04464
varkor <github@varkor.com>
2018-04-19 19:19:11 +0100

    Update tests to check for Checking

--- tests/testsuite/cargotest/support/mod.rs
+++ tests/testsuite/cargotest/support/mod.rs
@@ -992,6 +992,7 @@ fn substitute_macros(input: &str) -> String {
     let macros = [
         ("[RUNNING]", "     Running"),
         ("[COMPILING]", "   Compiling"),
+        ("[CHECKING]", "    Checking"),
         ("[CREATED]", "     Created"),
         ("[FINISHED]", "    Finished"),
         ("[ERROR]", "error:"),
