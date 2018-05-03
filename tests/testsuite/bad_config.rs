adc25b29b79ed343362a84edcfd134a727aa4bd8
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-05-03 13:22:03 +0300

    Fix the test

--- tests/testsuite/bad_config.rs
+++ tests/testsuite/bad_config.rs
@@ -284,10 +284,7 @@ fn invalid_global_config() {
         p.cargo("build").arg("-v"),
         execs().with_status(101).with_stderr(
             "\
-error: failed to parse manifest at `[..]`
-
-Caused by:
-  Couldn't load Cargo configuration
+[ERROR] Couldn't load Cargo configuration
 
 Caused by:
   could not parse TOML configuration in `[..]`
