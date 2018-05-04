de7714dc117ff5dbaf422607a105824b41a67e87
Pascal Hertleif <killercup@gmail.com>
2018-05-04 23:40:05 +0200

    Add multiple suggestions test

--- cargo-fix/tests/all/smoke.rs
+++ cargo-fix/tests/all/smoke.rs
@@ -100,3 +100,31 @@ fn preserve_line_endings() {
     p.expect_cmd("cargo fix").run();
     assert!(p.read("src/lib.rs").contains("\r\n"));
 }
+
+#[test]
+fn multiple_suggestions_for_the_same_thing() {
+    let p = project()
+        .file("src/lib.rs", "\
+            fn main() {
+                let xs = vec![String::from(\"foo\")];
+                // error: no diplay in scope, and there are two options
+                // (std::path::Display and std::fmt::Display)
+                let d: &Display = &xs;
+                println!(\"{}\", d);
+            }
+        ")
+        .build();
+
+    let stderr = "\
+[CHECKING] foo v0.1.0 (CWD)
+error: Cannot replace slice of data that was already replaced
+error: Could not compile `foo`.
+
+To learn more, run the command again with --verbose.
+";
+
+    p.expect_cmd("cargo fix")
+        .stderr(stderr)
+        .status(101)
+        .run();
+}
