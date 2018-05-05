b0e0cc01c16a7516d9ec59280558482846de5af0
Alex Crichton <alex@alexcrichton.com>
2018-05-04 22:44:38 -0700

    Switch tests to working-code-by-default
    Don't use a compilation error to test rustfix but instead use a lint.

--- cargo-fix/tests/all/smoke.rs
+++ cargo-fix/tests/all/smoke.rs
@@ -17,15 +17,12 @@ fn no_changes_necessary() {
 }
 
 #[test]
-fn fixes_missing_ampersand() {
+fn fixes_extra_mut() {
     let p = project()
         .file("src/lib.rs", r#"
-            fn add(a: &u32) -> u32 {
-                a + 1
-            }
-
             pub fn foo() -> u32 {
-                add(1)
+                let mut x = 3;
+                x
             }
         "#)
         .build();
@@ -44,12 +41,10 @@ fn fixes_missing_ampersand() {
 fn fixes_two_missing_ampersands() {
     let p = project()
         .file("src/lib.rs", r#"
-            fn add(a: &u32) -> u32 {
-                a + 1
-            }
-
             pub fn foo() -> u32 {
-                add(1) + add(1)
+                let mut x = 3;
+                let mut y = 3;
+                x + y
             }
         "#)
         .build();
@@ -65,15 +60,12 @@ fn fixes_two_missing_ampersands() {
 }
 
 #[test]
-fn tricky_ampersand() {
+fn tricky() {
     let p = project()
         .file("src/lib.rs", r#"
-            fn add(a: &u32) -> u32 {
-                a + 1
-            }
-
             pub fn foo() -> u32 {
-                add(1) + add(1)
+                let mut x = 3; let mut y = 3;
+                x + y
             }
         "#)
         .build();
@@ -93,38 +85,10 @@ fn preserve_line_endings() {
     let p = project()
         .file("src/lib.rs", "\
             fn add(a: &u32) -> u32 { a + 1 }\r\n\
-            pub fn foo() -> u32 { add(1) }\r\n\
+            pub fn foo() -> u32 { let mut x = 3; add(&x) }\r\n\
         ")
         .build();
 
     p.expect_cmd("cargo-fix fix").run();
     assert!(p.read("src/lib.rs").contains("\r\n"));
-}
-
-#[test]
-fn multiple_suggestions_for_the_same_thing() {
-    let p = project()
-        .file("src/lib.rs", "\
-            fn main() {
-                let xs = vec![String::from(\"foo\")];
-                // error: no diplay in scope, and there are two options
-                // (std::path::Display and std::fmt::Display)
-                let d: &Display = &xs;
-                println!(\"{}\", d);
-            }
-        ")
-        .build();
-
-    let stderr = "\
-[CHECKING] foo v0.1.0 (CWD)
-error: Cannot replace slice of data that was already replaced
-error: Could not compile `foo`.
-
-To learn more, run the command again with --verbose.
-";
-
-    p.expect_cmd("cargo-fix fix")
-        .stderr(stderr)
-        .status(101)
-        .run();
 }
