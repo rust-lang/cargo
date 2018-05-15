ffc570a7569949ab27953429529954b61d314f9d
Eric Huss <eric@huss.org>
2018-05-15 09:29:34 -0700

    Fix passing --edition to rustdoc during doctests.
    Fixes #5538

--- src/cargo/core/compiler/compilation.rs
+++ src/cargo/core/compiler/compilation.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 use semver::Version;
 use lazycell::LazyCell;
 
-use core::{Package, PackageId, Target, TargetKind};
+use core::{Feature, Package, PackageId, Target, TargetKind};
 use util::{self, join_paths, process, CargoResult, Config, ProcessBuilder};
 use super::BuildContext;
 
@@ -93,12 +93,23 @@ impl<'cfg> Compilation<'cfg> {
 
     /// See `process`.
     pub fn rustc_process(&self, pkg: &Package) -> CargoResult<ProcessBuilder> {
-        self.fill_env(self.rustc_process.clone(), pkg, true)
+        let mut p = self.fill_env(self.rustc_process.clone(), pkg, true)?;
+        let manifest = pkg.manifest();
+        if manifest.features().is_enabled(Feature::edition()) {
+            p.arg(format!("--edition={}", manifest.edition()));
+        }
+        Ok(p)
     }
 
     /// See `process`.
     pub fn rustdoc_process(&self, pkg: &Package) -> CargoResult<ProcessBuilder> {
-        self.fill_env(process(&*self.config.rustdoc()?), pkg, false)
+        let mut p = self.fill_env(process(&*self.config.rustdoc()?), pkg, false)?;
+        let manifest = pkg.manifest();
+        if manifest.features().is_enabled(Feature::edition()) {
+            p.arg("-Zunstable-options");
+            p.arg(format!("--edition={}", &manifest.edition()));
+        }
+        Ok(p)
     }
 
     /// See `process`.
