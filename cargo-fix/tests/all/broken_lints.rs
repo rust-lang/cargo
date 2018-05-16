d6f7fe802b44b8c4c412d5d7c44d0ce2fb7d5178
Pascal Hertleif <killercup@gmail.com>
2018-05-16 22:28:41 +0200

    Comment out broken lint test for now
    The lint in use is fixed in the compiler, so the fixing no longer fails
    which means this test no longer works. I couldn't write a rustc shim
    that outputs overlapping suggestions in 5min so I'll do this for now and
    come back to the real issue later.

--- cargo-fix/tests/all/broken_lints.rs
+++ cargo-fix/tests/all/broken_lints.rs
@@ -3,37 +3,37 @@
 //! TODO: Add rustc shim that outputs wrong suggestions instead of depending on
 //! actual rustc bugs!
 
-use super::project;
+// use super::project;
 
-#[test]
-fn tell_user_about_broken_lints() {
-    let p = project()
-        .file(
-            "src/lib.rs",
-            r#"
-                pub fn foo() {
-                    let mut i = 42;
-                }
-            "#,
-        )
-        .build();
+// #[test]
+// fn tell_user_about_broken_lints() {
+//     let p = project()
+//         .file(
+//             "src/lib.rs",
+//             r#"
+//                 pub fn foo() {
+//                     let mut i = 42;
+//                 }
+//             "#,
+//         )
+//         .build();
 
-    p.expect_cmd("cargo-fix fix")
-        .stderr_contains(r"warning: error applying suggestions to `src/lib.rs`")
-        .stderr_contains("The full error message was:")
-        .stderr_contains(
-            "> Could not replace range 56...60 in file -- maybe parts of it were already replaced?",
-        )
-        .stderr_contains(
-            "\
-             This likely indicates a bug in either rustc or rustfix itself,\n\
-             and we would appreciate a bug report! You're likely to see \n\
-             a number of compiler warnings after this message which rustfix\n\
-             attempted to fix but failed. If you could open an issue at\n\
-             https://github.com/rust-lang-nursery/rustfix/issues\n\
-             quoting the full output of this command we'd be very appreciative!\n\n\
-             ",
-        )
-        .status(0)
-        .run();
-}
+//     p.expect_cmd("cargo-fix fix")
+//         .stderr_contains(r"warning: error applying suggestions to `src/lib.rs`")
+//         .stderr_contains("The full error message was:")
+//         .stderr_contains(
+//             "> Could not replace range 56...60 in file -- maybe parts of it were already replaced?",
+//         )
+//         .stderr_contains(
+//             "\
+//              This likely indicates a bug in either rustc or rustfix itself,\n\
+//              and we would appreciate a bug report! You're likely to see \n\
+//              a number of compiler warnings after this message which rustfix\n\
+//              attempted to fix but failed. If you could open an issue at\n\
+//              https://github.com/rust-lang-nursery/rustfix/issues\n\
+//              quoting the full output of this command we'd be very appreciative!\n\n\
+//              ",
+//         )
+//         .status(0)
+//         .run();
+// }
