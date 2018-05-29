0e8ab4d0de99c70d92c9708a3f992d67bb7bf776
Esteve Fernandez <esteve@apache.org>
2018-05-29 17:08:49 +0200

    Removed stream().

--- tests/testsuite/rustdocflags.rs
+++ tests/testsuite/rustdocflags.rs
@@ -102,7 +102,7 @@ fn rerun() {
     );
     assert_that(
         p.cargo("doc").env("RUSTDOCFLAGS", "--cfg=bar"),
-        execs().stream().with_status(0).with_stderr(
+        execs().with_status(0).with_stderr(
             "\
 [DOCUMENTING] foo v0.0.1 ([..])
 [GENERATING] [..]/foo/target/doc/foo/index.html
