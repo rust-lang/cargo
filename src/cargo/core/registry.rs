38c6023a4d93edb42ae61d72a96f0b318c4fa5e8
Eh2406 <YeomanYaacov@gmail.com>
2018-07-14 18:21:04 -0400

    limit the heuristic to git dependencies

--- src/cargo/core/registry.rs
+++ src/cargo/core/registry.rs
@@ -556,12 +556,37 @@ fn lock(locked: &LockedMap, patches: &HashMap<Url, Vec<PackageId>>, summary: Sum
         //    requirement has changed. In this case we must discard the
         //    locked version because the dependency needs to be
         //    re-resolved.
+        //
+        // 3. We don't have a lock entry for this dependency, in which
+        //    case it was likely an optional dependency which wasn't
+        //    included previously so we just pass it through anyway.
+        //
+        // Cases 1/2 are handled by `matches_id` and case 3 is handled by
+        // falling through to the logic below.
         if let Some(&(_, ref locked_deps)) = pair {
             let locked = locked_deps.iter().find(|id| dep.matches_id(id));
             if let Some(locked) = locked {
                 trace!("\tfirst hit on {}", locked);
                 let mut dep = dep.clone();
                 dep.lock_to(locked);
+                return dep;
+            }
+        }
+
+        // Querying a git dependency has the side effect of pulling changes.
+        // So even the conservative re-resolution may be to aggressive.
+        // If this git dependency did not have a locked version, then we query
+        // all known locked packages to see if they match this dependency.
+        // If anything does then we lock it to that and move on.
+        if dep.source_id().git_reference().is_some() {
+            let v = locked
+                .get(dep.source_id())
+                .and_then(|map| map.get(&*dep.name()))
+                .and_then(|vec| vec.iter().find(|&&(ref id, _)| dep.matches_id(id)));
+            if let Some(&(ref id, _)) = v {
+                trace!("\tsecond hit on {}", id);
+                let mut dep = dep.clone();
+                dep.lock_to(id);
                 return dep;
             }
         }
