5ac592fa9e9d932748dd85bee2449f879a238f6a
Wilco Kusee <wilcokusee@gmail.com>
2018-07-15 10:17:19 +0200

    Add more unit tests

--- tests/testsuite/lints.rs
+++ tests/testsuite/lints.rs
@@ -26,3 +26,50 @@ fn deny() {
             .with_stderr_contains("[..]error: function is never used: `foo`[..]"),
     );
 }
+
+#[test]
+fn empty_lints_block() {
+    let p = project("foo")
+        .file(
+            "Cargo.toml",
+            r#"
+            [project]
+            name = "foo"
+            version = "0.0.1"
+            authors = []
+
+            [lints]
+        "#,
+        )
+        .file("src/lib.rs", "fn foo() {}")
+        .build();
+
+    assert_that(
+        p.cargo("build"),
+        execs().with_status(0),
+    );
+}
+
+#[test]
+fn invalid_state() {
+    let p = project("foo")
+        .file(
+            "Cargo.toml",
+            r#"
+            [project]
+            name = "foo"
+            version = "0.0.1"
+            authors = []
+
+            [lints]
+            non_snake_case = "something something"
+        "#,
+        )
+        .file("src/lib.rs", "fn foo() {}")
+        .build();
+
+    assert_that(
+        p.cargo("build"),
+        execs().with_status(0),
+    );
+}
