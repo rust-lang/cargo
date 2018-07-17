2aa8b3015cb4c218f7ebf27214e64b836ed85e89
knight42 <anonymousknight96@gmail.com>
2018-07-17 10:20:39 +0800

    test: add test for replaced source

--- tests/testsuite/search.rs
+++ tests/testsuite/search.rs
@@ -1,33 +1,71 @@
 use std::fs::{self, File};
 use std::io::prelude::*;
-use std::path::PathBuf;
+use std::path::Path;
 
 use cargo::util::ProcessBuilder;
 use support;
 use support::execs;
 use support::git::repo;
 use support::paths;
+use support::registry::{api_path, registry as registry_url, registry_path};
 use support::hamcrest::assert_that;
 use url::Url;
 
-fn registry_path() -> PathBuf {
-    paths::root().join("registry")
-}
-fn registry() -> Url {
-    Url::from_file_path(&*registry_path()).ok().unwrap()
-}
-fn api_path() -> PathBuf {
-    paths::root().join("api")
-}
 fn api() -> Url {
     Url::from_file_path(&*api_path()).ok().unwrap()
 }
 
+fn write_crates(dest: &Path) {
+    let content = r#"{
+        "crates": [{
+            "created_at": "2014-11-16T20:17:35Z",
+            "description": "Design by contract style assertions for Rust",
+            "documentation": null,
+            "downloads": 2,
+            "homepage": null,
+            "id": "hoare",
+            "keywords": [],
+            "license": null,
+            "links": {
+                "owners": "/api/v1/crates/hoare/owners",
+                "reverse_dependencies": "/api/v1/crates/hoare/reverse_dependencies",
+                "version_downloads": "/api/v1/crates/hoare/downloads",
+                "versions": "/api/v1/crates/hoare/versions"
+            },
+            "max_version": "0.1.1",
+            "name": "hoare",
+            "repository": "https://github.com/nick29581/libhoare",
+            "updated_at": "2014-11-20T21:49:21Z",
+            "versions": null
+        }],
+        "meta": {
+            "total": 1
+        }
+    }"#;
+
+    // Older versions of curl don't peel off query parameters when looking for
+    // filenames, so just make both files.
+    //
+    // On windows, though, `?` is an invalid character, but we always build curl
+    // from source there anyway!
+    File::create(&dest)
+        .unwrap()
+        .write_all(content.as_bytes())
+        .unwrap();
+    if !cfg!(windows) {
+        File::create(&dest.with_file_name("crates?q=postgres&per_page=10"))
+            .unwrap()
+            .write_all(content.as_bytes())
+            .unwrap();
+    }
+}
+
 fn setup() {
-    let config = paths::root().join(".cargo/config");
-    fs::create_dir_all(config.parent().unwrap()).unwrap();
+    let cargo_home = paths::root().join(".cargo");
+    fs::create_dir_all(cargo_home).unwrap();
     fs::create_dir_all(&api_path().join("api/v1")).unwrap();
 
+    // Init a new registry
     let _ = repo(&registry_path())
         .file(
             "config.json",
@@ -40,6 +78,30 @@ fn setup() {
             ),
         )
         .build();
+
+    let base = api_path().join("api/v1/crates");
+    write_crates(&base);
+}
+
+fn set_cargo_config() {
+    let config = paths::root().join(".cargo/config");
+
+    File::create(&config)
+        .unwrap()
+        .write_all(
+            format!(
+                r#"
+[source.crates-io]
+registry = 'https://wut'
+replace-with = 'dummy-registry'
+
+[source.dummy-registry]
+registry = '{reg}'
+"#,
+                reg = registry_url(),
+            ).as_bytes(),
+        )
+        .unwrap();
 }
 
 fn cargo_process(s: &str) -> ProcessBuilder {
@@ -49,58 +111,55 @@ fn cargo_process(s: &str) -> ProcessBuilder {
 }
 
 #[test]
+fn not_update() {
+    setup();
+    set_cargo_config();
+
+    use cargo::core::{Shell, Source, SourceId};
+    use cargo::sources::RegistrySource;
+    use cargo::util::Config;
+
+    let sid = SourceId::for_registry(&registry_url()).unwrap();
+    let cfg = Config::new(Shell::new(), paths::root(), paths::home().join(".cargo"));
+    let mut regsrc = RegistrySource::remote(&sid, &cfg);
+    regsrc.update().unwrap();
+
+    assert_that(
+        cargo_process("search").arg("postgres"),
+        execs()
+            .with_status(0)
+            .with_stdout_contains(
+                "hoare = \"0.1.1\"    # Design by contract style assertions for Rust",
+            )
+            .with_stderr(""), // without "Updating registry ..."
+    );
+}
+
+#[test]
+fn replace_default() {
+    setup();
+    set_cargo_config();
+
+    assert_that(
+        cargo_process("search").arg("postgres"),
+        execs()
+            .with_status(0)
+            .with_stdout_contains(
+                "hoare = \"0.1.1\"    # Design by contract style assertions for Rust",
+            )
+            .with_stderr_contains("[..]Updating registry[..]"),
+    );
+}
+
+#[test]
 fn simple() {
     setup();
-
-    let contents = r#"{
-        "crates": [{
-            "created_at": "2014-11-16T20:17:35Z",
-            "description": "Design by contract style assertions for Rust",
-            "documentation": null,
-            "downloads": 2,
-            "homepage": null,
-            "id": "hoare",
-            "keywords": [],
-            "license": null,
-            "links": {
-                "owners": "/api/v1/crates/hoare/owners",
-                "reverse_dependencies": "/api/v1/crates/hoare/reverse_dependencies",
-                "version_downloads": "/api/v1/crates/hoare/downloads",
-                "versions": "/api/v1/crates/hoare/versions"
-            },
-            "max_version": "0.1.1",
-            "name": "hoare",
-            "repository": "https://github.com/nick29581/libhoare",
-            "updated_at": "2014-11-20T21:49:21Z",
-            "versions": null
-        }],
-        "meta": {
-            "total": 1
-        }
-    }"#;
-    let base = api_path().join("api/v1/crates");
-
-    // Older versions of curl don't peel off query parameters when looking for
-    // filenames, so just make both files.
-    //
-    // On windows, though, `?` is an invalid character, but we always build curl
-    // from source there anyway!
-    File::create(&base)
-        .unwrap()
-        .write_all(contents.as_bytes())
-        .unwrap();
-    if !cfg!(windows) {
-        File::create(&base.with_file_name("crates?q=postgres&per_page=10"))
-            .unwrap()
-            .write_all(contents.as_bytes())
-            .unwrap();
-    }
 
     assert_that(
         cargo_process("search")
             .arg("postgres")
             .arg("--index")
-            .arg(registry().to_string()),
+            .arg(registry_url().to_string()),
         execs().with_status(0).with_stdout_contains(
             "hoare = \"0.1.1\"    # Design by contract style assertions for Rust",
         ),
@@ -113,55 +172,11 @@ fn simple() {
 fn simple_with_host() {
     setup();
 
-    let contents = r#"{
-        "crates": [{
-            "created_at": "2014-11-16T20:17:35Z",
-            "description": "Design by contract style assertions for Rust",
-            "documentation": null,
-            "downloads": 2,
-            "homepage": null,
-            "id": "hoare",
-            "keywords": [],
-            "license": null,
-            "links": {
-                "owners": "/api/v1/crates/hoare/owners",
-                "reverse_dependencies": "/api/v1/crates/hoare/reverse_dependencies",
-                "version_downloads": "/api/v1/crates/hoare/downloads",
-                "versions": "/api/v1/crates/hoare/versions"
-            },
-            "max_version": "0.1.1",
-            "name": "hoare",
-            "repository": "https://github.com/nick29581/libhoare",
-            "updated_at": "2014-11-20T21:49:21Z",
-            "versions": null
-        }],
-        "meta": {
-            "total": 1
-        }
-    }"#;
-    let base = api_path().join("api/v1/crates");
-
-    // Older versions of curl don't peel off query parameters when looking for
-    // filenames, so just make both files.
-    //
-    // On windows, though, `?` is an invalid character, but we always build curl
-    // from source there anyway!
-    File::create(&base)
-        .unwrap()
-        .write_all(contents.as_bytes())
-        .unwrap();
-    if !cfg!(windows) {
-        File::create(&base.with_file_name("crates?q=postgres&per_page=10"))
-            .unwrap()
-            .write_all(contents.as_bytes())
-            .unwrap();
-    }
-
     assert_that(
         cargo_process("search")
             .arg("postgres")
             .arg("--host")
-            .arg(registry().to_string()),
+            .arg(registry_url().to_string()),
         execs()
             .with_status(0)
             .with_stderr(&format!(
@@ -177,7 +192,7 @@ to update to a fixed version or contact the upstream maintainer
 about this warning.
 [UPDATING] registry `{reg}`
 ",
-                reg = registry()
+                reg = registry_url()
             ))
             .with_stdout_contains(
                 "hoare = \"0.1.1\"    # Design by contract style assertions for Rust",
@@ -191,57 +206,13 @@ about this warning.
 fn simple_with_index_and_host() {
     setup();
 
-    let contents = r#"{
-        "crates": [{
-            "created_at": "2014-11-16T20:17:35Z",
-            "description": "Design by contract style assertions for Rust",
-            "documentation": null,
-            "downloads": 2,
-            "homepage": null,
-            "id": "hoare",
-            "keywords": [],
-            "license": null,
-            "links": {
-                "owners": "/api/v1/crates/hoare/owners",
-                "reverse_dependencies": "/api/v1/crates/hoare/reverse_dependencies",
-                "version_downloads": "/api/v1/crates/hoare/downloads",
-                "versions": "/api/v1/crates/hoare/versions"
-            },
-            "max_version": "0.1.1",
-            "name": "hoare",
-            "repository": "https://github.com/nick29581/libhoare",
-            "updated_at": "2014-11-20T21:49:21Z",
-            "versions": null
-        }],
-        "meta": {
-            "total": 1
-        }
-    }"#;
-    let base = api_path().join("api/v1/crates");
-
-    // Older versions of curl don't peel off query parameters when looking for
-    // filenames, so just make both files.
-    //
-    // On windows, though, `?` is an invalid character, but we always build curl
-    // from source there anyway!
-    File::create(&base)
-        .unwrap()
-        .write_all(contents.as_bytes())
-        .unwrap();
-    if !cfg!(windows) {
-        File::create(&base.with_file_name("crates?q=postgres&per_page=10"))
-            .unwrap()
-            .write_all(contents.as_bytes())
-            .unwrap();
-    }
-
     assert_that(
         cargo_process("search")
             .arg("postgres")
             .arg("--index")
-            .arg(registry().to_string())
+            .arg(registry_url().to_string())
             .arg("--host")
-            .arg(registry().to_string()),
+            .arg(registry_url().to_string()),
         execs()
             .with_status(0)
             .with_stderr(&format!(
@@ -257,7 +228,7 @@ to update to a fixed version or contact the upstream maintainer
 about this warning.
 [UPDATING] registry `{reg}`
 ",
-                reg = registry()
+                reg = registry_url()
             ))
             .with_stdout_contains(
                 "hoare = \"0.1.1\"    # Design by contract style assertions for Rust",
@@ -269,56 +240,12 @@ about this warning.
 fn multiple_query_params() {
     setup();
 
-    let contents = r#"{
-        "crates": [{
-            "created_at": "2014-11-16T20:17:35Z",
-            "description": "Design by contract style assertions for Rust",
-            "documentation": null,
-            "downloads": 2,
-            "homepage": null,
-            "id": "hoare",
-            "keywords": [],
-            "license": null,
-            "links": {
-                "owners": "/api/v1/crates/hoare/owners",
-                "reverse_dependencies": "/api/v1/crates/hoare/reverse_dependencies",
-                "version_downloads": "/api/v1/crates/hoare/downloads",
-                "versions": "/api/v1/crates/hoare/versions"
-            },
-            "max_version": "0.1.1",
-            "name": "hoare",
-            "repository": "https://github.com/nick29581/libhoare",
-            "updated_at": "2014-11-20T21:49:21Z",
-            "versions": null
-        }],
-        "meta": {
-            "total": 1
-        }
-    }"#;
-    let base = api_path().join("api/v1/crates");
-
-    // Older versions of curl don't peel off query parameters when looking for
-    // filenames, so just make both files.
-    //
-    // On windows, though, `?` is an invalid character, but we always build curl
-    // from source there anyway!
-    File::create(&base)
-        .unwrap()
-        .write_all(contents.as_bytes())
-        .unwrap();
-    if !cfg!(windows) {
-        File::create(&base.with_file_name("crates?q=postgres+sql&per_page=10"))
-            .unwrap()
-            .write_all(contents.as_bytes())
-            .unwrap();
-    }
-
     assert_that(
         cargo_process("search")
             .arg("postgres")
             .arg("sql")
             .arg("--index")
-            .arg(registry().to_string()),
+            .arg(registry_url().to_string()),
         execs().with_status(0).with_stdout_contains(
             "hoare = \"0.1.1\"    # Design by contract style assertions for Rust",
         ),
