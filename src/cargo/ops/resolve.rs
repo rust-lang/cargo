9f20169888f9dcc70b93174e37c46fe8c0339654
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-23 07:46:05 +0100

    Give an explanation for exempting implicit_hasher

--- src/cargo/ops/resolve.rs
+++ src/cargo/ops/resolve.rs
@@ -133,7 +133,8 @@ fn resolve_with_registry<'cfg>(
 ///
 /// The previous resolve normally comes from a lockfile. This function does not
 /// read or write lockfiles from the filesystem.
-#[cfg_attr(feature = "cargo-clippy", allow(implicit_hasher))] // exempted
+// While unfortunate, generalising this over different hashers would be a large project
+#[cfg_attr(feature = "cargo-clippy", allow(implicit_hasher))]
 pub fn resolve_with_previous<'a, 'cfg>(
     registry: &mut PackageRegistry<'cfg>,
     ws: &Workspace<'cfg>,
