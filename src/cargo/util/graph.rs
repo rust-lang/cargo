fd5576d2e67f23a82aa4042725365a24b5b28a94
Eh2406 <YeomanYaacov@gmail.com>
2018-10-04 20:44:30 -0400

    remove Graph::sort as it is unused

--- src/cargo/util/graph.rs
+++ src/cargo/util/graph.rs
@@ -6,11 +6,6 @@ pub struct Graph<N, E> {
     nodes: HashMap<N, HashMap<N, E>>,
 }
 
-enum Mark {
-    InProgress,
-    Done,
-}
-
 pub type Nodes<'a, N, E> = Keys<'a, N, HashMap<N, E>>;
 pub type Edges<'a, N, E> = Iter<'a, N, E>;
 
@@ -39,32 +34,6 @@ impl<N: Eq + Hash + Clone, E: Default> Graph<N, E> {
 
     pub fn edges(&self, from: &N) -> Option<Edges<N, E>> {
         self.nodes.get(from).map(|set| set.iter())
-    }
-
-    pub fn sort(&self) -> Option<Vec<N>> {
-        let mut ret = Vec::new();
-        let mut marks = HashMap::new();
-
-        for node in self.nodes.keys() {
-            self.visit(node, &mut ret, &mut marks);
-        }
-
-        Some(ret)
-    }
-
-    fn visit(&self, node: &N, dst: &mut Vec<N>, marks: &mut HashMap<N, Mark>) {
-        if marks.contains_key(node) {
-            return;
-        }
-
-        marks.insert(node.clone(), Mark::InProgress);
-
-        for child in self.nodes[node].keys() {
-            self.visit(child, dst, marks);
-        }
-
-        dst.push(node.clone());
-        marks.insert(node.clone(), Mark::Done);
     }
 
     pub fn iter(&self) -> Nodes<N, E> {
