10b3ebf037d52de25d7ba1e5c9f34e74e806d63a
Alex Butler <alexheretic@gmail.com>
2018-10-09 21:44:32 +0100

    Wrap all, including io, manifest errors

--- tests/testsuite/member_errors.rs
+++ tests/testsuite/member_errors.rs
@@ -44,13 +44,15 @@ fn toml_deserialize_manifest_error() {
     let error = Workspace::new(&root_manifest_path, &Config::default().unwrap()).unwrap_err();
     eprintln!("{:?}", error);
 
-    let manifest_err = error
+    let manifest_errs: Vec<_> = error
         .iter_chain()
         .filter_map(|err| err.downcast_ref::<ManifestError>())
-        .last()
-        .expect("No ManifestError");
+        .map(|err| err.manifest_path())
+        .collect();
 
-    assert_eq!(manifest_err.manifest_path(), &member_manifest_path);
+    assert_eq!(manifest_errs.len(), 2, "{:?}", manifest_errs);
+    assert_eq!(manifest_errs[0], &root_manifest_path);
+    assert_eq!(manifest_errs[1], &member_manifest_path);
 }
 
 /// Tests inclusion of a `ManifestError` pointing to a member manifest
@@ -90,15 +92,19 @@ fn member_manifest_path_io_error() {
 
     let root_manifest_path = p.root().join("Cargo.toml");
     let member_manifest_path = p.root().join("bar").join("Cargo.toml");
+    let missing_manifest_path = p.root().join("bar").join("nosuch").join("Cargo.toml");
 
     let error = Workspace::new(&root_manifest_path, &Config::default().unwrap()).unwrap_err();
     eprintln!("{:?}", error);
 
-    let manifest_err = error
+    let manifest_errs: Vec<_> = error
         .iter_chain()
         .filter_map(|err| err.downcast_ref::<ManifestError>())
-        .last()
-        .expect("No ManifestError");
+        .map(|err| err.manifest_path())
+        .collect();
 
-    assert_eq!(manifest_err.manifest_path(), &member_manifest_path);
+    assert_eq!(manifest_errs.len(), 3, "{:?}", manifest_errs);
+    assert_eq!(manifest_errs[0], &root_manifest_path);
+    assert_eq!(manifest_errs[1], &member_manifest_path);
+    assert_eq!(manifest_errs[2], &missing_manifest_path);
 }
