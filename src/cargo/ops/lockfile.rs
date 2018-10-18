b5d89fef47e2288a5cd06f84d30983df07666a5e
Dale Wijnand <dale.wijnand@gmail.com>
2018-10-18 14:30:40 +0100

    Explain why top comments are preserved in Cargo.lock

--- src/cargo/ops/lockfile.rs
+++ src/cargo/ops/lockfile.rs
@@ -43,6 +43,9 @@ pub fn write_pkg_lockfile(ws: &Workspace, resolve: &Resolve) -> CargoResult<()> 
 
     let mut out = String::new();
 
+    // Preserve the top comments in the lockfile
+    // This is in preparation for marking it as generated
+    // https://github.com/rust-lang/cargo/issues/6180
     if let Ok(orig) = &orig {
         for line in orig.lines().take_while(|line| line.starts_with("#")) {
             out.push_str(line);
