a087785fa253838fef5293d8a78097a0f3bcc008
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2018-10-26 20:13:05 +0900

    Fix and add tests for built-in cmd shadowing

--- tests/testsuite/cargo_alias_config.rs
+++ tests/testsuite/cargo_alias_config.rs
@@ -137,7 +137,30 @@ fn cant_shadow_builtin() {
     p.cargo("build")
         .with_stderr(
             "\
-[WARNING] alias `build` is ignored, because it is shadowed by a built in command
+[WARNING] alias `build` is ignored, because it is shadowed by a built-in command
+[COMPILING] foo v0.5.0 ([..])
+[FINISHED] dev [unoptimized + debuginfo] target(s) in [..]
+",
+        ).run();
+}
+
+#[test]
+fn cant_shadow_builtin_alias() {
+    let p = project()
+        .file("Cargo.toml", &basic_bin_manifest("foo"))
+        .file("src/main.rs", "fn main() {}")
+        .file(
+            ".cargo/config",
+            r#"
+            [alias]
+            b = "fetch"
+         "#,
+        ).build();
+
+    p.cargo("b")
+        .with_stderr(
+            "\
+[WARNING] alias `b` is ignored, because it is shadowed by a built-in alias for `build`
 [COMPILING] foo v0.5.0 ([..])
 [FINISHED] dev [unoptimized + debuginfo] target(s) in [..]
 ",
