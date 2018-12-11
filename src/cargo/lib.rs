0832b7d3c1d5d9b7701c63c5b5745a4ba0ca7555
Alex Crichton <alex@alexcrichton.com>
2018-12-10 17:57:10 -0800

    Turn on Rust 2018 idiom warnings unconditionally
    It may take us awhile to get used to them, so let's see how this goes!

--- src/cargo/lib.rs
+++ src/cargo/lib.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(test, deny(warnings))]
+#![warn(rust_2018_idioms)] // while we're getting used to 2018
 // Clippy isn't enforced by CI, and know that @alexcrichton isn't a fan :)
 #![allow(clippy::boxed_local)] // bug rust-lang-nursery/rust-clippy#1123
 #![allow(clippy::cyclomatic_complexity)] // large project
