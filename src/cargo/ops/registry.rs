46eca09ca3fd034eff6eebcff4996091004b201d
Katharina Fey <kookie@spacekookie.de>
2018-12-17 12:28:30 +0100

    Adding feature-flags to `cargo publish` and `cargo package`
    This change adds the `--features` and `--all-features` flags to the
    above mentioned commands. This is to allow publishing of crates that
    have certain feature requirements for compilation, without having to
    rely on `--no-verify` which isn't good practise.
    
    This PR adds two new fields `features` and `all_features` to both the
    `PackageOpts` and `PublishOpts` and also adds the argument options
    to the CLI commands.
    
    Merging this feature will allow people to publish crates on crates.io
    that require some feature flag to compile (or all?) without needing
    to rely on not checking the package before uploading, potentially
    resulting in less packaging errors and broken packages.

--- src/cargo/ops/registry.rs
+++ src/cargo/ops/registry.rs
@@ -39,6 +39,8 @@ pub struct PublishOpts<'cfg> {
     pub target: Option<String>,
     pub dry_run: bool,
     pub registry: Option<String>,
+    pub features: Vec<String>,
+    pub all_features: bool
 }
 
 pub fn publish(ws: &Workspace<'_>, opts: &PublishOpts<'_>) -> CargoResult<()> {
@@ -82,6 +84,9 @@ pub fn publish(ws: &Workspace<'_>, opts: &PublishOpts<'_>) -> CargoResult<()> {
             allow_dirty: opts.allow_dirty,
             target: opts.target.clone(),
             jobs: opts.jobs,
+            registry: opts.registry.clone(),
+            features: opts.features.clone(),
+            all_features: opts.all_features,
         },
     )?
     .unwrap();
