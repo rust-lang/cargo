8466e3dba7b6d9648fa2fc2c20a421460eae987f
Eric Huss <eric@huss.org>
2019-03-31 19:37:26 -0700

    Fix unused import warning.

--- tests/testsuite/death.rs
+++ tests/testsuite/death.rs
@@ -131,8 +131,6 @@ fn ctrl_c_kills_everyone() {
 
 #[cfg(unix)]
 fn ctrl_c(child: &mut Child) {
-    use libc;
-
     let r = unsafe { libc::kill(-(child.id() as i32), libc::SIGINT) };
     if r < 0 {
         panic!("failed to kill: {}", io::Error::last_os_error());
