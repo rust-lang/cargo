5d4c8c86e361718af4f8acd8daefdbb19943b58d
Philip Craig <philipjcraig@gmail.com>
2019-04-03 16:42:52 +1000

    Change example to automatically determine filename

--- examples/fix-json.rs
+++ examples/fix-json.rs
@@ -2,14 +2,14 @@ extern crate failure;
 extern crate rustfix;
 
 use failure::Error;
-use std::{env, fs, process, collections::HashSet};
+use std::{collections::HashMap, collections::HashSet, env, fs, process};
 
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
-    let (suggestions_file, source_file) = match args.as_slice() {
-        [_, suggestions_file, source_file] => (suggestions_file, source_file),
+    let suggestions_file = match args.as_slice() {
+        [_, suggestions_file] => suggestions_file,
         _ => {
-            println!("USAGE: fix-json <suggestions-file> <source-file>");
+            println!("USAGE: fix-json <suggestions-file>");
             process::exit(1);
         }
     };
@@ -21,11 +21,21 @@ fn main() -> Result<(), Error> {
         rustfix::Filter::Everything,
     )?;
 
-    let source = fs::read_to_string(&source_file)?;
+    let mut files = HashMap::new();
+    for suggestion in suggestions {
+        let file = suggestion.solutions[0].replacements[0]
+            .snippet
+            .file_name
+            .clone();
+        let entry = files.entry(file).or_insert(Vec::new());
+        entry.push(suggestion);
+    }
 
-    let fixes = rustfix::apply_suggestions(&source, &suggestions)?;
-
-    println!("{}", fixes);
+    for (source_file, suggestions) in &files {
+        let source = fs::read_to_string(&source_file)?;
+        let fixes = rustfix::apply_suggestions(&source, suggestions)?;
+        fs::write(&source_file, fixes)?;
+    }
 
     Ok(())
 }
