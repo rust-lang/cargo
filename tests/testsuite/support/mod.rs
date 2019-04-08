f9f339a2c8a7ff648c8130862d35457142fbed85
Eric Huss <eric@huss.org>
2019-04-08 10:05:45 -0700

    Fix mutable_borrow_reservation_conflict warning.

--- tests/testsuite/support/mod.rs
+++ tests/testsuite/support/mod.rs
@@ -791,7 +791,8 @@ impl Execs {
     pub fn cwd<T: AsRef<OsStr>>(&mut self, path: T) -> &mut Self {
         if let Some(ref mut p) = self.process_builder {
             if let Some(cwd) = p.get_cwd() {
-                p.cwd(cwd.join(path.as_ref()));
+                let new_path = cwd.join(path.as_ref());
+                p.cwd(new_path);
             } else {
                 p.cwd(path);
             }
