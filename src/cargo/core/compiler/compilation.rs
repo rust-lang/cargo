f8e261ecd30f735a46723dbf16f1df3168c77aee
Mike Hommey <mh@glandium.org>
2019-04-16 10:17:26 +0900

    Use env::var_os("HOME") rather than env::var("HOME")
    Because the value is used to create a Path, there's no reason to go all
    the way validating it is utf-8 (although in practice, it almost
    certainly is, but it doesn't even matter).

--- src/cargo/core/compiler/compilation.rs
+++ src/cargo/core/compiler/compilation.rs
@@ -183,7 +183,7 @@ impl<'cfg> Compilation<'cfg> {
             // These are the defaults when DYLD_FALLBACK_LIBRARY_PATH isn't
             // set. Since Cargo is explicitly setting the value, make sure the
             // defaults still work.
-            if let Ok(home) = env::var("HOME") {
+            if let Some(home) = env::var_os("HOME") {
                 search_path.push(PathBuf::from(home).join("lib"));
             }
             search_path.push(PathBuf::from("/usr/local/lib"));
