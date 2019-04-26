5dacbb17cf986bd6290ffce781ff8aeb173e1575
Aaron Hill <aa1ronham@gmail.com>
2019-04-25 23:24:32 -0400

    Relax assertion

--- src/cargo/core/dependency.rs
+++ src/cargo/core/dependency.rs
@@ -301,8 +301,10 @@ impl Dependency {
 
     /// Sets whether the dependency is public.
     pub fn set_public(&mut self, public: bool) -> &mut Dependency {
-        // Setting 'public' only makes sense for normal dependencies
-        assert_eq!(self.kind(), Kind::Normal);
+        if !public {
+            // Setting 'private' only makes sense for normal dependencies
+            assert_eq!(self.kind(), Kind::Normal);
+        }
         Rc::make_mut(&mut self.inner).public = public;
         self
     }
