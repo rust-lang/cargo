b79dbbc8e34cb61df881b6378b4c8b9d92ac5c3a
Eh2406 <YeomanYaacov@gmail.com>
2019-07-12 15:05:11 -0400

    Only have one copy of the graph. (borrow checker not happy)

--- src/cargo/util/graph.rs
+++ src/cargo/util/graph.rs
@@ -15,6 +15,7 @@ struct EdgeLink<E: Clone> {
 #[derive(Clone)]
 struct GraphCore<N: Clone, E: Clone> {
     edges: Vec<EdgeLink<E>>,
+    back_refs: Vec<(NonZeroUsize, usize)>,
     link_count: usize,
     nodes: indexmap::IndexMap<N, indexmap::IndexMap<N, (usize, Option<usize>)>>,
 }
@@ -32,6 +33,104 @@ impl<N: Clone, E: Clone> GraphCore<N, E> {
             len_edges: self.edges.len(),
             len_nodes: self.nodes.len(),
         }
+    }
+}
+
+impl<N: Clone + Eq + Hash, E: Clone + PartialEq> GraphCore<N, E> {
+    fn reset_to(&mut self, age: GraphAge) {
+        while self.nodes.len() > age.len_nodes {
+            self.nodes.pop();
+        }
+        for (_, lookup) in self.nodes.iter_mut() {
+            lookup.retain(|_, idx| {
+                if idx.1 >= age.len_edges {
+                    idx.1 = None;
+                }
+                *idx.0 <= age.link_count
+            });
+        }
+        while self
+            .back_refs
+            .last()
+            .filter(|(idx, _)| idx.get() >= age.len_edges)
+            .is_some()
+        {
+            let (_, idx) = self.back_refs.pop().unwrap();
+            self.edges[idx].next = None;
+        }
+        self.edges.truncate(age.len_edges);
+    }
+
+    pub fn add(&mut self, node: N) -> GraphAge {
+        self.nodes.entry(node).or_insert_with(IndexMap::new);
+        self.len()
+    }
+
+    pub fn link(&mut self, node: N, child: N) -> GraphAge {
+        use indexmap::map::Entry;
+        match self
+            .nodes
+            .entry(node.clone())
+            .or_insert_with(IndexMap::new)
+            .entry(child.clone())
+        {
+            Entry::Vacant(entry) => {
+                self.link_count += 1;
+                entry.insert((self.link_count, None));
+            }
+            Entry::Occupied(_) => {}
+        };
+        self.len()
+    }
+
+    pub fn add_edge(&mut self, node: N, child: N, edge: E) -> GraphAge {
+        use indexmap::map::Entry;
+        match self
+            .nodes
+            .entry(node.clone())
+            .or_insert_with(IndexMap::new)
+            .entry(child.clone())
+        {
+            Entry::Vacant(entry) => {
+                self.edges.push(EdgeLink {
+                    value: edge,
+                    next: None,
+                });
+                let edge_index = self.edges.len() - 1;
+                self.link_count += 1;
+                entry.insert((self.link_count, Some(edge_index)));
+            }
+            Entry::Occupied(mut entry) => {
+                let edge_index = *entry.get();
+                match edge_index {
+                    (link_count, None) => {
+                        self.edges.push(EdgeLink {
+                            value: edge,
+                            next: None,
+                        });
+                        let edge_index = self.edges.len() - 1;
+                        entry.insert((link_count, Some(edge_index)));
+                    }
+                    (_, Some(mut edge_index)) => loop {
+                        if inner.edges[edge_index].value == edge {
+                            return self.len();
+                        }
+                        if self.edges[edge_index].next.is_none() {
+                            self.edges.push(EdgeLink {
+                                value: edge,
+                                next: None,
+                            });
+                            let new_index = NonZeroUsize::new(self.edges.len() - 1).unwrap();
+                            self.edges[edge_index].next = Some(new_index);
+                            self.back_refs.push((new_index, edge_index));
+                            return self.len();
+                        }
+                        edge_index = self.edges[edge_index].next.unwrap().get();
+                    },
+                }
+            }
+        }
+        self.len()
     }
 }
 
@@ -72,6 +171,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
         Graph {
             inner: Rc::new(GraphCore {
                 edges: vec![],
+                back_refs: vec![],
                 link_count: 0,
                 nodes: Default::default(),
             }),
@@ -83,80 +183,37 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
         }
     }
 
-    pub fn add(&mut self, node: N) {
-        assert_eq!(self.age, self.inner.len());
+    fn borrow(&self) -> &GraphCore<N, E> {
+        let inner = &self.inner;
+        assert!(self.age.len_edges <= inner.len().len_edges);
+        assert!(self.age.len_nodes <= inner.len().len_nodes);
+        inner
+    }
+
+    fn activate(&mut self) -> &mut GraphCore<N, E> {
+        // if {
+        //     let inner = &self.inner;
+        //     self.age.len_edges >= inner.len().len_edges
+        //         || self.age.len_nodes >= inner.len().len_nodes
+        // } {
         let inner = Rc::make_mut(&mut self.inner);
-        inner.nodes.entry(node).or_insert_with(IndexMap::new);
-        self.age = inner.len();
+        inner.reset_to(self.age);
+        inner
+    }
+
+    pub fn add(&mut self, node: N) {
+        let age = self.activate().add(node);
+        self.age = age;
     }
 
     pub fn link(&mut self, node: N, child: N) {
-        use indexmap::map::Entry;
-        assert_eq!(self.age, self.inner.len());
-        let inner = Rc::make_mut(&mut self.inner);
-        match inner
-            .nodes
-            .entry(node.clone())
-            .or_insert_with(IndexMap::new)
-            .entry(child.clone())
-        {
-            Entry::Vacant(entry) => {
-                inner.link_count += 1;
-                entry.insert((inner.link_count, None));
-            }
-            Entry::Occupied(_) => {}
-        };
+        let age = self.activate().link(node, child);
+        self.age = age;
     }
 
     pub fn add_edge(&mut self, node: N, child: N, edge: E) {
-        use indexmap::map::Entry;
-        assert_eq!(self.age, self.inner.len());
-        let inner = Rc::make_mut(&mut self.inner);
-        match inner
-            .nodes
-            .entry(node.clone())
-            .or_insert_with(IndexMap::new)
-            .entry(child.clone())
-        {
-            Entry::Vacant(entry) => {
-                inner.edges.push(EdgeLink {
-                    value: edge,
-                    next: None,
-                });
-                let edge_index = inner.edges.len() - 1;
-                inner.link_count += 1;
-                entry.insert((inner.link_count, Some(edge_index)));
-            }
-            Entry::Occupied(mut entry) => {
-                let edge_index = *entry.get();
-                match edge_index {
-                    (link_count, None) => {
-                        inner.edges.push(EdgeLink {
-                            value: edge,
-                            next: None,
-                        });
-                        let edge_index = inner.edges.len() - 1;
-                        entry.insert((link_count, Some(edge_index)));
-                    }
-                    (_, Some(mut edge_index)) => loop {
-                        if inner.edges[edge_index].value == edge {
-                            return;
-                        }
-                        if inner.edges[edge_index].next.is_none() {
-                            inner.edges.push(EdgeLink {
-                                value: edge,
-                                next: None,
-                            });
-                            let new_index = NonZeroUsize::new(inner.edges.len() - 1).unwrap();
-                            inner.edges[edge_index].next = Some(new_index);
-                            return;
-                        }
-                        edge_index = inner.edges[edge_index].next.unwrap().get();
-                    },
-                }
-            }
-        }
-        self.age = inner.len();
+        let age = self.activate().add_edge(node, child, edge);
+        self.age = age;
     }
 
     pub fn contains<Q: ?Sized>(&self, k: &Q) -> bool
@@ -164,7 +221,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
         N: Borrow<Q>,
         Q: Hash + Eq,
     {
-        self.inner
+        self.borrow()
             .nodes
             .get_full(k)
             .filter(|(i, _, _)| *i < self.age.len_nodes)
@@ -173,9 +230,9 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
 
     pub fn edge(&self, from: &N, to: &N) -> Edges<'_, E> {
         Edges {
-            graph: &self.inner.edges[..self.age.len_edges],
+            graph: &self.borrow().edges[..self.age.len_edges],
             index: self
-                .inner
+                .borrow()
                 .nodes
                 .get(from)
                 .and_then(|x| x.get(to).copied())
@@ -185,8 +242,8 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
     }
 
     pub fn edges(&self, from: &N) -> impl Iterator<Item = (&N, Edges<'_, E>)> {
-        let edges = &self.inner.edges[..self.age.len_edges];
-        self.inner
+        let edges = &self.borrow().edges[..self.age.len_edges];
+        self.borrow()
             .nodes
             .get_full(from)
             .filter(|(i, _, _)| *i < self.age.len_nodes)
@@ -211,7 +268,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
         let mut ret = Vec::new();
         let mut marks = HashSet::new();
 
-        for node in self.inner.nodes.keys().take(self.age.len_nodes) {
+        for node in self.borrow().nodes.keys().take(self.age.len_nodes) {
             self.sort_inner_visit(node, &mut ret, &mut marks);
         }
 
@@ -223,7 +280,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
             return;
         }
 
-        if let Some(nodes) = self.inner.nodes.get(node) {
+        if let Some(nodes) = self.borrow().nodes.get(node) {
             for (child, (c, _)) in nodes.iter().rev() {
                 if *c <= self.age.link_count {
                     self.sort_inner_visit(child, dst, marks);
@@ -235,7 +292,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &N> {
-        self.inner.nodes.keys().take(self.age.len_nodes)
+        self.borrow().nodes.keys().take(self.age.len_nodes)
     }
 
     /// Checks if there is a path from `from` to `to`.
@@ -244,7 +301,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
         let mut seen = HashSet::new();
         seen.insert(from);
         while let Some((_, _, iter)) = stack.pop().and_then(|p| {
-            self.inner
+            self.borrow()
                 .nodes
                 .get_full(p)
                 .filter(|(i, _, _)| *i < self.age.len_nodes)
@@ -268,7 +325,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
     pub fn path_to_bottom<'a>(&'a self, mut pkg: &'a N) -> Vec<&'a N> {
         let mut result = vec![pkg];
         while let Some(p) = self
-            .inner
+            .borrow()
             .nodes
             .get_full(pkg)
             .filter(|(i, _, _)| *i < self.age.len_nodes)
@@ -295,7 +352,7 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> Graph<N, E> {
         // it's used for!
         let mut result = vec![pkg];
         let first_pkg_depending_on = |pkg: &N, res: &[&N]| {
-            self.inner
+            self.borrow()
                 .nodes
                 .iter()
                 .take(self.age.len_nodes)
@@ -330,7 +387,7 @@ impl<N: fmt::Display + Eq + Hash + Clone, E: Clone + fmt::Debug + PartialEq> fmt
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(fmt, "Graph {{")?;
 
-        for (from, e) in self.inner.nodes.iter().take(self.age.len_nodes) {
+        for (from, e) in self.borrow().nodes.iter().take(self.age.len_nodes) {
             writeln!(fmt, "  - {}", from)?;
 
             for (to, (c, _)) in e.iter() {
@@ -351,7 +408,7 @@ impl<N: fmt::Display + Eq + Hash + Clone, E: Clone + fmt::Debug + PartialEq> fmt
 
 impl<N: Eq + Hash + Clone, E: Eq + Clone + PartialEq> PartialEq for Graph<N, E> {
     fn eq(&self, other: &Graph<N, E>) -> bool {
-        self.inner.nodes.eq(&other.inner.nodes)
+        self.borrow().nodes.eq(&other.borrow().nodes)
     }
 }
 impl<N: Eq + Hash + Clone, E: Eq + Clone> Eq for Graph<N, E> {}
