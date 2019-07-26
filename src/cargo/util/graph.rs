af7e6b6b1bbdf775be1818115bad9dbff94d2a55
Eh2406 <YeomanYaacov@gmail.com>
2019-07-26 14:13:38 -0400

    document why `reset_to` works

--- src/cargo/util/graph.rs
+++ src/cargo/util/graph.rs
@@ -31,10 +31,15 @@ pub struct Graph<N: Clone, E: Clone> {
     nodes: indexmap::IndexMap<N, indexmap::IndexMap<N, EdgeIndex>>,
 }
 
-/// All the data needed to query the prefix of a `Graph`
+/// All the data needed to query the prefix of a `Graph`. The only way for eny of the `len_` in
+/// this to decrease is to call `reset_to`. All other modifications of a graph will increase at
+/// least one of the `len_`.
 #[derive(Copy, Clone, PartialEq, Debug)]
 struct GraphAge {
+    /// The number of stored edges, increased when `add_edge` or `link` is called.
     len_edges: usize,
+    /// The number of stored nodes, increased when `add` is called or
+    /// if `add_edge` or `link` is called with a previously unseen node.
     len_nodes: usize,
 }
 
@@ -55,15 +60,20 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
         }
     }
 
+    /// This resets a Graph to the same state as it was when the passed `age` was made.
+    /// All the other `&mut` methods are guaranteed to increase the `len` of the Graph.
+    /// So the reset can be accomplished by throwing out all newer items and fixing internal pointers.
     fn reset_to(&mut self, age: GraphAge) {
         // the prefix we are resetting to had `age.len_nodes`, so remove all newer nodes
         assert!(self.nodes.len() >= age.len_nodes);
+        // IndexMap dose not have a `truncate` so we roll our own
         while self.nodes.len() > age.len_nodes {
             self.nodes.pop();
         }
 
         // the prefix we are resetting to had `age.len_edges`, so remove all links pointing to newer edges
         for (_, lookup) in self.nodes.iter_mut() {
+            // IndexMap dose not have a `last` so we roll our own
             while lookup.len() >= 1
                 && lookup
                     .get_index(lookup.len() - 1)
@@ -76,7 +86,7 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
 
         // the prefix we are resetting to had `age.len_edges`, so
         assert!(self.edges.len() >= age.len_edges);
-        // remove all newer edges
+        // remove all newer edges and record the references that need to be fixed
         let to_fix: Vec<EdgeIndex> = self
             .edges
             .drain(age.len_edges..)
@@ -84,7 +94,7 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
             .filter(|idx| idx <= &age.len_edges)
             .collect();
 
-        // fix references into the newer edges we are about to remove
+        // fix references into the newer edges we remove
         for idx in to_fix {
             self.edges[idx].next = None;
         }
@@ -93,15 +103,15 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
 
     pub fn add(&mut self, node: N) {
         // IndexMap happens to do exactly what we need to keep the ordering correct.
+        // This can be undone as it will increase the `len_nodes` if `node` is new.
         self.nodes.entry(node).or_insert_with(IndexMap::new);
     }
 
     /// connect `node`to `child` with out associating any data.
-    /// Note that if this and `add_edge` are used on the same graph
-    ///      odd things may happen when `reset_to` is called.
     pub fn link(&mut self, node: N, child: N) {
         use indexmap::map::Entry;
         // IndexMap happens to do exactly what we need to keep the ordering correct.
+        // This can be undone as it will increase the `len_nodes` if `node` is new.
         match self
             .nodes
             .entry(node)
@@ -109,7 +119,8 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
             .entry(child)
         {
             Entry::Vacant(entry) => {
-                // add the new edge, and link and fix the new link count
+                // add the new edge and link
+                // This can be undone as it will increase the `len_edges`.
                 self.edges.push(EdgeLink {
                     value: None,
                     next: None,
@@ -129,6 +140,7 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
         use indexmap::map::Entry;
         let edge = Some(edge);
         // IndexMap happens to do exactly what we need to keep the ordering correct.
+        // This can be undone as it will increase the `len_nodes` if `node` is new.
         match self
             .nodes
             .entry(node)
@@ -136,7 +148,8 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
             .entry(child)
         {
             Entry::Vacant(entry) => {
-                // add the new edge, and link and fix the new link count
+                // add the new edge and link
+                // This can be undone as it will increase the `len_edges`.
                 self.edges.push(EdgeLink {
                     value: edge,
                     next: None,
@@ -155,6 +168,7 @@ impl<N: Clone + Eq + Hash, E: Clone + PartialEq> Graph<N, E> {
                     }
                     if self.edges[edge_index].next.is_none() {
                         // we found the end, add the new edge
+                        // This can be undone as it will increase the `len_edges`.
                         self.edges.push(EdgeLink {
                             value: edge,
                             next: None,
@@ -335,7 +349,7 @@ impl<N: Eq + Hash + Clone, E: Eq + Clone> Eq for Graph<N, E> {}
 ///  - It can only be modified using a strict "Stack Discipline", only modifying the biggest clone
 ///    of the graph.
 ///  - You can drop bigger modified clones, to allow a smaller clone to be activated for modifying.
-///    this "backtracking" operation can be `O(n)`
+///    this "backtracking" operation can be `O(delta)`
 ///
 /// The "Stack Discipline" is checked on best effort basis. If you attempt to read or write to a
 /// bigger clone after modifying a smaller clone it will probably panic. It may not panic if you
@@ -403,7 +417,12 @@ impl<N: Eq + Hash + Clone, E: Clone + PartialEq> StackGraph<N, E> {
             g.len()
         };
     }
+}
 
+/// Methods for viewing the prefix of a `Graph` as stored in a `StackGraph`.
+/// Other views of the inner `Graph` may have added things after this `StackGraph` was created.
+/// So, we need to filter everything to only the prefix recorded by `self.age`.
+impl<N: Eq + Hash + Clone, E: Clone + PartialEq> StackGraph<N, E> {
     pub fn contains<Q: ?Sized>(&self, k: &Q) -> bool
     where
         N: Borrow<Q>,
@@ -475,8 +494,12 @@ impl<N: fmt::Display + Eq + Hash + Clone, E: Clone + fmt::Debug + PartialEq> fmt
     }
 }
 
+/// And Iterator of the edges related to one link in a `Graph` or a `StackGraphView`.
 #[derive(Clone, Debug)]
 pub struct Edges<'a, E: Clone> {
+    /// The arena the linked list is stored in. If we are used with a `StackGraphView`
+    /// then this is only the related prefix of the arena. So we need to filter out
+    /// pointers past the part we are given.
     graph: &'a [EdgeLink<E>],
     index: Option<EdgeIndex>,
 }
@@ -509,6 +532,9 @@ impl<'a, E: Clone> Iterator for Edges<'a, E> {
     }
 }
 
+/// A RAII gard that allows getting `&` references to the prefix of a `Graph` as stored in a `StackGraph`.
+/// Other views of the inner `Graph` may have added things after this `StackGraph` was created.
+/// So, we need to filter everything to only the prefix recorded by `self.age`.
 pub struct StackGraphView<'a, N: Clone, E: Clone> {
     inner: Ref<'a, Graph<N, E>>,
     age: GraphAge,
