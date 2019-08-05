45699e9f21fc9da4fe34b5d27c08fb3c3a6fb839
Alex Crichton <alex@alexcrichton.com>
2019-08-05 09:42:28 -0700

    Add support for customizing JSON diagnostics from Cargo
    Cargo has of #7143 enabled pipelined compilation by default which
    affects how the compiler is invoked, especially with respect to JSON
    messages. This, in some testing, has proven to cause quite a few issues
    with rustbuild's current integration with Cargo. This commit is aimed at
    adding features to Cargo to solve this issue.
    
    This commit adds the ability to customize the stream of JSON messages
    coming from Cargo. The new feature for Cargo is that it now also mirrors
    rustc in how you can configure the JSON stream. Multiple
    `--message-format` arguments are now supported and the value specified
    is a comma-separated list of directives. In addition to the existing
    `human`, `short`, and `json` directives these new directives have been
    added:
    
    * `json-render-diagnostics` - instructs Cargo to render rustc
      diagnostics and only print out JSON messages for artifacts and Cargo
      things.
    
    * `json-diagnostic-short` - indicates that the `rendered` field of rustc
      diagnostics should use the "short" rendering.
    
    * `json-diagnostic-rendered-ansi` - indicates that the `rendered` field of rustc
      diagnostics should embed ansi color codes.
    
    The first option here, `json-render-diagnostics`, will be used by
    rustbuild unconditionally. Additionally `json-diagnostic-short` will be
    conditionally used based on the input to rustbuild itself.
    
    This should be enough for external tools to customize how Cargo is
    invoked and how all kinds of JSON diagnostics get printed, and it's
    thought that we can relatively easily tweak this as necessary to extend
    it and such.

--- src/cargo/core/compiler/build_config.rs
+++ src/cargo/core/compiler/build_config.rs
@@ -112,7 +112,10 @@ impl BuildConfig {
     /// Whether or not the *user* wants JSON output. Whether or not rustc
     /// actually uses JSON is decided in `add_error_format`.
     pub fn emit_json(&self) -> bool {
-        self.message_format == MessageFormat::Json
+        match self.message_format {
+            MessageFormat::Json { .. } => true,
+            _ => false,
+        }
     }
 
     pub fn test(&self) -> bool {
@@ -123,7 +126,17 @@ impl BuildConfig {
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum MessageFormat {
     Human,
-    Json,
+    Json {
+        /// Whether rustc diagnostics are rendered by cargo or included into the
+        /// output stream.
+        render_diagnostics: bool,
+        /// Whether the `rendered` field of rustc diagnostics are using the
+        /// "short" rendering.
+        short: bool,
+        /// Whether the `rendered` field of rustc diagnostics embed ansi color
+        /// codes.
+        ansi: bool,
+    },
     Short,
 }
 
