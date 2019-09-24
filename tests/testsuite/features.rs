rust-lang/cargo

commit 51c26b6776bc059da97433a98cef786f88bdbafb

Add support for multiple --features options

src/cargo/util/command_prelude.rs
@@ -100,7 +100,9 @@ pub trait AppExt: Sized {
 
     fn arg_features(self) -> Self {
         self._arg(
-            opt("features", "Space-separated list of features to activate").value_name("FEATURES"),
+            opt("features", "Space-separated list of features to activate")
+                .multiple(true)
+                .value_name("FEATURES"),
         )
         ._arg(opt("all-features", "Activate all available features"))
         ._arg(opt(

tests/testsuite/features.rs
@@ -1911,3 +1911,60 @@ fn no_feature_for_non_optional_dep() {
 
     p.cargo("build --features bar/a").run();
 }
+
+#[cargo_test]
+fn features_option_given_twice() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [features]
+                a = []
+                b = []
+             "#,
+        )
+        .file(
+            "src/main.rs",
+            r#"
+                #[cfg(all(feature = "a", feature = "b"))]
+                fn main() {}
+            "#,
+        )
+        .build();
+
+    p.cargo("build --features a --features b").run();
+}
+
+#[cargo_test]
+fn multi_multi_features() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [features]
+                a = []
+                b = []
+                c = []
+            "#,
+        )
+        .file(
+            "src/main.rs",
+            r#"
+               #[cfg(all(feature = "a", feature = "b", feature = "c"))]
+               fn main() {}
+            "#,
+        )
+        .build();
+
+    p.cargo("build --features a --features").arg("b c").run();
+}

commit 1fa02e77b6c3c2cf03a4ff39c57e6beed171e49e

Fix interpretation of `--features a b` on the CLI
Fixes an accidental regression from #7084 where `--features a b` was
erroneously mistinterpreted as `--features "a b"`.

Closes #7418

src/cargo/util/command_prelude.rs
@@ -100,11 +100,11 @@ pub trait AppExt: Sized {
     }
 
     fn arg_features(self) -> Self {
-        self._arg(
-            opt("features", "Space-separated list of features to activate")
-                .multiple(true)
-                .value_name("FEATURES"),
-        )
+        self._arg(multi_opt(
+            "features",
+            "FEATURES",
+            "Space-separated list of features to activate",
+        ))
         ._arg(opt("all-features", "Activate all available features"))
         ._arg(opt(
             "no-default-features",

tests/testsuite/features.rs
@@ -1959,3 +1959,32 @@ fn multi_multi_features() {
 
     p.cargo("build --features a --features").arg("b c").run();
 }
+
+#[cargo_test]
+fn cli_parse_ok() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [features]
+                a = []
+            "#,
+        )
+        .file(
+            "src/main.rs",
+            r#"
+               #[cfg(feature = "a")]
+               fn main() {
+                    assert_eq!(std::env::args().nth(1).unwrap(), "b");
+               }
+            "#,
+        )
+        .build();
+
+    p.cargo("run --features a b").run();
+}