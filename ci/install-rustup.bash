38c2d2b4d7e922826460be3178922bba96912601
Lzu Tao <taolzu@gmail.com>
2020-01-02 15:11:17 +0700

    actions: Install rustup only (and not install toolchain)

--- ci/install-rustup.bash
+++ ci/install-rustup.bash
@@ -6,11 +6,6 @@
 # "Device or resource busy".
 
 set -e
-if [[ -z "$1" ]]; then
-    echo "First parameter must be toolchain to install."
-    exit 1
-fi
-TOOLCHAIN="$1"
 
 # Install/update rustup.
 RUSTUP_MINOR_VER=$(rustup -V 2> /dev/null | grep -o -E '1\.[0-9]{2}' | cut -d . -f2)
@@ -18,6 +13,6 @@ if [[ -n $RUSTUP_MINOR_VER && $RUSTUP_MINOR_VER -ge 20 ]]; then
     echo "$(rustup -V)" already installed
     rustup set profile minimal
 else
-    curl -sSL https://sh.rustup.rs | sh -s -- -y --default-toolchain="$TOOLCHAIN" --profile=minimal
+    curl -sSL https://sh.rustup.rs | sh -s -- -y --default-toolchain=none --profile=minimal
     echo "##[add-path]$HOME/.cargo/bin"
 fi
