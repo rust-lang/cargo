3dbac42a57ae8914a9fec765276acac33360305d
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-08-11 17:46:47 -0400

    Renamed SourceIdIntoUrl -> SourceIdAsUrl

--- src/cargo/core/source/source_id.rs
+++ src/cargo/core/source/source_id.rs
@@ -144,8 +144,8 @@ impl SourceId {
     }
 
     /// A view of the `SourceId` that can be `Display`ed as a URL.
-    pub fn as_url(&self) -> SourceIdIntoUrl<'_> {
-        SourceIdIntoUrl {
+    pub fn as_url(&self) -> SourceIdAsUrl<'_> {
+        SourceIdAsUrl {
             inner: &*self.inner,
         }
     }
@@ -545,11 +545,11 @@ impl Hash for SourceId {
 }
 
 /// A `Display`able view into a `SourceId` that will write it as a url
-pub struct SourceIdIntoUrl<'a> {
+pub struct SourceIdAsUrl<'a> {
     inner: &'a SourceIdInner,
 }
 
-impl<'a> fmt::Display for SourceIdIntoUrl<'a> {
+impl<'a> fmt::Display for SourceIdAsUrl<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self.inner {
             SourceIdInner {
