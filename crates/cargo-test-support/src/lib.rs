75822354aea01344d26186c58bbe0308e2dba672
Joshua Nelson <jyn514@gmail.com>
2020-11-21 22:05:36 -0500

    Count RUSTC_BOOTSTRAP as nightly
    Rustdoc will count this as nightly, so cargo should too. This allows
    running nightly-only tests without having to rebuild all 175
    dependencies.

--- crates/cargo-test-support/src/lib.rs
+++ crates/cargo-test-support/src/lib.rs
@@ -1571,8 +1571,11 @@ pub fn rustc_host() -> String {
 
 pub fn is_nightly() -> bool {
     env::var("CARGO_TEST_DISABLE_NIGHTLY").is_err()
-        && RUSTC
-            .with(|r| r.verbose_version.contains("-nightly") || r.verbose_version.contains("-dev"))
+        && (RUSTC.with(|r| {
+            r.verbose_version.contains("-nightly")
+                || r.verbose_version.contains("-dev")
+                || env::var("RUSTC_BOOTSTRAP").as_deref() == Ok("1")
+        }))
 }
 
 pub fn process<T: AsRef<OsStr>>(t: T) -> cargo::util::ProcessBuilder {
