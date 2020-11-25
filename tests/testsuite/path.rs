5799fc8a1ae9d205c818222613bd8ebeb9bf841e
Jon Gjengset <jongje@amazon.com>
2020-11-25 11:54:50 -0800

    Fix up expected output in path test

--- tests/testsuite/path.rs
+++ tests/testsuite/path.rs
@@ -1044,12 +1044,12 @@ fn deep_path_error() {
         .with_status(101)
         .with_stderr(
             "\
-[ERROR] failed to get `c` as a dependency of package `b v0.1.0 [..]`
+[ERROR] failed to prefetch dependencies of package `b v0.1.0 [..]`
     ... which is depended on by `a v0.1.0 [..]`
     ... which is depended on by `foo v0.1.0 [..]`
 
 Caused by:
-  failed to load source for dependency `c`
+  failed to load source for dependency `c` during prefetching
 
 Caused by:
   Unable to update [..]/foo/c
