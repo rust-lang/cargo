fd25f93693f7321f691e9a82e626cab2a21ebf47
Jon Gjengset <jongje@amazon.com>
2020-12-18 14:53:29 -0800

    Oops

--- tests/testsuite/metadata.rs
+++ tests/testsuite/metadata.rs
@@ -1984,7 +1984,7 @@ fn deps_with_bin_only() {
                       "rename": null,
                       "optional": false,
                       "uses_default_features": true,
-                      "manifest_path": "[..]/bdep",
+                      "path": "[..]/foo/bdep",
                       "features": [],
                       "target": null,
                       "registry": null
