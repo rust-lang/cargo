97a821d4f2beb8b3e36e29fa5d2add34e843598a
Chris Field <chris@fieldrndservices.com>
2021-01-08 13:28:17 -0500

    Change JSON format
    The JSON format does not parse or group or drop prefixes for
    configurations. For each compiler target (host, etc.), the
    configurations are just strings in an array. The targets is changed to
    an array of maps as well.

--- src/cargo/core/compiler/rustc_cfg.rs
+++ src/cargo/core/compiler/rustc_cfg.rs
@@ -10,39 +10,52 @@ const VERSION: u32 = 1;
 
 // {
 //     "version": 1,
-//     "host": {
-//         "names": ["windows", "debug_assertions"],
-//         "arch":"x86_64",
-//         "endian":"little",
-//         "env":"msvc",
-//         "family":"windows",
-//         "features":["fxsr","sse","sse2"],
-//         "os":"windows",
-//         "pointer_width":"64",
-//         "vendor":"pc"
-//     },
-//     "targets": {
-//         "x86_64-unknown-linux-gnu": {
-//             "names": ["debug_assertions", "unix"],
-//             "arch":"x86_64",
-//             "endian":"little",
-//             "env":"gnu",
-//             "family":"unix",
-//             "features": ["fxsr","sse","sse2"],
-//             "os":"linux",
-//             "pointer_width":"64",
-//             "vendor":"unknown"
+//     "host": [
+//         "windows",
+//         "debug_assertions",
+//         "target_arch='x86_64'",
+//         "target_endian='little'",
+//         "target_env='msvc'",
+//         "target_family='windows'",
+//         "target_feature='fxsr'",
+//         "target_feature='sse'",
+//         "target_feature='sse2'",
+//         "target_os='windows'",
+//         "target_pointer_width='64'",
+//         "target_vendor='pc'"
+//     ],
+//     "targets": [
+//         {
+//             "x86_64-unknown-linux-gnu": [
+//                 "unix",
+//                 "debug_assertions",
+//                 "target_arch='x86_64'",
+//                 "target_endian='little'",
+//                 "target_env='gnu'",
+//                 "target_family='unix'",
+//                 "target_feature='fxsr'",
+//                 "target_feature='sse'",
+//                 "target_feature='sse2'",
+//                 "target_os='linux'",
+//                 "target_pointer_width='64'",
+//                 "target_vendor='unknown'"
+//             ]
 //         },
-//         "i686-pc-windows-msvc": {
-//             "names": ["windows", "debug_assertions"],
-//             "arch":"x86",
-//             "endian":"little",
-//             "env":"msvc",
-//             "family":"windows",
-//             "features":["fxsr","sse","sse2"],
-//             "os":"windows",
-//             "pointer_width":"32",
-//             "vendor":"pc"
+//         {
+//             "i686-pc-windows-msvc": [
+//                 "windows",
+//                 "debug_assertions",
+//                 "target_arch='x86'",
+//                 "target_endian='little'",
+//                 "target_env='msvc'",
+//                 "target_family='windows'",
+//                 "target_feature='fxsr'",
+//                 "target_feature='sse'",
+//                 "target_feature='sse2'",
+//                 "target_os='windows'",
+//                 "target_pointer_width='32'",
+//                 "target_vendor='pc'"
+//             ]
 //         }
 //     }
 // }
@@ -50,81 +63,41 @@ const VERSION: u32 = 1;
 #[derive(serde::Serialize)]
 struct SerializedRustcCfg<'a> {
     version: u32,
-    host: SerializedCfg<'a>,
-    targets: HashMap<&'a str, SerializedCfg<'a>>,
+    host: SerializedTargetData<'a>,
+    targets: Vec<HashMap<&'a str, SerializedTargetData<'a>>>,
 }
 
 #[derive(serde::Serialize)]
-struct SerializedCfg<'a> {
-    arch: Option<&'a str>,
-    endian: Option<&'a str>,
-    env: Option<&'a str>,
-    family: Option<&'a str>,
-    features: Vec<&'a str>,
-    names: Vec<&'a str>,
-    os: Option<&'a str>,
-    pointer_width: Option<&'a str>,
-    vendor: Option<&'a str>,
+struct SerializedTargetData<'a> {
+    cfgs: Vec<&'a str>,
 }
 
-impl<'a> SerializedCfg<'a> {
+impl<'a> SerializedTargetData<'a> {
     fn new(rtd: &'a RustcTargetData, kind: CompileKind) -> Self {
-        let features = rtd
-            .cfg(kind)
-            .iter()
-            .filter_map(|c| match c {
-                Cfg::Name(..) => None,
-                Cfg::KeyPair(k, v) => {
-                    if k == "target_feature" {
-                        Some(v.as_str())
-                    } else {
-                        None
-                    }
-                }
-            })
-            .collect();
-        let names = rtd
-            .cfg(kind)
-            .iter()
-            .filter_map(|c| match c {
-                Cfg::Name(s) => Some(s.as_str()),
-                Cfg::KeyPair(..) => None,
-            })
-            .collect();
         Self {
-            arch: Self::find(rtd, kind, "target_arch"),
-            endian: Self::find(rtd, kind, "target_endian"),
-            env: Self::find(rtd, kind, "target_env"),
-            family: Self::find(rtd, kind, "target_family"),
-            features,
-            names,
-            os: Self::find(rtd, kind, "target_os"),
-            pointer_width: Self::find(rtd, kind, "target_pointer_width"),
-            vendor: Self::find(rtd, kind, "target_vendor"),
+            cfgs: rtd
+                .cfg(kind)
+                .iter()
+                .map(|c| match c {
+                    Cfg::Name(n) => n.as_str(),
+                    Cfg::KeyPair(k, v) => format!("{}='{}'", k, v).as_str()
+                })
+                .collect()
         }
-    }
-
-    fn find(rtd: &'a RustcTargetData, kind: CompileKind, key: &str) -> Option<&'a str> {
-        rtd.cfg(kind).iter().find_map(|c| match c {
-            Cfg::Name(..) => None,
-            Cfg::KeyPair(k, v) => {
-                if k == key {
-                    Some(v.as_str())
-                } else {
-                    None
-                }
-            }
-        })
     }
 }
 
 pub fn emit_serialized_rustc_cfg(rtd: &RustcTargetData, kinds: &[CompileKind]) -> CargoResult<()> {
-    let host = SerializedCfg::new(rtd, CompileKind::Host);
+    let host = SerializedTargetData::new(rtd, CompileKind::Host);
     let targets = kinds
         .iter()
         .filter_map(|k| match k {
             CompileKind::Host => None,
-            CompileKind::Target(ct) => Some((ct.short_name(), SerializedCfg::new(rtd, *k))),
+            CompileKind::Target(ct) => {
+                let mut target = HashMap::with_capacity(1);
+                target.insert(ct.short_name(), SerializedTargetData::new(rtd, *k));
+                Some(target)
+            },
         })
         .collect();
     let s = SerializedRustcCfg {
