6977dee10961fb6701c6cf8a083d5c0ca5c779f7
Kornel <kornel@geekhood.net>
2021-03-02 18:13:08 +0000

    Explain `cargo install` is not for libraries

--- src/cargo/ops/cargo_install.rs
+++ src/cargo/ops/cargo_install.rs
@@ -305,7 +305,12 @@ fn install_one(
     // *something* to install. Explicit `--bin` or `--example` flags will be
     // checked at the start of `compile_ws`.
     if !opts.filter.is_specific() && !pkg.targets().iter().any(|t| t.is_bin()) {
-        bail!("specified package `{}` has no binaries", pkg);
+        bail!(
+            "there is nothing to install in `{}`, because it has no binaries\n\
+             `cargo install` is only for installing programs, and can't be used with libraries.\n\
+             To use a library crate, add it as a dependency in a Cargo project instead.",
+            pkg
+        );
     }
 
     // Helper for --no-track flag to make sure it doesn't overwrite anything.
