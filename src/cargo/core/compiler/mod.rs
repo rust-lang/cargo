48e0306c42752ca97c3488ad93b56b9831ca3465
Eric Huss <eric@huss.org>
2021-03-10 21:33:48 -0800

    Fix logic for determining prefer-dynamic for a dylib.

--- src/cargo/core/compiler/mod.rs
+++ src/cargo/core/compiler/mod.rs
@@ -792,7 +792,7 @@ fn build_base_args(
     }
 
     let prefer_dynamic = (unit.target.for_host() && !unit.target.is_custom_build())
-        || (crate_types.contains(&CrateType::Dylib) && bcx.ws.members().any(|p| *p != unit.pkg));
+        || (crate_types.contains(&CrateType::Dylib) && !cx.is_primary_package(unit));
     if prefer_dynamic {
         cmd.arg("-C").arg("prefer-dynamic");
     }
