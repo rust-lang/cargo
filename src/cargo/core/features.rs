b9272c5a4e4c5db76071bd733d567f3d5f237076
PicoJr <picojr_dev@gmx.com>
2021-04-19 21:16:05 +0200

    Update src/cargo/core/features.rs
    Co-authored-by: Eric Huss <eric@huss.org>

--- src/cargo/core/features.rs
+++ src/cargo/core/features.rs
@@ -594,7 +594,7 @@ cli_options!(
     pub enable_future_incompat_feature: bool = ("Enable creation of a future-incompat report for all dependencies"),
     pub extra_link_arg: bool = ("Allow `cargo:rustc-link-arg` in build scripts"),
     pub features: Option<Vec<String>>  = (HIDDEN),
-    pub jobserver_per_rustc: bool = ("TODO"),
+    pub jobserver_per_rustc: bool = (HIDDEN),
     pub minimal_versions: bool = ("Install minimal dependency versions instead of maximum"),
     pub mtime_on_use: bool = ("Configure Cargo to update the mtime of used files"),
     pub multitarget: bool = ("Allow passing multiple `--target` flags to the cargo subcommand selected"),
