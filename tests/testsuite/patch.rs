a846226f7180253bf0446463aa4ea8fe989e8e24
hi-rustin <rustin.liu@gmail.com>
2021-07-09 13:15:13 +0800

    Remove unrelated cases
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tests/testsuite/patch.rs
+++ tests/testsuite/patch.rs
@@ -795,37 +795,6 @@ fn add_patch_with_features() {
         .file(
             "Cargo.toml",
             r#"
-                [package]
-                name = "foo"
-                version = "0.0.1"
-                authors = []
-
-                [dependencies]
-                bar = "0.1.0"
-            "#,
-        )
-        .file("src/lib.rs", "")
-        .file("bar/Cargo.toml", &basic_manifest("bar", "0.1.0"))
-        .file("bar/src/lib.rs", r#""#)
-        .build();
-
-    p.cargo("build")
-        .with_stderr(
-            "\
-[UPDATING] `[ROOT][..]` index
-[DOWNLOADING] crates ...
-[DOWNLOADED] bar v0.1.0 [..]
-[COMPILING] bar v0.1.0
-[COMPILING] foo v0.0.1 ([CWD])
-[FINISHED] dev [unoptimized + debuginfo] target(s) in [..]
-",
-        )
-        .run();
-    p.cargo("build").with_stderr("[FINISHED] [..]").run();
-
-    p.change_file(
-        "Cargo.toml",
-        r#"
             [package]
             name = "foo"
             version = "0.0.1"
@@ -837,13 +806,18 @@ fn add_patch_with_features() {
             [patch.crates-io]
             bar = { path = 'bar', features = ["some_feature"] }
         "#,
-    );
+        )
+        .file("src/lib.rs", "")
+        .file("bar/Cargo.toml", &basic_manifest("bar", "0.1.0"))
+        .file("bar/src/lib.rs", r#""#)
+        .build();
 
     p.cargo("build")
         .with_stderr(
             "\
 [WARNING] patch for `bar` uses the features mechanism. \
 default-features and features will not take effect because the patch dependency does not support this mechanism
+[UPDATING] `[ROOT][..]` index
 [COMPILING] bar v0.1.0 ([CWD]/bar)
 [COMPILING] foo v0.0.1 ([CWD])
 [FINISHED] dev [unoptimized + debuginfo] target(s) in [..]
@@ -869,37 +843,6 @@ fn add_patch_with_setting_default_features() {
         .file(
             "Cargo.toml",
             r#"
-                [package]
-                name = "foo"
-                version = "0.0.1"
-                authors = []
-
-                [dependencies]
-                bar = "0.1.0"
-            "#,
-        )
-        .file("src/lib.rs", "")
-        .file("bar/Cargo.toml", &basic_manifest("bar", "0.1.0"))
-        .file("bar/src/lib.rs", r#""#)
-        .build();
-
-    p.cargo("build")
-        .with_stderr(
-            "\
-[UPDATING] `[ROOT][..]` index
-[DOWNLOADING] crates ...
-[DOWNLOADED] bar v0.1.0 [..]
-[COMPILING] bar v0.1.0
-[COMPILING] foo v0.0.1 ([CWD])
-[FINISHED] dev [unoptimized + debuginfo] target(s) in [..]
-",
-        )
-        .run();
-    p.cargo("build").with_stderr("[FINISHED] [..]").run();
-
-    p.change_file(
-        "Cargo.toml",
-        r#"
             [package]
             name = "foo"
             version = "0.0.1"
@@ -911,13 +854,18 @@ fn add_patch_with_setting_default_features() {
             [patch.crates-io]
             bar = { path = 'bar', default-features = false, features = ["none_default_feature"] }
         "#,
-    );
+        )
+        .file("src/lib.rs", "")
+        .file("bar/Cargo.toml", &basic_manifest("bar", "0.1.0"))
+        .file("bar/src/lib.rs", r#""#)
+        .build();
 
     p.cargo("build")
         .with_stderr(
             "\
 [WARNING] patch for `bar` uses the features mechanism. \
 default-features and features will not take effect because the patch dependency does not support this mechanism
+[UPDATING] `[ROOT][..]` index
 [COMPILING] bar v0.1.0 ([CWD]/bar)
 [COMPILING] foo v0.0.1 ([CWD])
 [FINISHED] dev [unoptimized + debuginfo] target(s) in [..]
