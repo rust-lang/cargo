6b0e2f742319254007a06580e9ce9530a2d944d4
QiangHeisenberg <lziqiang1@gmail.com>
2021-08-19 19:23:58 +0800

    Add cargo doc --examples subcommand

--- src/bin/cargo/commands/doc.rs
+++ src/bin/cargo/commands/doc.rs
@@ -12,6 +12,10 @@ pub fn cli() -> App {
             "open",
             "Opens the docs in a browser after the operation",
         ))
+		.arg(opt(
+            "examples",
+            "Document all examples",
+        ))
         .arg_package_spec(
             "Package to document",
             "Document all packages in the workspace",
