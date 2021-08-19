51d48e96c2e2165bd514930a2db6cc3427f2e5be
l00556901 <liziqiang5@huawei.com>
2021-08-19 19:53:28 +0800

    fmt

--- src/bin/cargo/commands/doc.rs
+++ src/bin/cargo/commands/doc.rs
@@ -12,10 +12,7 @@ pub fn cli() -> App {
             "open",
             "Opens the docs in a browser after the operation",
         ))
-	.arg(opt(
-            "examples",
-            "Document all examples",
-        ))
+        .arg(opt("examples", "Document all examples"))
         .arg_package_spec(
             "Package to document",
             "Document all packages in the workspace",
