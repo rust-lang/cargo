8c3b1fcd9f5ef00a8a1275acd128b3c25384dbe0
dswij <dswijj@gmail.com>
2021-09-30 13:50:37 +0800

    Add tests for retry option values in network config
    This commit adds tests for DurationString type deserializer which is
    used in network config.

--- tests/testsuite/net_config.rs
+++ tests/testsuite/net_config.rs
@@ -109,3 +109,137 @@ fn net_retry_backoff_time() {
         .env("CARGO_LOG", "DEBUG")
         .run();
 }
+
+#[cargo_test]
+fn net_retry_config_bad_unit() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [dependencies.bar]
+                git = "http://127.0.0.1:11/foo/bar"
+            "#,
+        )
+        .file(
+            ".cargo/config",
+            r#"
+           [net]
+           retry-delay="10m"
+           [http]
+           timeout=1
+            "#,
+        )
+        .file("src/main.rs", "")
+        .build();
+
+    p.cargo("build -v -j 1")
+        .with_status(101)
+        .with_stderr_contains("[..] unknown variant `m`, expected `s` or `ms`")
+        .run();
+}
+
+#[cargo_test]
+fn net_retry_config_bad_format() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [dependencies.bar]
+                git = "http://127.0.0.1:11/foo/bar"
+            "#,
+        )
+        .file(
+            ".cargo/config",
+            r#"
+           [net]
+           retry-delay="10m1"
+           [http]
+           timeout=1
+            "#,
+        )
+        .file("src/main.rs", "")
+        .build();
+
+    p.cargo("build -v -j 1")
+        .with_status(101)
+        .with_stderr_contains("[..] unknown variant `m1`, expected `s` or `ms`")
+        .run();
+}
+
+#[cargo_test]
+fn net_retry_config_bad_format2() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [dependencies.bar]
+                git = "http://127.0.0.1:11/foo/bar"
+            "#,
+        )
+        .file(
+            ".cargo/config",
+            r#"
+           [net]
+           retry-delay="m10"
+           [http]
+           timeout=1
+            "#,
+        )
+        .file("src/main.rs", "")
+        .build();
+
+    p.cargo("build -v -j 1")
+        .with_status(101)
+        .with_stderr_contains(
+            "[..] Invalid value format: `m10`, expecting a positive number followed by unit",
+        )
+        .run();
+}
+
+#[cargo_test]
+fn net_retry_config_bad_format3() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [project]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+
+                [dependencies.bar]
+                git = "http://127.0.0.1:11/foo/bar"
+            "#,
+        )
+        .file(
+            ".cargo/config",
+            r#"
+           [net]
+           retry-delay="10"
+           [http]
+           timeout=1
+            "#,
+        )
+        .file("src/main.rs", "")
+        .build();
+
+    p.cargo("build -v -j 1")
+        .with_status(101)
+        .with_stderr_contains("[..] No unit is found on value: `10`")
+        .run();
+}
