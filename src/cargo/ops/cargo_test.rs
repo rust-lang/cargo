8c1dd028c1a48a848f4f8cea44269501723b9b60
gilescope <gilescope@gmail.com>
2021-11-09 07:53:22 +0000

    doc tests were now the slow bit

--- src/cargo/ops/cargo_test.rs
+++ src/cargo/ops/cargo_test.rs
@@ -85,6 +85,7 @@ struct Context {
 }
 
 type TestError = (TargetKind, String, String, anyhow::Error);
+type TestDocError = (crate::util::errors::Test, anyhow::Error);
 
 fn run_unit_tests(
     config: &Config,
@@ -269,99 +270,194 @@ fn run_doc_tests(
     let doctest_xcompile = config.cli_unstable().doctest_xcompile;
     let doctest_in_workspace = config.cli_unstable().doctest_in_workspace;
 
-    for doctest_info in &compilation.to_doc_test {
-        let Doctest {
-            args,
-            unstable_opts,
-            unit,
-            linker,
-            script_meta,
-        } = doctest_info;
+    let processing = AtomicU32::new(0);
+    thread::scope(|s| {
+        let mut handles = vec![];
+        let test_jobs = options.compile_opts.build_config.test_jobs;
+        let parallel = test_jobs > 1;
 
-        if !doctest_xcompile {
-            match unit.kind {
-                CompileKind::Host => {}
-                CompileKind::Target(target) => {
-                    if target.short_name() != compilation.host {
-                        // Skip doctests, -Zdoctest-xcompile not enabled.
-                        continue;
+        for doctest_info in &compilation.to_doc_test {
+            let Doctest {
+                args,
+                unstable_opts,
+                unit,
+                linker,
+                script_meta,
+            } = doctest_info;
+
+            if !doctest_xcompile {
+                match unit.kind {
+                    CompileKind::Host => {}
+                    CompileKind::Target(target) => {
+                        if target.short_name() != compilation.host {
+                            // Skip doctests, -Zdoctest-xcompile not enabled.
+                            continue;
+                        }
                     }
                 }
             }
-        }
 
-        config.shell().status("Doc-tests", unit.target.name())?;
-        let mut p = compilation.rustdoc_process(unit, *script_meta)?;
-        p.arg("--crate-name").arg(&unit.target.crate_name());
-        p.arg("--test");
+            let mut p = compilation.rustdoc_process(unit, *script_meta)?;
+            p.arg("--crate-name").arg(&unit.target.crate_name());
+            p.arg("--test");
 
-        if doctest_in_workspace {
-            add_path_args(ws, unit, &mut p);
-            // FIXME(swatinem): remove the `unstable-options` once rustdoc stabilizes the `test-run-directory` option
-            p.arg("-Z").arg("unstable-options");
-            p.arg("--test-run-directory")
-                .arg(unit.pkg.root().to_path_buf());
-        } else {
-            p.arg(unit.target.src_path().path().unwrap());
-        }
-
-        if doctest_xcompile {
-            if let CompileKind::Target(target) = unit.kind {
-                // use `rustc_target()` to properly handle JSON target paths
-                p.arg("--target").arg(target.rustc_target());
+            if doctest_in_workspace {
+                add_path_args(ws, unit, &mut p);
+                // FIXME(swatinem): remove the `unstable-options` once rustdoc stabilizes the `test-run-directory` option
+                p.arg("-Z").arg("unstable-options");
+                p.arg("--test-run-directory")
+                    .arg(unit.pkg.root().to_path_buf());
+            } else {
+                p.arg(unit.target.src_path().path().unwrap());
             }
-            p.arg("-Zunstable-options");
-            p.arg("--enable-per-target-ignores");
-            if let Some((runtool, runtool_args)) = compilation.target_runner(unit.kind) {
-                p.arg("--runtool").arg(runtool);
-                for arg in runtool_args {
-                    p.arg("--runtool-arg").arg(arg);
+
+            if doctest_xcompile {
+                if let CompileKind::Target(target) = unit.kind {
+                    // use `rustc_target()` to properly handle JSON target paths
+                    p.arg("--target").arg(target.rustc_target());
+                }
+                p.arg("-Zunstable-options");
+                p.arg("--enable-per-target-ignores");
+                if let Some((runtool, runtool_args)) = compilation.target_runner(unit.kind) {
+                    p.arg("--runtool").arg(runtool);
+                    for arg in runtool_args {
+                        p.arg("--runtool-arg").arg(arg);
+                    }
+                }
+                if let Some(linker) = linker {
+                    let mut joined = OsString::from("linker=");
+                    joined.push(linker);
+                    p.arg("-C").arg(joined);
                 }
             }
-            if let Some(linker) = linker {
-                let mut joined = OsString::from("linker=");
-                joined.push(linker);
-                p.arg("-C").arg(joined);
+
+            for &rust_dep in &[
+                &compilation.deps_output[&unit.kind],
+                &compilation.deps_output[&CompileKind::Host],
+            ] {
+                let mut arg = OsString::from("dependency=");
+                arg.push(rust_dep);
+                p.arg("-L").arg(arg);
+            }
+
+            for native_dep in compilation.native_dirs.iter() {
+                p.arg("-L").arg(native_dep);
+            }
+
+            for arg in test_args {
+                p.arg("--test-args").arg(arg);
+            }
+
+            if config.shell().verbosity() == Verbosity::Quiet {
+                p.arg("--test-args").arg("--quiet");
+            }
+
+            p.args(args);
+
+            if *unstable_opts {
+                p.arg("-Zunstable-options");
+            }
+
+            let mut ctx = Context {
+                exe_display: unit.target.name().to_string(),
+                cmd: String::new(),
+            };
+            write!(ctx.cmd, "{}", &p).unwrap();
+
+            let processing_t = &processing;
+            let (tx, rx) = std::sync::mpsc::channel();
+            let handle = s.spawn(move |_| {
+                std::thread::park();
+
+                let result = p
+                    .exec_with_streaming(
+                        &mut |line| {
+                            tx.send(OutOrErr::Out(line.to_string())).unwrap();
+                            Ok(())
+                        },
+                        &mut |line| {
+                            tx.send(OutOrErr::Err(line.to_string())).unwrap();
+                            Ok(())
+                        },
+                        false,
+                    )
+                    .map_err(|e| (Test::Doc, e));
+                processing_t.fetch_sub(1, Ordering::Relaxed);
+                result
+            });
+            if parallel {
+                handles.push((handle, rx, ctx));
+            } else {
+                handle.thread().unpark();
+                if !process_doc_output(handle, &mut errors, ctx, config, options, rx)? {
+                    break;
+                }
             }
         }
 
-        for &rust_dep in &[
-            &compilation.deps_output[&unit.kind],
-            &compilation.deps_output[&CompileKind::Host],
-        ] {
-            let mut arg = OsString::from("dependency=");
-            arg.push(rust_dep);
-            p.arg("-L").arg(arg);
-        }
+        if parallel {
+            let mut threads: Vec<_> = handles.iter().map(|(h, _, _)| h.thread().clone()).collect();
 
-        for native_dep in compilation.native_dirs.iter() {
-            p.arg("-L").arg(native_dep);
-        }
+            //Have a thread unparking so that there's no more than n running at once...
+            let proc = &processing;
+            s.spawn(move |_| {
+                while !threads.is_empty() {
+                    if proc.load(Ordering::Relaxed) < test_jobs {
+                        proc.fetch_add(1, Ordering::Relaxed);
+                        threads.pop().unwrap().unpark();
+                    } else {
+                        std::thread::sleep(Duration::from_millis(500));
+                    }
+                }
+            });
 
-        for arg in test_args {
-            p.arg("--test-args").arg(arg);
-        }
-
-        if config.shell().verbosity() == Verbosity::Quiet {
-            p.arg("--test-args").arg("--quiet");
-        }
-
-        p.args(args);
-
-        if *unstable_opts {
-            p.arg("-Zunstable-options");
-        }
-
-        config
-            .shell()
-            .verbose(|shell| shell.status("Running", p.to_string()))?;
-        if let Err(e) = p.exec() {
-            let e = e.downcast::<ProcessError>()?;
-            errors.push(e);
-            if !options.no_fail_fast {
-                return Ok((Test::Doc, errors));
+            // Report results in the standard order...
+            for (handle, rx, ctx) in handles {
+                if !process_doc_output(handle, &mut errors, ctx, config, options, rx)? {
+                    break;
+                }
             }
+        }
+        let out: Result<(), anyhow::Error> = Ok(());
+        out
+    })
+    .unwrap()?;
+    let mut res = vec![];
+    for (_, e) in errors.into_iter() {
+        res.push(e.downcast::<ProcessError>()?);
+    }
+    Ok((Test::Doc, res))
+}
+
+/// Puts test output on the sceen.
+/// Returns false if we should early exit due to test failures.
+fn process_doc_output<'scope>(
+    handle: ScopedJoinHandle<'scope, Result<std::process::Output, TestDocError>>,
+    errors: &mut Vec<TestDocError>,
+    ctx: Context,
+    config: &Config,
+    options: &TestOptions,
+    rx: std::sync::mpsc::Receiver<OutOrErr>,
+) -> CargoResult<bool> {
+    config.shell().status("Doc-tests", ctx.exe_display)?;
+
+    config
+        .shell()
+        .verbose(|shell| shell.status("Running", &ctx.cmd))?;
+
+    for line in &rx {
+        match line {
+            OutOrErr::Out(line) => writeln!(config.shell().out(), "{}", line).unwrap(),
+            OutOrErr::Err(line) => writeln!(config.shell().err(), "{}", line).unwrap(),
         }
     }
-    Ok((Test::Doc, errors))
+    let result = handle.join().unwrap();
+
+    if let Err(err) = result {
+        errors.push(err);
+        if !options.no_fail_fast {
+            return Ok(false);
+        }
+    }
+    Ok(true)
 }
