c2332dc0bac67315e3032734c046b50ae78b5776
Aaron Hill <aa1ronham@gmail.com>
2021-12-05 13:27:57 -0600

    Detect if rustc supports '--json future-incompat'

--- src/cargo/core/compiler/mod.rs
+++ src/cargo/core/compiler/mod.rs
@@ -641,7 +641,7 @@ fn rustdoc(cx: &mut Context<'_, '_>, unit: &Unit) -> CargoResult<Work> {
         rustdoc.arg("--cfg").arg(&format!("feature=\"{}\"", feat));
     }
 
-    add_error_format_and_color(cx, &mut rustdoc, false);
+    add_error_format_and_color(cx, &mut rustdoc, unit, false);
     add_allow_features(cx, &mut rustdoc);
 
     if let Some(args) = cx.bcx.extra_args_for(unit) {
@@ -790,7 +790,12 @@ fn add_allow_features(cx: &Context<'_, '_>, cmd: &mut ProcessBuilder) {
 /// intercepting messages like rmeta artifacts, etc. rustc includes a
 /// "rendered" field in the JSON message with the message properly formatted,
 /// which Cargo will extract and display to the user.
-fn add_error_format_and_color(cx: &Context<'_, '_>, cmd: &mut ProcessBuilder, pipelined: bool) {
+fn add_error_format_and_color(
+    cx: &Context<'_, '_>,
+    cmd: &mut ProcessBuilder,
+    unit: &Unit,
+    pipelined: bool,
+) {
     cmd.arg("--error-format=json");
     let mut json = String::from("--json=diagnostic-rendered-ansi");
     if pipelined {
@@ -798,9 +803,16 @@ fn add_error_format_and_color(cx: &Context<'_, '_>, cmd: &mut ProcessBuilder, pi
         // to emit a message that cargo will intercept.
         json.push_str(",artifacts");
     }
-    // Always emit a future-incompat report, so we can report
-    // future-incompat dependencies to the user
-    json.push_str(",future-incompat");
+    if cx
+        .bcx
+        .target_data
+        .info(unit.kind)
+        .supports_json_future_incompat
+    {
+        // Emit a future-incompat report (when supported by rustc), so we can report
+        // future-incompat dependencies to the user
+        json.push_str(",future-incompat");
+    }
 
     match cx.bcx.build_config.message_format {
         MessageFormat::Short | MessageFormat::Json { short: true, .. } => {
@@ -861,7 +873,7 @@ fn build_base_args(
     edition.cmd_edition_arg(cmd);
 
     add_path_args(bcx.ws, unit, cmd);
-    add_error_format_and_color(cx, cmd, cx.rmeta_required(unit));
+    add_error_format_and_color(cx, cmd, unit, cx.rmeta_required(unit));
     add_allow_features(cx, cmd);
 
     let mut contains_dy_lib = false;
