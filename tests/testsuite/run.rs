rust-lang/cargo

commit 3fd28143de72d2d0e63186273d68aa890d8297e7

Support multiple `--target` flags on the CLI
This commit refactors the internals of Cargo to no longer have a
singular `--target` flag (and singular `requested_target` kind throught)
but to instead have a list. The semantics of multiple `--target` flags
is to build the selected targets for each of the input `--target` flag
inputs.

For now this is gated behind `-Zmultitarget` as an unstable features,
since I'm not entirely sure this is the interface we want. In general
it'd be great if we had a way to simply specify `Unit` structures of
what to build on the CLI, but we're in general very far away from that,
so I figured that this is probably sufficient at least for testing for
now.

cc #8156

src/cargo/core/compiler/compilation.rs
@@ -8,15 +8,14 @@ use semver::Version;
 
 use super::BuildContext;
 use crate::core::compiler::CompileKind;
-use crate::core::{Edition, Package, PackageId, Target};
+use crate::core::compiler::Unit;
+use crate::core::{Edition, Package, PackageId};
 use crate::util::{self, config, join_paths, process, CargoResult, Config, ProcessBuilder};
 
 /// Structure with enough information to run `rustdoc --test`.
 pub struct Doctest {
-    /// The package being doc-tested.
-    pub package: Package,
-    /// The target being tested (currently always the package's lib).
-    pub target: Target,
+    /// What's being doctested
+    pub unit: Unit,
     /// Arguments needed to pass to rustdoc to run this test.
     pub args: Vec<OsString>,
     /// Whether or not -Zunstable-options is needed.
@@ -26,11 +25,12 @@ pub struct Doctest {
 /// A structure returning the result of a compilation.
 pub struct Compilation<'cfg> {
     /// An array of all tests created during this compilation.
-    /// `(package, target, path_to_test_exe)`
-    pub tests: Vec<(Package, Target, PathBuf)>,
+    /// `(unit, path_to_test_exe)` where `unit` contains information such as the
+    /// package, compile target, etc.
+    pub tests: Vec<(Unit, PathBuf)>,
 
     /// An array of all binaries created.
-    pub binaries: Vec<PathBuf>,
+    pub binaries: Vec<(Unit, PathBuf)>,
 
     /// All directories for the output of native build commands.
     ///
@@ -41,20 +41,17 @@ pub struct Compilation<'cfg> {
     pub native_dirs: BTreeSet<PathBuf>,
 
     /// Root output directory (for the local package's artifacts)
-    pub root_output: PathBuf,
+    pub root_output: HashMap<CompileKind, PathBuf>,
 
     /// Output directory for rust dependencies.
     /// May be for the host or for a specific target.
-    pub deps_output: PathBuf,
+    pub deps_output: HashMap<CompileKind, PathBuf>,
 
-    /// Output directory for the rust host dependencies.
-    pub host_deps_output: PathBuf,
+    /// The path to the host libdir for the compiler used
+    sysroot_host_libdir: PathBuf,
 
-    /// The path to rustc's own libstd
-    pub host_dylib_path: PathBuf,
-
-    /// The path to libstd for the target
-    pub target_dylib_path: PathBuf,
+    /// The path to libstd for each target
+    sysroot_target_libdir: HashMap<CompileKind, PathBuf>,
 
     /// Extra environment variables that were passed to compilations and should
     /// be passed to future invocations of programs.
@@ -69,9 +66,6 @@ pub struct Compilation<'cfg> {
     /// Flags to pass to rustdoc when invoked from cargo test, per package.
     pub rustdocflags: HashMap<PackageId, Vec<String>>,
 
-    pub host: String,
-    pub target: String,
-
     config: &'cfg Config,
 
     /// Rustc process to be used by default
@@ -82,7 +76,7 @@ pub struct Compilation<'cfg> {
     /// rustc_workspace_wrapper_process
     primary_rustc_process: Option<ProcessBuilder>,
 
-    target_runner: Option<(PathBuf, Vec<String>)>,
+    target_runners: HashMap<CompileKind, Option<(PathBuf, Vec<String>)>>,
 }
 
 impl<'cfg> Compilation<'cfg> {
@@ -100,23 +94,28 @@ impl<'cfg> Compilation<'cfg> {
             }
         }
 
-        let default_kind = bcx.build_config.requested_kind;
         Ok(Compilation {
             // TODO: deprecated; remove.
             native_dirs: BTreeSet::new(),
-            root_output: PathBuf::from("/"),
-            deps_output: PathBuf::from("/"),
-            host_deps_output: PathBuf::from("/"),
-            host_dylib_path: bcx
+            root_output: HashMap::new(),
+            deps_output: HashMap::new(),
+            sysroot_host_libdir: bcx
                 .target_data
                 .info(CompileKind::Host)
                 .sysroot_host_libdir
                 .clone(),
-            target_dylib_path: bcx
-                .target_data
-                .info(default_kind)
-                .sysroot_target_libdir
-                .clone(),
+            sysroot_target_libdir: bcx
+                .build_config
+                .requested_kinds
+                .iter()
+                .chain(Some(&CompileKind::Host))
+                .map(|kind| {
+                    (
+                        *kind,
+                        bcx.target_data.info(*kind).sysroot_target_libdir.clone(),
+                    )
+                })
+                .collect(),
             tests: Vec::new(),
             binaries: Vec::new(),
             extra_env: HashMap::new(),
@@ -127,16 +126,20 @@ impl<'cfg> Compilation<'cfg> {
             rustc_process: rustc,
             rustc_workspace_wrapper_process,
             primary_rustc_process,
-            host: bcx.host_triple().to_string(),
-            target: bcx.target_data.short_name(&default_kind).to_string(),
-            target_runner: target_runner(bcx, default_kind)?,
+            target_runners: bcx
+                .build_config
+                .requested_kinds
+                .iter()
+                .chain(Some(&CompileKind::Host))
+                .map(|kind| Ok((*kind, target_runner(bcx, *kind)?)))
+                .collect::<CargoResult<HashMap<_, _>>>()?,
         })
     }
 
     /// See `process`.
     pub fn rustc_process(
         &self,
-        pkg: &Package,
+        unit: &Unit,
         is_primary: bool,
         is_workspace: bool,
     ) -> CargoResult<ProcessBuilder> {
@@ -148,17 +151,22 @@ impl<'cfg> Compilation<'cfg> {
             self.rustc_process.clone()
         };
 
-        self.fill_env(rustc, pkg, true)
+        self.fill_env(rustc, &unit.pkg, unit.kind, true)
     }
 
     /// See `process`.
-    pub fn rustdoc_process(&self, pkg: &Package, target: &Target) -> CargoResult<ProcessBuilder> {
-        let mut p = self.fill_env(process(&*self.config.rustdoc()?), pkg, false)?;
-        if target.edition() != Edition::Edition2015 {
-            p.arg(format!("--edition={}", target.edition()));
+    pub fn rustdoc_process(&self, unit: &Unit) -> CargoResult<ProcessBuilder> {
+        let mut p = self.fill_env(
+            process(&*self.config.rustdoc()?),
+            &unit.pkg,
+            unit.kind,
+            true,
+        )?;
+        if unit.target.edition() != Edition::Edition2015 {
+            p.arg(format!("--edition={}", unit.target.edition()));
         }
 
-        for crate_type in target.rustc_crate_types() {
+        for crate_type in unit.target.rustc_crate_types() {
             p.arg("--crate-type").arg(crate_type);
         }
 
@@ -171,20 +179,21 @@ impl<'cfg> Compilation<'cfg> {
         cmd: T,
         pkg: &Package,
     ) -> CargoResult<ProcessBuilder> {
-        self.fill_env(process(cmd), pkg, true)
+        self.fill_env(process(cmd), pkg, CompileKind::Host, false)
     }
 
-    pub fn target_runner(&self) -> &Option<(PathBuf, Vec<String>)> {
-        &self.target_runner
+    pub fn target_runner(&self, kind: CompileKind) -> Option<&(PathBuf, Vec<String>)> {
+        self.target_runners.get(&kind).and_then(|x| x.as_ref())
     }
 
     /// See `process`.
     pub fn target_process<T: AsRef<OsStr>>(
         &self,
         cmd: T,
+        kind: CompileKind,
         pkg: &Package,
     ) -> CargoResult<ProcessBuilder> {
-        let builder = if let Some((ref runner, ref args)) = *self.target_runner() {
+        let builder = if let Some((runner, args)) = self.target_runner(kind) {
             let mut builder = process(runner);
             builder.args(args);
             builder.arg(cmd);
@@ -192,7 +201,7 @@ impl<'cfg> Compilation<'cfg> {
         } else {
             process(cmd)
         };
-        self.fill_env(builder, pkg, false)
+        self.fill_env(builder, pkg, kind, false)
     }
 
     /// Prepares a new process with an appropriate environment to run against
@@ -204,26 +213,28 @@ impl<'cfg> Compilation<'cfg> {
         &self,
         mut cmd: ProcessBuilder,
         pkg: &Package,
-        is_host: bool,
+        kind: CompileKind,
+        is_rustc_tool: bool,
     ) -> CargoResult<ProcessBuilder> {
-        let mut search_path = if is_host {
-            let mut search_path = vec![self.host_deps_output.clone()];
-            search_path.push(self.host_dylib_path.clone());
-            search_path
+        let mut search_path = Vec::new();
+        if is_rustc_tool {
+            search_path.push(self.deps_output[&CompileKind::Host].clone());
+            search_path.push(self.sysroot_host_libdir.clone());
         } else {
-            let mut search_path =
-                super::filter_dynamic_search_path(self.native_dirs.iter(), &self.root_output);
-            search_path.push(self.deps_output.clone());
-            search_path.push(self.root_output.clone());
+            search_path.extend(super::filter_dynamic_search_path(
+                self.native_dirs.iter(),
+                &self.root_output[&kind],
+            ));
+            search_path.push(self.deps_output[&kind].clone());
+            search_path.push(self.root_output[&kind].clone());
             // For build-std, we don't want to accidentally pull in any shared
             // libs from the sysroot that ships with rustc. This may not be
             // required (at least I cannot craft a situation where it
             // matters), but is here to be safe.
             if self.config.cli_unstable().build_std.is_none() {
-                search_path.push(self.target_dylib_path.clone());
+                search_path.push(self.sysroot_target_libdir[&kind].clone());
             }
-            search_path
-        };
+        }
 
         let dylib_path = util::dylib_path();
         let dylib_path_is_empty = dylib_path.is_empty();

tests/testsuite/run.rs
@@ -849,6 +849,7 @@ fn run_with_library_paths() {
             fn main() {{
                 let search_path = std::env::var_os("{}").unwrap();
                 let paths = std::env::split_paths(&search_path).collect::<Vec<_>>();
+                println!("{{:#?}}", paths);
                 assert!(paths.contains(&r#"{}"#.into()));
                 assert!(paths.contains(&r#"{}"#.into()));
             }}

src/bin/cargo/commands/clean.rs
@@ -28,7 +28,7 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
     let opts = CleanOptions {
         config,
         spec: values(args, "package"),
-        target: args.target(),
+        targets: args.targets(),
         requested_profile: args.get_profile_name(config, "dev", ProfileChecking::Checked)?,
         profile_specified: args.is_present("profile") || args.is_present("release"),
         doc: args.is_present("doc"),

src/bin/cargo/commands/fetch.rs
@@ -28,7 +28,7 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
 
     let opts = FetchOptions {
         config,
-        target: args.target(),
+        targets: args.targets(),
     };
     let _ = ops::fetch(&ws, &opts)?;
     Ok(())

src/bin/cargo/commands/metadata.rs
@@ -12,13 +12,11 @@ pub fn cli() -> App {
         )
         .arg(opt("quiet", "No output printed to stdout").short("q"))
         .arg_features()
-        .arg(
-            opt(
-                "filter-platform",
-                "Only include resolve dependencies matching the given target-triple",
-            )
-            .value_name("TRIPLE"),
-        )
+        .arg(multi_opt(
+            "filter-platform",
+            "TRIPLE",
+            "Only include resolve dependencies matching the given target-triple",
+        ))
         .arg(opt(
             "no-deps",
             "Output information only about the workspace members \
@@ -51,7 +49,7 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
         all_features: args.is_present("all-features"),
         no_default_features: args.is_present("no-default-features"),
         no_deps: args.is_present("no-deps"),
-        filter_platform: args.value_of("filter-platform").map(|s| s.to_string()),
+        filter_platforms: args._values_of("filter-platform"),
         version,
     };
 

src/bin/cargo/commands/package.rs
@@ -42,7 +42,7 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
             list: args.is_present("list"),
             check_metadata: !args.is_present("no-metadata"),
             allow_dirty: args.is_present("allow-dirty"),
-            target: args.target(),
+            targets: args.targets(),
             jobs: args.jobs()?,
             features: args._values_of("features"),
             all_features: args.is_present("all-features"),

src/bin/cargo/commands/publish.rs
@@ -40,7 +40,7 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
             index,
             verify: !args.is_present("no-verify"),
             allow_dirty: args.is_present("allow-dirty"),
-            target: args.target(),
+            targets: args.targets(),
             jobs: args.jobs()?,
             dry_run: args.is_present("dry-run"),
             registry,

src/bin/cargo/commands/tree.rs
@@ -129,15 +129,15 @@ pub fn exec(config: &mut Config, args: &ArgMatches<'_>) -> CliResult {
         )?;
     }
 
-    let target = if args.is_present("all-targets") {
+    let targets = if args.is_present("all-targets") {
         config
             .shell()
             .warn("the --all-targets flag has been changed to --target=all")?;
-        Some("all")
+        vec!["all".to_string()]
     } else {
-        args.value_of("target")
+        args._values_of("target")
     };
-    let target = tree::Target::from_cli(target);
+    let target = tree::Target::from_cli(targets);
 
     let edge_kinds = parse_edge_kinds(config, args)?;
     let graph_features = edge_kinds.contains(&EdgeKind::Feature);

commit 88b413ec895d5777422521d6d2015cb66996ab3e

Properly set dylib search path for doctests
Fixes #8531.

This issue was introduced by 3fd28143de72d2d0e63186273d68aa890d8297e7.
Prior to that commit, the `rustdoc_process` function took other branch
of the if statement being modified by this commit. The flag was
previously called `is_host`, and `rustdoc` was run reporting `false`. In
that commit, the flag was changed to `is_rustc_tool`, and rustdoc began
reporting `true`, which resulted in the native directories added by
build scripts no longer being appended to LD_LIBRARY_PATH when running
doctests.

This commit changes the behavior so that we are always appending the
same set of paths, and the only variance is if we are cross compiling or
not. An alternative would be to change rustdoc to always pass
`kind: CompileKind::Host, is_rustc_tool: false`, but as rustdoc is in
fact a rustc tool, that feels wrong. The commit which introduced the bug
did not include context on why the flag was changed to `is_rustc_tool`,
so I don't have a sense for if we should just change that flag to mean
something else.

While the test suite previously had an explicit test for the
library path behavior of `cargo run`, it did not test this for various
test forms. This commit modifies the test to ensure the behavior is
consistent across `run`, normal tests, and doctests.

src/cargo/core/compiler/compilation.rs
@@ -267,6 +267,10 @@ impl<'cfg> Compilation<'cfg> {
     ) -> CargoResult<ProcessBuilder> {
         let mut search_path = Vec::new();
         if is_rustc_tool {
+            search_path.extend(super::filter_dynamic_search_path(
+                self.native_dirs.iter(),
+                &self.root_output[&CompileKind::Host],
+            ));
             search_path.push(self.deps_output[&CompileKind::Host].clone());
             search_path.push(self.sysroot_host_libdir.clone());
         } else {

tests/testsuite/run.rs
@@ -1146,9 +1146,27 @@ fn run_with_library_paths() {
         )
         .file(
             "src/main.rs",
+            r##"
+            extern crate foo;
+
+            fn main() {
+                foo::assert_search_path();
+            }
+            "##,
+        )
+        .file(
+            "src/lib.rs",
             &format!(
                 r##"
-                    fn main() {{
+                    #[test]
+                    fn test_search_path() {{
+                        assert_search_path();
+                    }}
+
+                    /// ```
+                    /// foo::assert_search_path();
+                    /// ```
+                    pub fn assert_search_path() {{
                         let search_path = std::env::var_os("{}").unwrap();
                         let paths = std::env::split_paths(&search_path).collect::<Vec<_>>();
                         println!("{{:#?}}", paths);
@@ -1164,6 +1182,7 @@ fn run_with_library_paths() {
         .build();
 
     p.cargo("run").run();
+    p.cargo("test").run();
 }
 
 #[cargo_test]