41ea720c3fca63ef8bf2ec2cb7e5517cb381880e
CAD97 <cad97@cad97.com>
2022-03-13 22:38:50 -0500

    Add simple example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 //! Convenience wrappers for cargo buildscript input/output.
+//!
+//! # Example
+//!
+//! ```rust
+//! build::rerun_if_changed("build.rs"); // only rerun for buildscript changes
+//! build::rustc_cfg("has_buildrs"); // set #[cfg(has_buildrs)]
+//! dbg!(build::cargo()); // path to the cargo executable
+//! dbg!(build::cargo_manifest_dir()); // the directory of the build manifest
+//! ```
 
 /// Inputs to the build script, in the form of environment variables.
 pub mod input;
