92fbc4e34482c2ecae8fcd513a590e8e6638ce5f
Scott Schafer <schaferjscott@gmail.com>
2022-05-06 20:51:29 -0500

    move all `snapshot/cargo_add/` tests to `testsuite/cargo_add/`

---
+++ tests/testsuite/cargo_add/no_optional/mod.rs
@@ -0,0 +1,25 @@
+use cargo_test_support::compare::assert;
+use cargo_test_support::prelude::*;
+use cargo_test_support::Project;
+
+use crate::cargo_add::init_registry;
+use cargo_test_support::curr_dir;
+
+#[cargo_test]
+fn no_optional() {
+    init_registry();
+    let project = Project::from_template(curr_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    snapbox::cmd::Command::cargo()
+        .arg("add")
+        .arg_line("my-package1 my-package2@0.4.1 --no-optional")
+        .current_dir(cwd)
+        .assert()
+        .success()
+        .stdout_matches_path(curr_dir!().join("stdout.log"))
+        .stderr_matches_path(curr_dir!().join("stderr.log"));
+
+    assert().subset_matches(curr_dir!().join("out"), &project_root);
+}
---
+++ tests/testsuite/cargo_add/overwrite_inherit_features_noop/mod.rs
@@ -0,0 +1,24 @@
+use cargo_test_support::compare::assert;
+use cargo_test_support::prelude::*;
+use cargo_test_support::Project;
+
+use cargo_test_support::curr_dir;
+
+#[cargo_test]
+fn overwrite_inherit_features_noop() {
+    let project = Project::from_template(curr_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    snapbox::cmd::Command::cargo()
+        .masquerade_as_nightly_cargo()
+        .arg("add")
+        .args(["foo", "-p", "bar"])
+        .current_dir(cwd)
+        .assert()
+        .success()
+        .stdout_matches_path(curr_dir!().join("stdout.log"))
+        .stderr_matches_path(curr_dir!().join("stderr.log"));
+
+    assert().subset_matches(curr_dir!().join("out"), &project_root);
+}
