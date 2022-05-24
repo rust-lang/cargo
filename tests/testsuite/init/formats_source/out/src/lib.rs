8401ef16f6c431a77bb2bfe3ed3e669832c7e5ba
merelymyself <poonsters6@gmail.com>
2022-05-24 20:42:26 +0800

    fixed issue with formats_source

--- tests/testsuite/init/formats_source/out/src/lib.rs
+++ tests/testsuite/init/formats_source/out/src/lib.rs
@@ -1,10 +1,10 @@
 #[cfg(test)]
 mod tests {
-    // Uncomment this to use items from the containing module.
-    // use super::*;
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+  // Uncomment this to use items from the containing module.
+  // use super::*;
+  #[test]
+  fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+  }
 }
