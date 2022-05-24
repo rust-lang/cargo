a022179dce1c625a5ee3b46633d6c1c8cb7b701c
Deadbeef <ent3rm4n@gmail.com>
2022-05-24 21:27:06 +1000

    Removed re-export of sysroot types from mock-proc-macro

--- tests/testsuite/mock-std/library/proc_macro/src/lib.rs
+++ tests/testsuite/mock-std/library/proc_macro/src/lib.rs
@@ -1,11 +1,6 @@
 #![feature(staged_api)]
 #![stable(since = "1.0.0", feature = "dummy")]
 
-extern crate proc_macro;
-
-#[stable(since = "1.0.0", feature = "dummy")]
-pub use proc_macro::*;
-
 #[stable(since = "1.0.0", feature = "dummy")]
 pub fn custom_api() {
 }
