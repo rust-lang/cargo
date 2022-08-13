cb8cbb8a3e8525fe5c9337fb972337e5cb0f04e9
Thomas Harmon <thomas.alan.harmon@gmail.com>
2022-08-13 15:26:20 -0700

    Cargo fmt

--- tests/testsuite/cargo_add_with_vendored_pkgs.rs
+++ tests/testsuite/cargo_add_with_vendored_pkgs.rs
@@ -1,11 +1,12 @@
 use cargo_test_support::project;
 
-
 #[cargo_test]
 fn carg_add_with_vendored_packages() {
     let p = project()
         .file("src/main.rs", r#"fn main() { println!("hi!"); }"#)
-        .file("Cargo.toml", r#"[package]
+        .file(
+            "Cargo.toml",
+            r#"[package]
 name = "example"
 version = "0.1.0"
 edition = "2021"
@@ -13,29 +14,37 @@ edition = "2021"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-xml-rs = "0.8.4""#)
+xml-rs = "0.8.4""#,
+        )
         .build();
 
     p.cargo("vendor ./vendor")
-        .with_stdout(r#"
+        .with_stdout(
+            r#"
 [source.crates-io]
 replace-with = "vendored-sources"
 
 [source.vendored-sources]
-directory = "./vendor""#)
+directory = "./vendor""#,
+        )
         .run();
-    p.change_file(".cargo/config.toml", r#"
+    p.change_file(
+        ".cargo/config.toml",
+        r#"
     [source.crates-io]
     replace-with = "vendored-sources"
     
     [source.vendored-sources]
-    directory = "./vendor""#);
+    directory = "./vendor""#,
+    );
     p.cargo("add cbindgen")
         // current output
         //.with_stdout(r#"warning: translating `cbindgen` to `xml-rs`
-      //Adding xml-rs v0.8.4 to dependencies."#)
-      // correct output
-        .with_stdout(r#"    Updating crates.io index
-      Adding xml-rs v0.8.4 to dependencies."#)
+        //Adding xml-rs v0.8.4 to dependencies."#)
+        // correct output
+        .with_stdout(
+            r#"    Updating crates.io index
+      Adding xml-rs v0.8.4 to dependencies."#,
+        )
         .run();
 }
