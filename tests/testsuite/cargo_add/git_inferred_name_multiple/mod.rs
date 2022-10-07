6ae292100bd22c8229b5fb0349458682e92fc340
hi-rustin <rustin.liu@gmail.com>
2022-10-07 19:00:14 +0800

    Update cargo add test
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- tests/testsuite/cargo_add/git_inferred_name_multiple/mod.rs
+++ tests/testsuite/cargo_add/git_inferred_name_multiple/mod.rs
@@ -23,6 +23,41 @@ fn git_inferred_name_multiple() {
                 &cargo_test_support::basic_manifest("my-package2", "0.3.0+my-package2"),
             )
             .file("p2/src/lib.rs", "")
+            .file(
+                "p3/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package3", "0.3.0+my-package2"),
+            )
+            .file("p3/src/lib.rs", "")
+            .file(
+                "p4/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package4", "0.3.0+my-package2"),
+            )
+            .file("p4/src/lib.rs", "")
+            .file(
+                "p5/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package5", "0.3.0+my-package2"),
+            )
+            .file("p5/src/lib.rs", "")
+            .file(
+                "p6/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package6", "0.3.0+my-package2"),
+            )
+            .file("p6/src/lib.rs", "")
+            .file(
+                "p7/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package7", "0.3.0+my-package2"),
+            )
+            .file("p7/src/lib.rs", "")
+            .file(
+                "p8/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package8", "0.3.0+my-package2"),
+            )
+            .file("p8/src/lib.rs", "")
+            .file(
+                "p9/Cargo.toml",
+                &cargo_test_support::basic_manifest("my-package9", "0.3.0+my-package2"),
+            )
+            .file("p9/src/lib.rs", "")
     });
     let git_url = git_dep.url().to_string();
 
