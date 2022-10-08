299252c78dbe2a3b5a678268dc1b0d5be3736b33
Cassaundra Smith <cass@cassaundra.org>
2022-10-07 17:34:22 -0700

    Clean profile, patch, and replace in cargo remove
    After a successful removal of a dependency, clean up the profile, patch, and
    replace sections to remove all references to the dependency.

---
+++ tests/testsuite/cargo_remove/gc_profile/mod.rs
@@ -0,0 +1,25 @@
+use cargo_test_support::compare::assert_ui;
+use cargo_test_support::curr_dir;
+use cargo_test_support::CargoCommand;
+use cargo_test_support::Project;
+
+use crate::cargo_remove::init_registry;
+
+#[cargo_test]
+fn case() {
+    init_registry();
+    let project = Project::from_template(curr_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    snapbox::cmd::Command::cargo_ui()
+        .arg("remove")
+        .args(["toml"])
+        .current_dir(cwd)
+        .assert()
+        .success()
+        .stdout_matches_path(curr_dir!().join("stdout.log"))
+        .stderr_matches_path(curr_dir!().join("stderr.log"));
+
+    assert_ui().subset_matches(curr_dir!().join("out"), &project_root);
+}
