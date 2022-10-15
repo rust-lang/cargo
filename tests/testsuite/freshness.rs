6848006cbe829462c458dc08214d6b854cbb7fbd
Rémy Rakic <remy.rakic+github@gmail.com>
2022-10-15 15:02:14 +0000

    update tests for build deps debuginfo optimization

--- tests/testsuite/freshness.rs
+++ tests/testsuite/freshness.rs
@@ -1477,7 +1477,7 @@ fn reuse_panic_pm() {
         .with_stderr_unordered(
             "\
 [COMPILING] bar [..]
-[RUNNING] `rustc --crate-name bar bar/src/lib.rs [..]--crate-type lib --emit=[..]link[..]-C debuginfo=2 [..]
+[RUNNING] `rustc --crate-name bar bar/src/lib.rs [..]--crate-type lib --emit=[..]link[..]
 [RUNNING] `rustc --crate-name bar bar/src/lib.rs [..]--crate-type lib --emit=[..]link -C panic=abort[..]-C debuginfo=2 [..]
 [COMPILING] somepm [..]
 [RUNNING] `rustc --crate-name somepm [..]
