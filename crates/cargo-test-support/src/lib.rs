9e48c3ddf96278e5591a899f7e0d24ae64184c91
David Laban <alsuren@gmail.com>
2022-11-19 20:08:17 +0000

    Revert "REVERTME: enable trace debugging for tests"
    This reverts commit df6cd75a00b6575bfa9bc6f6f1eba905e863c70c.

--- crates/cargo-test-support/src/lib.rs
+++ crates/cargo-test-support/src/lib.rs
@@ -415,7 +415,6 @@ impl Project {
         if let Some(ref mut p) = execs.process_builder {
             p.env("CARGO", cargo);
             p.env("CARGO_TREE_FROM_UNIT_GRAPH", "1");
-            p.env("CARGO_LOG", "trace");
             p.arg_line(cmd);
         }
         execs
