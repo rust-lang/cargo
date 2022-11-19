1a114480851ef4993689d0af4638a6caa147f679
David Laban <alsuren@gmail.com>
2022-11-19 14:11:54 +0000

    REVERTME: run tests with new Graph builder algorithm

--- crates/cargo-test-support/src/lib.rs
+++ crates/cargo-test-support/src/lib.rs
@@ -414,6 +414,7 @@ impl Project {
         let mut execs = self.process(&cargo);
         if let Some(ref mut p) = execs.process_builder {
             p.env("CARGO", cargo);
+            p.env("CARGO_TREE_FROM_UNIT_GRAPH", "1");
             p.arg_line(cmd);
         }
         execs
