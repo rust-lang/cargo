91d39bc742c9b30684a51214bc847072bb1b1c9c
Eric Huss <eric@huss.org>
2023-04-09 09:11:22 -0700

    Share the check_token function between login and logout tests.

--- tests/testsuite/logout.rs
+++ tests/testsuite/logout.rs
@@ -1,9 +1,8 @@
 //! Tests for the `cargo logout` command.
 
-use cargo_test_support::install::cargo_home;
+use super::login::check_token;
 use cargo_test_support::registry::TestRegistry;
 use cargo_test_support::{cargo_process, registry};
-use std::fs;
 
 #[cargo_test]
 fn gated() {
@@ -21,32 +20,9 @@ the `cargo logout` command.
         .run();
 }
 
-/// Checks whether or not the token is set for the given token.
-fn check_config_token(registry: Option<&str>, should_be_set: bool) {
-    let credentials = cargo_home().join("credentials.toml");
-    let contents = fs::read_to_string(&credentials).unwrap();
-    let toml: toml::Table = contents.parse().unwrap();
-    if let Some(registry) = registry {
-        assert_eq!(
-            toml.get("registries")
-                .and_then(|registries| registries.get(registry))
-                .and_then(|registry| registry.get("token"))
-                .is_some(),
-            should_be_set
-        );
-    } else {
-        assert_eq!(
-            toml.get("registry")
-                .and_then(|registry| registry.get("token"))
-                .is_some(),
-            should_be_set
-        );
-    }
-}
-
 fn simple_logout_test(registry: &TestRegistry, reg: Option<&str>, flag: &str, note: &str) {
     let msg = reg.unwrap_or("crates-io");
-    check_config_token(reg, true);
+    check_token(Some(registry.token()), reg);
     let mut cargo = cargo_process(&format!("logout -Z unstable-options {}", flag));
     if reg.is_none() {
         cargo.replace_crates_io(registry.index_url());
@@ -61,7 +37,7 @@ If you need to revoke the token, visit {note} and follow the instructions there.
 "
         ))
         .run();
-    check_config_token(reg, false);
+    check_token(None, reg);
 
     let mut cargo = cargo_process(&format!("logout -Z unstable-options {}", flag));
     if reg.is_none() {
@@ -71,7 +47,7 @@ If you need to revoke the token, visit {note} and follow the instructions there.
         .masquerade_as_nightly_cargo(&["cargo-logout"])
         .with_stderr(&format!("[LOGOUT] not currently logged in to `{msg}`"))
         .run();
-    check_config_token(reg, false);
+    check_token(None, reg);
 }
 
 #[cargo_test]
@@ -89,4 +65,6 @@ fn other_registry() {
         "--registry alternative",
         "the `alternative` website",
     );
+    // It should not touch crates.io.
+    check_token(Some("sekrit"), None);
 }
