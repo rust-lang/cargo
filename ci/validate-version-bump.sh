7d1b777d96355aae776cc13e763e5e0bd5263b6f
Weihang Lo <me@weihanglo.tw>
2023-07-26 00:42:50 +0100

    ci: integrate `xtask-bump-check`

--- ci/validate-version-bump.sh
+++ ci/validate-version-bump.sh
@@ -16,46 +16,7 @@ set -euo pipefail
 base_sha=$(git rev-parse "${BASE_SHA:-HEAD~1}")
 head_sha=$(git rev-parse "${HEAD_SHA:-HEAD}")
 
-echo "Base branch  is $base_sha"
-echo "Current head is $head_sha"
+echo "Base revision is $base_sha"
+echo "Head revision is $head_sha"
 
-# Gets crate names of members that has been changed from $bash_sha to $head_sha.
-changed_crates=$(
-    git diff --name-only "$base_sha" "$head_sha" -- crates/ credential/ benches/ \
-    | cut  -d'/' -f2 \
-    | sort -u
-)
-
-if  [ -z "$changed_crates" ]
-then
-    echo "No file changed in member crates."
-    exit 0
-fi
-
-# Checks publish status for only crates with code changes.
-publish_status_table=$(
-    echo "$changed_crates" \
-    | xargs printf -- '--package %s\n' \
-    | xargs cargo unpublished
-)
-
-# "yes" -> code changed but no version difference -> need a bump
-# Prints 2nd column (sep by space), which is the name of the crate.
-crates_need_bump=$(
-    echo "$publish_status_table" \
-    | { grep '| yes ' || true; } \
-    | awk '{print $2}'
-)
-
-if  [ -z "$crates_need_bump" ]
-then
-    echo "No version bump needed for member crates."
-    exit 0
-fi
-
-echo "Detected changes in these crates but no version bump found:"
-echo "$crates_need_bump"
-echo
-echo "Please bump at least one patch version for each corresponding Cargo.toml:"
-echo 'Run "cargo unpublished" to read the publish status table for details.'
-exit 1
+cargo bump-check --base-rev "$base_sha" --head-rev "$head_sha"
