c07043b7cf3e2ac33903cb6f2ec14dcfb878ff2f
Weihang Lo <me@weihanglo.tw>
2023-08-13 21:52:57 +0100

    fix(log): enable ansi color only in terminal

--- crates/xtask-bump-check/src/main.rs
+++ crates/xtask-bump-check/src/main.rs
@@ -20,6 +20,7 @@ fn setup_logger() {
     let env = tracing_subscriber::EnvFilter::from_env("CARGO_LOG");
 
     tracing_subscriber::fmt()
+        .with_ansi(std::io::IsTerminal::is_terminal(&std::io::stderr()))
         .with_writer(std::io::stderr)
         .with_env_filter(env)
         .init();
