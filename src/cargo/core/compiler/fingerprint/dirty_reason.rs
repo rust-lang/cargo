a76564b9bab37fafab51328bc84006ddc97cb304
hi-rustin <rustin.liu@gmail.com>
2023-12-07 13:23:46 +0800

    Update the dirty reason message
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/cargo/core/compiler/fingerprint/dirty_reason.rs
+++ src/cargo/core/compiler/fingerprint/dirty_reason.rs
@@ -168,7 +168,7 @@ impl DirtyReason {
             DirtyReason::LocalLengthsChanged => {
                 s.dirty_because(unit, "the local lengths changed")?;
                 s.note(
-                    "This could happen because of added/removed `cargo:rerun-if` instructions in the build script",
+                    "This could happen because of added/removed `cargo::rerun-if` instructions in the build script",
                 )?;
 
                 Ok(())
