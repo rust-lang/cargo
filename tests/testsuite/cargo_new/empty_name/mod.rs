2db5c9dd59673662bf835929bd3156b8f0a66cf4
Ed Page <eopage@gmail.com>
2023-12-13 09:56:30 -0600

    fix(new): Catch empty package names earlier

--- tests/testsuite/cargo_new/empty_name/mod.rs
+++ tests/testsuite/cargo_new/empty_name/mod.rs
@@ -14,7 +14,7 @@ fn case() {
         .args(["foo", "--name", ""])
         .current_dir(cwd)
         .assert()
-        .success()
+        .failure()
         .stdout_matches_path(curr_dir!().join("stdout.log"))
         .stderr_matches_path(curr_dir!().join("stderr.log"));
 
