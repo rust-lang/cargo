d7b811b686cec5e74a83f84eaf6cf533915b853e
Weihang Lo <me@weihanglo.tw>
2023-12-19 05:42:18 -0500

    refactor(util-schemas): error type for `PartialVersion`

--- crates/cargo-util-schemas/src/core/mod.rs
+++ crates/cargo-util-schemas/src/core/mod.rs
@@ -4,5 +4,6 @@ mod source_kind;
 
 pub use package_id_spec::PackageIdSpec;
 pub use partial_version::PartialVersion;
+pub use partial_version::PartialVersionError;
 pub use source_kind::GitReference;
 pub use source_kind::SourceKind;
