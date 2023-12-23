44c69d83d6612c06aa728c98acb663d1e3b29fbe
Weihang Lo <me@weihanglo.tw>
2023-12-23 00:37:43 -0500

    test: demonstrate `target.triple.rustdocflags` doesnt work

--- tests/testsuite/rustdocflags.rs
+++ tests/testsuite/rustdocflags.rs
@@ -1,6 +1,8 @@
 //! Tests for setting custom rustdoc flags.
 
 use cargo_test_support::project;
+use cargo_test_support::rustc_host;
+use cargo_test_support::rustc_host_env;
 
 #[cargo_test]
 fn parses_env() {
@@ -157,5 +159,31 @@ fn not_affected_by_target_rustflags() {
     // `cargo doc` shouldn't fail.
     p.cargo("doc -v")
         .with_stderr_contains("[RUNNING] `rustdoc [..] --cfg foo[..]`")
+        .run();
+}
+
+#[cargo_test]
+fn target_triple_rustdocflags_works() {
+    let host = rustc_host();
+    let host_env = rustc_host_env();
+    let p = project().file("src/lib.rs", "").build();
+
+    // target.triple.rustdocflags in env works
+    p.cargo("doc -v")
+        .env(
+            &format!("CARGO_TARGET_{host_env}_RUSTDOCFLAGS"),
+            "--cfg=foo",
+        )
+        .with_stderr_contains("[RUNNING] `rustdoc[..]--cfg[..]foo[..]`")
+        .run();
+
+    // target.triple.rustdocflags in config is not supported.
+    p.cargo("doc -v")
+        .arg("--config")
+        .arg(format!("target.{host}.rustdocflags=['--cfg', 'foo']"))
+        .with_status(101)
+        .with_stderr(format!(
+            "[ERROR] expected a table, but found a array for `target.{host}.rustdocflags` in --config cli option"
+        ))
         .run();
 }
