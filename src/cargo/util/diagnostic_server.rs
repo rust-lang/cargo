8ffd9cd86f8e901cbefcdff7cc262095f2f95759
Eric Huss <eric@huss.org>
2024-01-02 14:29:12 -0800

    Add tests validating the sequence for `cargo fix`.
    This adds a set of tests which validates various edge cases around how
    `cargo fix` works in terms of calling `rustc` multiple times. This uses
    a replacement of `rustc` so it doesn't depend on the behavior of rustc
    itself which is not always stable.

--- src/cargo/util/diagnostic_server.rs
+++ src/cargo/util/diagnostic_server.rs
@@ -234,7 +234,7 @@ https://doc.rust-lang.org/edition-guide/editions/transitioning-an-existing-proje
 fn gen_please_report_this_bug_text(url: &str) -> String {
     format!(
         "This likely indicates a bug in either rustc or cargo itself,\n\
-     and we would appreciate a bug report! You're likely to see \n\
+     and we would appreciate a bug report! You're likely to see\n\
      a number of compiler warnings after this message which cargo\n\
      attempted to fix but failed. If you could open an issue at\n\
      {}\n\
