0d480df39ae57ba4302b0b39bf662ae1af4e36a8
Weihang Lo <me@weihanglo.tw>
2024-01-08 20:07:29 -0500

    refactor(cli): use format alignment

--- src/bin/cargo/cli.rs
+++ src/bin/cargo/cli.rs
@@ -70,10 +70,9 @@ pub fn main(config: &mut LazyConfig) -> CliResult {
             .iter()
             .map(|(option_name, option_help_message)| {
                 let option_name_kebab_case = option_name.replace("_", "-");
-                let padding = " ".repeat(longest_option - option_name.len()); // safe to subtract
                 format!(
-                    "    -Z {}{} -- {}",
-                    option_name_kebab_case, padding, option_help_message
+                    "    -Z {:<longest_option$} -- {}",
+                    option_name_kebab_case, option_help_message
                 )
             })
             .collect();
