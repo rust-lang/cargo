7b40cb646a37256b6816d3246463a78c9b949161
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-02-26 15:05:33 +0100

    Output dependencies for unit deps.

--- src/cargo/core/compiler/output_sbom.rs
+++ src/cargo/core/compiler/output_sbom.rs
@@ -1,7 +1,10 @@
 //! cargo-sbom precursor files for external tools to create SBOM files from.
 //! See [`output_sbom`] for more.
 
-use std::io::{BufWriter, Write};
+use std::{
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
 
 use cargo_util::paths::{self};
 use cargo_util_schemas::core::PackageIdSpec;
@@ -10,10 +13,11 @@ use serde::Serialize;
 
 use crate::{
     core::{compiler::FileFlavor, profiles::Profile, Target, TargetKind},
+    util::Rustc,
     CargoResult,
 };
 
-use super::{BuildRunner, CrateType, Unit};
+use super::{unit_graph::UnitDep, BuildRunner, CrateType, Unit};
 
 /// Typed version of a SBOM format version number.
 pub struct SbomFormatVersion<const V: u32>;
@@ -24,6 +28,30 @@ impl<const V: u32> Serialize for SbomFormatVersion<V> {
         S: serde::Serializer,
     {
         serializer.serialize_u32(V)
+    }
+}
+
+#[derive(Serialize, Clone, Debug)]
+struct SbomDependency {
+    package_id: PackageIdSpec,
+    features: Vec<String>,
+    extern_crate_name: String,
+}
+
+impl From<&UnitDep> for SbomDependency {
+    fn from(dep: &UnitDep) -> Self {
+        let features = dep
+            .unit
+            .features
+            .iter()
+            .map(|dep| dep.to_string())
+            .collect_vec();
+
+        Self {
+            package_id: dep.unit.pkg.package_id().to_spec(),
+            features,
+            extern_crate_name: dep.extern_crate_name.to_string(),
+        }
     }
 }
 
@@ -47,6 +75,25 @@ impl From<&Target> for SbomTarget {
 }
 
 #[derive(Serialize)]
+struct SbomRustc {
+    version: String,
+    wrapper: Option<PathBuf>,
+    commit_hash: Option<String>,
+    host: String,
+}
+
+impl From<&Rustc> for SbomRustc {
+    fn from(rustc: &Rustc) -> Self {
+        Self {
+            version: rustc.version.to_string(),
+            wrapper: rustc.wrapper.clone(),
+            commit_hash: rustc.commit_hash.clone(),
+            host: rustc.host.to_string(),
+        }
+    }
+}
+
+#[derive(Serialize)]
 struct Sbom {
     format_version: SbomFormatVersion<1>,
     package_id: PackageIdSpec,
@@ -55,12 +102,13 @@ struct Sbom {
     source: String,
     target: SbomTarget,
     profile: Profile,
-    dependencies: Vec<String>,
+    dependencies: Vec<SbomDependency>,
     features: Vec<String>,
+    rustc: SbomRustc,
 }
 
 impl Sbom {
-    pub fn new(unit: &Unit, dependencies: Vec<String>) -> Self {
+    pub fn new(unit: &Unit, dependencies: Vec<SbomDependency>, rustc: SbomRustc) -> Self {
         let package_id = unit.pkg.summary().package_id().to_spec();
         let name = unit.pkg.name().to_string();
         let version = unit.pkg.version().to_string();
@@ -79,6 +127,7 @@ impl Sbom {
             profile,
             dependencies,
             features,
+            rustc,
         }
     }
 }
@@ -86,7 +135,9 @@ impl Sbom {
 /// Saves a `<artifact>.cargo-sbom.json` file for the given [`Unit`].
 ///
 pub fn output_sbom(build_runner: &mut BuildRunner<'_, '_>, unit: &Unit) -> CargoResult<()> {
-    let _bcx = build_runner.bcx;
+    let bcx = build_runner.bcx;
+    let unit_deps = build_runner.unit_deps(unit);
+    let dependencies = unit_deps.iter().map(|dep| dep.into()).collect_vec();
 
     // TODO collect build & unit data, then transform into JSON output
     for output in build_runner
@@ -97,21 +148,14 @@ pub fn output_sbom(build_runner: &mut BuildRunner<'_, '_>, unit: &Unit) -> Cargo
         if let Some(ref link_dst) = output.hardlink {
             let output_path = link_dst.with_extension("cargo-sbom.json");
 
-            let dependencies = find_dependencies_for_unit(build_runner, unit)?;
-            let sbom = Sbom::new(unit, dependencies);
+            let rustc = bcx.rustc().into();
+            let sbom = Sbom::new(unit, dependencies.clone(), rustc);
 
-            let mut outfile = BufWriter::new(paths::create(output_path)?);
+            let mut outfile = BufWriter::new(paths::create(output_path.clone())?);
             let output = serde_json::to_string_pretty(&sbom)?;
             write!(outfile, "{}", output)?;
         }
     }
 
     Ok(())
-}
-
-fn find_dependencies_for_unit(
-    build_runner: &mut BuildRunner<'_, '_>,
-    unit: &Unit,
-) -> CargoResult<Vec<String>> {
-    Ok(Vec::new())
 }
