09836ef789314b3f8af8d61bf11a738294cc9100
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-02-26 12:19:02 +0100

    Add Sbom types to serialize data

--- src/cargo/core/compiler/output_sbom.rs
+++ src/cargo/core/compiler/output_sbom.rs
@@ -1,17 +1,18 @@
 //! cargo-sbom precursor files for external tools to create SBOM files from.
 //! See [`output_sbom`] for more.
 
-use std::{
-    io::{BufWriter, Write},
-    path::Path,
-};
+use std::io::{BufWriter, Write};
 
-use cargo_util::paths::{self, normalize_path};
+use cargo_util::paths::{self};
+use cargo_util_schemas::core::PackageIdSpec;
 use serde::Serialize;
 
-use crate::{core::compiler::FileFlavor, util::internal, CargoResult};
+use crate::{
+    core::{compiler::FileFlavor, Target, TargetKind},
+    CargoResult,
+};
 
-use super::{BuildRunner, Unit};
+use super::{BuildRunner, CrateType, Unit};
 
 /// Typed version of a SBOM format version number.
 pub struct SbomFormatVersion<const V: u32>;
@@ -26,18 +27,61 @@ impl<const V: u32> Serialize for SbomFormatVersion<V> {
 }
 
 #[derive(Serialize)]
-struct Test {
+struct SbomTarget {
+    kind: TargetKind,
+    crate_type: Option<CrateType>,
+    name: String,
+    edition: String,
+}
+
+impl From<&Target> for SbomTarget {
+    fn from(target: &Target) -> Self {
+        SbomTarget {
+            kind: target.kind().clone(),
+            crate_type: target.kind().rustc_crate_types().first().cloned(),
+            name: target.name().to_string(),
+            edition: target.edition().to_string(),
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct Sbom {
     format_version: SbomFormatVersion<1>,
-    package_id: String,
+    package_id: PackageIdSpec,
+    name: String,
+    version: String,
+    target: SbomTarget,
+    dependencies: Vec<String>,
+    features: Vec<String>,
+}
+
+impl Sbom {
+    pub fn new(unit: &Unit) -> Self {
+        let package_id = unit.pkg.summary().package_id().to_spec();
+        let name = unit.pkg.name().to_string();
+        let version = unit.pkg.version().to_string();
+        let features = unit.features.iter().map(|f| f.to_string()).collect();
+        let target: SbomTarget = (&unit.target).into();
+
+        Self {
+            format_version: SbomFormatVersion,
+            package_id,
+            name,
+            version,
+            target,
+            dependencies: Vec::new(),
+            features,
+        }
+    }
 }
 
 /// Saves a `<artifact>.cargo-sbom.json` file for the given [`Unit`].
 ///
-/// TODO add description
 pub fn output_sbom(build_runner: &mut BuildRunner<'_, '_>, unit: &Unit) -> CargoResult<()> {
     let _bcx = build_runner.bcx;
 
-    // TODO collcet build & unit data, then transform into JSON output
+    // TODO collect build & unit data, then transform into JSON output
     for output in build_runner
         .outputs(unit)?
         .iter()
@@ -46,13 +90,10 @@ pub fn output_sbom(build_runner: &mut BuildRunner<'_, '_>, unit: &Unit) -> Cargo
         if let Some(ref link_dst) = output.hardlink {
             let output_path = link_dst.with_extension("cargo-sbom.json");
 
-            let test = Test {
-                format_version: SbomFormatVersion,
-                package_id: "Test".to_string(),
-            };
+            let sbom = Sbom::new(unit);
 
             let mut outfile = BufWriter::new(paths::create(output_path)?);
-            let output = serde_json::to_string_pretty(&test)?;
+            let output = serde_json::to_string_pretty(&sbom)?;
             write!(outfile, "{}", output)?;
         }
     }
