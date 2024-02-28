ca675f97056cc0e246b4132d35d5b583dec33cf8
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-02-28 13:41:56 +0100

    Add test to check project with bin & lib
    * extract sbom config into helper function

--- tests/testsuite/sbom.rs
+++ tests/testsuite/sbom.rs
@@ -1,6 +1,16 @@
 //! Tests for cargo-sbom precursor files.
 
-use cargo_test_support::{basic_bin_manifest, project};
+use cargo_test_support::{basic_bin_manifest, project, ProjectBuilder};
+
+fn configured_project() -> ProjectBuilder {
+    project().file(
+        ".cargo/config.toml",
+        r#"
+            [build]
+            sbom = true
+        "#,
+    )
+}
 
 #[cargo_test]
 fn build_sbom_using_cargo_config() {
@@ -19,7 +29,6 @@ fn build_sbom_using_cargo_config() {
     p.cargo("build").run();
 
     let file = p.bin("foo").with_extension("cargo-sbom.json");
-    dbg!(&file);
     assert!(file.is_file());
 }
 
@@ -34,4 +43,33 @@ fn build_sbom_using_env_var() {
 
     let file = p.bin("foo").with_extension("cargo-sbom.json");
     assert!(file.is_file());
+}
+
+#[cargo_test]
+fn build_sbom_project_bin_and_lib() {
+    let p = configured_project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "foo"
+                version = "1.2.3"
+                authors = []
+
+                [lib]
+                crate-type = ["rlib"]
+            "#,
+        )
+        .file("src/main.rs", r#"fn main() { let _i = foo::give_five(); }"#)
+        .file("src/lib.rs", r#"pub fn give_five() -> i32 { 5 }"#)
+        .build();
+
+    p.cargo("build").stream().run();
+
+    assert!(p.bin("foo").with_extension("cargo-sbom.json").is_file());
+    assert_eq!(
+        1,
+        p.glob(p.target_debug_dir().join("libfoo.cargo-sbom.json"))
+            .count()
+    );
 }
