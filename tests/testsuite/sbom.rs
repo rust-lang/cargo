06bfce1183aa5dc9054a60b5965e288b68466ac3
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-03-11 12:02:02 +0100

    Add test to read JSON
    Still needs to check output.

--- tests/testsuite/sbom.rs
+++ tests/testsuite/sbom.rs
@@ -1,6 +1,14 @@
 //! Tests for cargo-sbom precursor files.
 
+use std::{fs::File, io::BufReader, path::Path};
+
 use cargo_test_support::{basic_bin_manifest, project, ProjectBuilder};
+
+fn read_json<P: AsRef<Path>>(path: P) -> anyhow::Result<serde_json::Value> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    Ok(serde_json::from_reader(reader)?)
+}
 
 fn configured_project() -> ProjectBuilder {
     project().file(
@@ -72,4 +80,33 @@ fn build_sbom_project_bin_and_lib() {
         p.glob(p.target_debug_dir().join("libfoo.cargo-sbom.json"))
             .count()
     );
+}
+
+#[cargo_test]
+fn build_sbom_with_simple_build_script() {
+    let p = configured_project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "foo"
+                version = "0.0.1"
+                authors = []
+                build = "build.rs"
+            "#,
+        )
+        .file("src/main.rs", "#[cfg(foo)] fn main() {}")
+        .file(
+            "build.rs",
+            r#"fn main() { println!("cargo::rustc-cfg=foo"); }"#,
+        )
+        .build();
+
+    p.cargo("build").stream().run();
+
+    let path = p.bin("foo").with_extension("cargo-sbom.json");
+    assert!(path.is_file());
+
+    let json = read_json(path).expect("Failed to read JSON");
+    dbg!(&json);
 }
