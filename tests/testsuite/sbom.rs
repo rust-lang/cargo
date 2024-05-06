f66df4227feb54c6d55118fde7c5c4441ea4c535
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-05-06 14:21:57 +0200

    Expand end-to-end tests
    This expands the tests to reflect end-to-end tests by comparing the
    generated JSON output files with expected strings.
    
    * add test helper to compare actual & expected JSON content
    * refactor setup of packages in test

--- tests/testsuite/sbom.rs
+++ tests/testsuite/sbom.rs
@@ -1,6 +1,27 @@
 //! Tests for cargo-sbom precursor files.
 
-use cargo_test_support::{basic_bin_manifest, project, ProjectBuilder};
+use std::path::PathBuf;
+
+use cargo_test_support::basic_bin_manifest;
+use cargo_test_support::compare;
+use cargo_test_support::project;
+use cargo_test_support::registry::Package;
+use cargo_test_support::ProjectBuilder;
+
+/// Helper function to compare expected JSON output against actual.
+#[track_caller]
+fn assert_json_output(actual_json_file: PathBuf, expected_json: &str) {
+    assert!(actual_json_file.is_file());
+
+    let actual_json = std::fs::read_to_string(actual_json_file).expect("Failed to read file");
+    let actual_json: serde_json::Value =
+        serde_json::from_str(actual_json.as_str()).expect("Failed to parse JSON");
+    let actual_json = serde_json::to_string(&actual_json).expect("Failed to convert JSON");
+
+    if let Err(error) = compare::match_json(expected_json, &actual_json, None) {
+        panic!("{}", error.to_string());
+    }
+}
 
 fn configured_project() -> ProjectBuilder {
     project().file(
@@ -45,7 +66,51 @@ fn build_sbom_using_cargo_config() {
         .run();
 
     let file = p.bin("foo").with_extension("cargo-sbom.json");
-    assert!(file.is_file());
+    assert_json_output(
+        file,
+        r#"
+        {
+            "format_version": 1,
+            "package_id": "path+file:///[..]/foo#0.5.0",
+            "name": "foo",
+            "version": "0.5.0",
+            "source": "[ROOT]/foo",
+            "target": {
+                "kind": [
+                    "bin"
+                ],
+                "crate_type": "bin",
+                "name": "foo",
+                "edition": "2015"
+            },
+            "profile": {
+                "name": "dev",
+                "opt_level": "0",
+                "lto": "false",
+                "codegen_backend": null,
+                "codegen_units": null,
+                "debuginfo": 2,
+                "split_debuginfo": "{...}",
+                "debug_assertions": true,
+                "overflow_checks": true,
+                "rpath": false,
+                "incremental": false,
+                "panic": "unwind",
+                "strip": {
+                    "deferred": "None"
+                }
+            },
+            "packages": [],
+            "features": [],
+            "rustc": {
+                "version": "[..]",
+                "wrapper": null,
+                "commit_hash": "[..]",
+                "host": "[..]"
+            }
+        }
+        "#,
+    );
 }
 
 #[cargo_test]
@@ -89,8 +154,8 @@ fn build_sbom_project_bin_and_lib() {
 
     assert!(p.bin("foo").with_extension("cargo-sbom.json").is_file());
     assert_eq!(
-        1,
-        p.glob(p.target_debug_dir().join("libfoo.cargo-sbom.json"))
+        2,
+        p.glob(p.target_debug_dir().join("*.cargo-sbom.json"))
             .count()
     );
 }
@@ -125,6 +190,28 @@ fn build_sbom_with_simple_build_script() {
 
 #[cargo_test]
 fn build_sbom_with_build_dependencies() {
+    Package::new("baz", "0.1.0").publish();
+    Package::new("bar", "0.1.0")
+        .build_dep("baz", "0.1.0")
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "bar"
+                version = "0.1.0"
+                build = "build.rs"
+
+                [build-dependencies]
+                baz = "0.1.0"
+            "#,
+        )
+        .file("src/lib.rs", "pub fn bar() -> i32 { 2 }")
+        .file(
+            "build.rs",
+            r#"fn main() { println!("cargo::rustc-cfg=foo"); }"#,
+        )
+        .publish();
+
     let p = configured_project()
         .file(
             "Cargo.toml",
@@ -135,27 +222,10 @@ fn build_sbom_with_build_dependencies() {
                 authors = []
 
                 [dependencies]
-                bar = { path = "./bar" }
+                bar = "0.1.0"
             "#,
         )
         .file("src/main.rs", "fn main() { let _i = bar::bar(); }")
-        .file("bar/src/lib.rs", "pub fn bar() -> i32 { 2 }")
-        .file(
-            "bar/Cargo.toml",
-            r#"
-                [package]
-                name = "bar"
-                version = "0.1.0"
-                build = "build.rs"
-
-                [build-dependencies]
-                cc = "1.0.46"
-            "#,
-        )
-        .file(
-            "bar/build.rs",
-            r#"fn main() { println!("cargo::rustc-cfg=foo"); }"#,
-        )
         .build();
 
     p.cargo("build -Zsbom")
@@ -163,5 +233,106 @@ fn build_sbom_with_build_dependencies() {
         .run();
 
     let path = p.bin("foo").with_extension("cargo-sbom.json");
-    assert!(path.is_file());
+    assert_json_output(
+        path,
+        r#"
+        {
+            "format_version": 1,
+            "package_id": "path+file:///[..]/foo#0.0.1",
+            "name": "foo",
+            "version": "0.0.1",
+            "source": "[ROOT]/foo",
+            "target": {
+                "kind": [
+                    "bin"
+                ],
+                "crate_type": "bin",
+                "name": "foo",
+                "edition": "2015"
+            },
+            "profile": {
+                "name": "dev",
+                "opt_level": "0",
+                "lto": "false",
+                "codegen_backend": null,
+                "codegen_units": null,
+                "debuginfo": 2,
+                "split_debuginfo": "{...}",
+                "debug_assertions": true,
+                "overflow_checks": true,
+                "rpath": false,
+                "incremental": false,
+                "panic": "unwind",
+                "strip": {
+                    "deferred": "None"
+                }
+            },
+            "packages": [
+                {
+                    "package_id": "bar 0.1.0 (registry+[..])",
+                    "package": "bar",
+                    "version": "0.1.0",
+                    "features": [],
+                    "build_type": "normal",
+                    "extern_crate_name": "bar",
+                    "dependencies": [
+                        {
+                            "name": "bar",
+                            "package_id": "bar 0.1.0 (registry+[..])",
+                            "version": "0.1.0",
+                            "features": []
+                        }
+                    ]
+                },
+                {
+                    "package_id": "bar 0.1.0 (registry+[..])",
+                    "package": "bar",
+                    "version": "0.1.0",
+                    "features": [],
+                    "build_type": "build",
+                    "extern_crate_name": "build_script_build",
+                    "dependencies": [
+                        {
+                            "name": "bar",
+                            "package_id": "bar 0.1.0 (registry+[..])",
+                            "version": "0.1.0",
+                            "features": []
+                        }
+                    ]
+                },
+                {
+                    "package_id": "bar 0.1.0 (registry+[..])",
+                    "package": "bar",
+                    "version": "0.1.0",
+                    "features": [],
+                    "build_type": "normal",
+                    "extern_crate_name": "build_script_build",
+                    "dependencies": [
+                        {
+                            "name": "baz",
+                            "package_id": "baz 0.1.0 (registry+[..])",
+                            "version": "0.1.0",
+                            "features": []
+                        }
+                    ]
+                },
+                {
+                    "package_id": "baz 0.1.0 (registry+[..])",
+                    "package": "baz",
+                    "version": "0.1.0",
+                    "features": [],
+                    "build_type": "normal",
+                    "extern_crate_name": "baz",
+                    "dependencies": []
+                }
+            ],
+            "features": [],
+            "rustc": {
+                "version": "[..]",
+                "wrapper": null,
+                "commit_hash": "[..]",
+                "host": "[..]"
+            }
+        }"#,
+    );
 }
