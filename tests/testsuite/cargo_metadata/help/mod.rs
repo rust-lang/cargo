d957a1c166a9248bb2ab0eec5bfc89a5b57ab38b
王子凌 <sling1994@live.com>
2024-05-13 17:51:54 +0800

    build: using the latest `glob` crate to adapt to the patterns with windows verbatim DISK prefix

--- tests/testsuite/cargo_metadata/help/mod.rs
+++ tests/testsuite/cargo_metadata/help/mod.rs
@@ -12,3 +12,26 @@ fn case() {
         .stdout_matches(file!["stdout.term.svg"])
         .stderr_matches(str![""]);
 }
+
+#[cfg(windows)]
+#[cargo_test]
+fn windows_verbatim_disk_case() {
+    // `canonicalize` func on Windows will return a path starts with `r"\\?\"`,
+    // which is called as `Verbatim disk prefix`.
+    // See: https://doc.rust-lang.org/std/path/enum.Prefix.html#variant.VerbatimDisk
+    snapbox::cmd::Command::cargo_ui()
+        .arg("metadata")
+        .args([
+            "--manifest-path",
+            std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("Cargo.toml")
+                .canonicalize()
+                .unwrap()
+                .to_str()
+                .unwrap(),
+            "--no-deps",
+            "--offline",
+        ])
+        .assert()
+        .success();
+}
