8b7de1ea816d8da6bcc32e69425e131bfad61a9f
Ed Page <eopage@gmail.com>
2024-06-07 15:45:02 -0500

    feat(test): Auto-redact crate sizes from cargo-package

--- crates/cargo-test-support/src/compare.rs
+++ crates/cargo-test-support/src/compare.rs
@@ -92,6 +92,11 @@ pub fn assert_ui() -> snapbox::Assert {
         regex::Regex::new("Finished.*in (?<redacted>[0-9]+(\\.[0-9]+))s").unwrap(),
     )
     .unwrap();
+    subs.insert(
+        "[FILE_SIZE]",
+        regex::Regex::new("(?<redacted>[0-9]+(\\.[0-9]+)([a-zA-Z]i)?)B").unwrap(),
+    )
+    .unwrap();
     snapbox::Assert::new()
         .action_env(snapbox::assert::DEFAULT_ACTION_ENV)
         .redact_with(subs)
@@ -144,6 +149,11 @@ pub fn assert_e2e() -> snapbox::Assert {
     subs.insert(
         "[ELAPSED]",
         regex::Regex::new("[FINISHED].*in (?<redacted>[0-9]+(\\.[0-9]+))s").unwrap(),
+    )
+    .unwrap();
+    subs.insert(
+        "[FILE_SIZE]",
+        regex::Regex::new("(?<redacted>[0-9]+(\\.[0-9]+)([a-zA-Z]i)?)B").unwrap(),
     )
     .unwrap();
     snapbox::Assert::new()
