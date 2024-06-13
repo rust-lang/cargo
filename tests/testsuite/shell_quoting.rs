3a19de7638d13783ec9650abd24e5ff1ae8a38e1
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-06-13 20:09:17 +0800

    refactor: apply suggestions from code review
    Co-authored-by: Weihang Lo <weihanglo@users.noreply.github.com>

--- tests/testsuite/shell_quoting.rs
+++ tests/testsuite/shell_quoting.rs
@@ -27,21 +27,14 @@ fn features_are_quoted() {
     p.cargo("check -v")
         .env("MSYSTEM", "1")
         .with_status(101)
-        .with_stderr_data(str![])
         .with_stderr_data(str![[r#"
 [CHECKING] foo v0.1.0 ([ROOT]/foo)
-[RUNNING] `rustc --crate-name foo --edition=2015 src/main.rs --error-format=json --json=diagnostic-rendered-ansi,artifacts,future-incompat --crate-type bin --emit=dep-info,metadata -C embed-bitcode=no -C debuginfo=2 --cfg 'feature="default"' --cfg 'feature="some_feature"' -C metadata=c2925e00c1458fcb -C extra-filename=-c2925e00c1458fcb --out-dir [ROOT]/foo/target/debug/deps -L dependency=[ROOT]/foo/target/debug/deps`
-error[E0425]: cannot find value `error` in this scope
- --> src/main.rs:1:12
-  |
-1 | fn main() {error}
-  |            ^^^^^ not found in this scope
-
-For more information about this error, try `rustc --explain E0425`.
+[RUNNING] `rustc [..] --cfg 'feature="default"' --cfg 'feature="some_feature"' [..]`
+...
 [ERROR] could not compile `foo` (bin "foo") due to 1 previous error
 
 Caused by:
-  process didn't exit successfully: `rustc --crate-name foo --edition=2015 src/main.rs --error-format=json --json=diagnostic-rendered-ansi,artifacts,future-incompat --crate-type bin --emit=dep-info,metadata -C embed-bitcode=no -C debuginfo=2 --cfg 'feature="default"' --cfg 'feature="some_feature"' -C metadata=c2925e00c1458fcb -C extra-filename=-c2925e00c1458fcb --out-dir [ROOT]/foo/target/debug/deps -L dependency=[ROOT]/foo/target/debug/deps` (exit status: 1)
+  process didn't exit successfully: [..] --cfg 'feature="default"' --cfg 'feature="some_feature"' [..]
 
 "#]])
         .run();
