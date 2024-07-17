9d164aa23e248975ef538b3edbdfd6b373558646
Rustin170506 <29879298+hi-rustin@users.noreply.github.com>
2024-07-17 20:50:31 +0800

    test: migrate all test from cargo-information

---
+++ tests/testsuite/cargo_info/with_frozen_within_ws/mod.rs
@@ -0,0 +1,37 @@
+use cargo_test_support::prelude::*;
+use cargo_test_support::{compare::assert_ui, current_dir, file, Project};
+
+use super::init_registry_without_token;
+
+#[cargo_test]
+fn case() {
+    init_registry_without_token();
+    for ver in [
+        "0.1.1+my-package",
+        "0.2.0+my-package",
+        "0.2.3+my-package",
+        "0.4.1+my-package",
+        "20.0.0+my-package",
+        "99999.0.0+my-package",
+        "99999.0.0-alpha.1+my-package",
+    ] {
+        cargo_test_support::registry::Package::new("my-package", ver).publish();
+    }
+
+    let project = Project::from_template(current_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    snapbox::cmd::Command::cargo_ui()
+        .arg("info")
+        .arg("unknown")
+        .arg("--frozen")
+        .arg("--registry=dummy-registry")
+        .current_dir(cwd)
+        .assert()
+        .failure()
+        .stdout_eq("")
+        .stderr_eq(file!["stderr.term.svg"]);
+
+    assert_ui().subset_matches(current_dir!().join("out"), &project_root);
+}
