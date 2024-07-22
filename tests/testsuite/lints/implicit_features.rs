363ddd4f583ca0a141eacfc0a8238f33b4204b72
Lin Yihai <linyihai@huawei.com>
2024-07-22 15:58:25 +0800

    test: The latest rustc version should not appear in the test output

--- tests/testsuite/lints/implicit_features.rs
+++ tests/testsuite/lints/implicit_features.rs
@@ -127,7 +127,7 @@ unused_optional_dependency = "allow"
         .masquerade_as_nightly_cargo(&["cargo-lints", "edition2024"])
         .with_stderr_data(str![[r#"
 [UPDATING] `dummy-registry` index
-[LOCKING] 2 packages to latest Rust 1.81.0-nightly compatible versions
+[LOCKING] 2 packages to latest Rust [..] compatible versions
 [CHECKING] foo v0.1.0 ([ROOT]/foo)
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
 
