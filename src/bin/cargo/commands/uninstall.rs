954581bdfb5c3c80b979a6d77e594fa6d14147d8
shannmu <shanmu1901@gmail.com>
2024-08-12 22:13:55 +0800

    feat: Add custom completer for completing installed binaries

--- src/bin/cargo/commands/uninstall.rs
+++ src/bin/cargo/commands/uninstall.rs
@@ -5,7 +5,14 @@ use cargo::ops;
 pub fn cli() -> Command {
     subcommand("uninstall")
         .about("Remove a Rust binary")
-        .arg(Arg::new("spec").value_name("SPEC").num_args(0..))
+        .arg(
+            Arg::new("spec")
+                .value_name("SPEC")
+                .num_args(0..)
+                .add::<clap_complete::ArgValueCandidates>(clap_complete::ArgValueCandidates::new(
+                    || get_installed_crates(),
+                )),
+        )
         .arg(opt("root", "Directory to uninstall packages from").value_name("DIR"))
         .arg_silent_suggestion()
         .arg_package_spec_simple("Package to uninstall")
@@ -36,4 +43,26 @@ pub fn exec(gctx: &mut GlobalContext, args: &ArgMatches) -> CliResult {
         .collect();
     ops::uninstall(root, specs, &values(args, "bin"), gctx)?;
     Ok(())
+}
+
+fn get_installed_crates() -> Vec<clap_complete::CompletionCandidate> {
+    get_installed_crates_().unwrap_or_default()
+}
+
+fn get_installed_crates_() -> Option<Vec<clap_complete::CompletionCandidate>> {
+    let mut candidates = Vec::new();
+
+    let gctx = GlobalContext::default().ok()?;
+
+    let root = ops::resolve_root(None, &gctx).ok()?;
+
+    let tracker = ops::InstallTracker::load(&gctx, &root).ok()?;
+
+    for (_, v) in tracker.all_installed_bins() {
+        for bin in v {
+            candidates.push(clap_complete::CompletionCandidate::new(bin));
+        }
+    }
+
+    Some(candidates)
 }
