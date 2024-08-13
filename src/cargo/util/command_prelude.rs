13c4c130e8560348dddfaf9123c287700a420e2c
shannmu <shanmu1901@gmail.com>
2024-08-13 22:38:01 +0800

    feat: Add custom completer for completing example names

--- src/cargo/util/command_prelude.rs
+++ src/cargo/util/command_prelude.rs
@@ -188,7 +188,10 @@ pub trait CommandExt: Sized {
             ._arg(flag("examples", examples).help_heading(heading::TARGET_SELECTION))
             ._arg(
                 optional_multi_opt("example", "NAME", example)
-                    .help_heading(heading::TARGET_SELECTION),
+                    .help_heading(heading::TARGET_SELECTION)
+                    .add(clap_complete::ArgValueCandidates::new(
+                        get_example_candidates,
+                    )),
             )
     }
 
@@ -206,7 +209,11 @@ pub trait CommandExt: Sized {
         )
         ._arg(flag("bins", bins).help_heading(heading::TARGET_SELECTION))
         ._arg(
-            optional_multi_opt("example", "NAME", example).help_heading(heading::TARGET_SELECTION),
+            optional_multi_opt("example", "NAME", example)
+                .help_heading(heading::TARGET_SELECTION)
+                .add(clap_complete::ArgValueCandidates::new(
+                    get_example_candidates,
+                )),
         )
         ._arg(flag("examples", examples).help_heading(heading::TARGET_SELECTION))
     }
@@ -218,7 +225,11 @@ pub trait CommandExt: Sized {
                 .add(clap_complete::ArgValueCandidates::new(get_bin_candidates)),
         )
         ._arg(
-            optional_multi_opt("example", "NAME", example).help_heading(heading::TARGET_SELECTION),
+            optional_multi_opt("example", "NAME", example)
+                .help_heading(heading::TARGET_SELECTION)
+                .add(clap_complete::ArgValueCandidates::new(
+                    get_example_candidates,
+                )),
         )
     }
 
@@ -1046,6 +1057,17 @@ pub fn lockfile_path(
     }
 
     return Ok(Some(path));
+}
+
+fn get_example_candidates() -> Vec<clap_complete::CompletionCandidate> {
+    get_targets_from_metadata()
+        .unwrap_or_default()
+        .into_iter()
+        .filter_map(|target| match target.kind() {
+            TargetKind::ExampleBin => Some(clap_complete::CompletionCandidate::new(target.name())),
+            _ => None,
+        })
+        .collect::<Vec<_>>()
 }
 
 fn get_bench_candidates() -> Vec<clap_complete::CompletionCandidate> {
