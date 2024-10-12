b9d09f0a8f1ed0b29eece424c0ba5e0520c27e7b
shannmu <shanmu1901@gmail.com>
2024-10-12 18:08:49 +0800

    test: Add test case for `get_registry_candidates`

--- tests/testsuite/shell_completions/mod.rs
+++ tests/testsuite/shell_completions/mod.rs
@@ -1,4 +1,22 @@
-#![allow(unused)]
+use std::path::PathBuf;
+
+use cargo::util::command_prelude::*;
+use cargo_test_support::cargo_test;
+
+#[cargo_test]
+fn test_get_registry_candidates() {
+    let current_dir = std::env::current_dir().expect("Failed to get current directory");
+    let cwd = PathBuf::from(file!()).parent().unwrap().join("template");
+    let cwd = current_dir.join(cwd);
+
+    let expected = snapbox::str![
+        "my-registry1
+my-registry2"
+    ];
+    let actual = print_candidates(get_registry_candidates(Some(cwd)).unwrap());
+    snapbox::assert_data_eq!(actual, expected);
+}
+
 fn print_candidates(candidates: Vec<clap_complete::CompletionCandidate>) -> String {
     candidates
         .into_iter()
