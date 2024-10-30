7d6889b5d903f554cc8aae1a361443e53aab4e2f
Alexander Saites <alexander@saites.dev>
2024-10-29 20:33:06 -0700

    replace special-case duplicate handling with error

--- crates/rustfix/src/error.rs
+++ crates/rustfix/src/error.rs
@@ -11,9 +11,15 @@ pub enum Error {
     #[error("invalid range {0:?}, original data is only {1} byte long")]
     DataLengthExceeded(Range<usize>, usize),
 
-    #[non_exhaustive] // There are plans to add fields to this variant at a later time.
+    #[non_exhaustive]
     #[error("cannot replace slice of data that was already replaced")]
-    AlreadyReplaced,
+    AlreadyReplaced {
+        /// The location of the intended replacement.
+        range: Range<usize>,
+        /// Whether the modification exactly matches (both range and data) the one it conflicts with.
+        /// Some clients may wish to simply ignore this condition.
+        is_identical: bool,
+    },
 
     #[error(transparent)]
     Utf8(#[from] std::string::FromUtf8Error),
