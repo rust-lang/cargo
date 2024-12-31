24dd205d5a395ef9c462d1bc4599df3fb2c87f9a
Weihang Lo <me@weihanglo.tw>
2024-12-31 13:32:11 -0500

    fix(package): deduplicate dirty symlink detection
    metdata path fields may point to a dirty symlilnk and cause
    duplicate report. This commit combines `dirty_metadata_paths`
    and `dirty_symlinks` into one so is able to de-duplicate them.

--- tests/testsuite/package.rs
+++ tests/testsuite/package.rs
@@ -1378,10 +1378,9 @@ fn dirty_file_outside_pkg_root_considered_dirty() {
     p.cargo("package --workspace --no-verify")
         .with_status(101)
         .with_stderr_data(str![[r#"
-[ERROR] 5 files in the working directory contain changes that were not yet committed into git:
+[ERROR] 4 files in the working directory contain changes that were not yet committed into git:
 
 LICENSE
-README.md
 README.md
 lib.rs
 original-dir/file
