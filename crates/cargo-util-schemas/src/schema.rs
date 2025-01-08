e6114c3395e5f4a2ebfb26324c50d1ab072959db
Ed Page <eopage@gmail.com>
2025-01-08 11:17:34 -0600

    fix(schema): Include lints sub-tables in packages
    This was done by upgrading schemars.  Hard to tell what else was changed
    because of the noise from the other changes in the layout.
    
    Fixes #15030

--- crates/cargo-util-schemas/src/schema.rs
+++ crates/cargo-util-schemas/src/schema.rs
@@ -2,19 +2,17 @@ use schemars::JsonSchema;
 
 use serde::{Deserialize, Serialize};
 
-use std::string::String;
-
 use toml::Value as TomlValue;
 
 #[derive(Serialize, Deserialize)]
 pub struct TomlValueWrapper(pub TomlValue);
 
 impl JsonSchema for TomlValueWrapper {
-    fn schema_name() -> String {
-        "TomlValue".to_string()
+    fn schema_name() -> std::borrow::Cow<'static, str> {
+        "TomlValue".into()
     }
 
-    fn json_schema(gen: &mut schemars::gen::SchemaGenerator) -> schemars::schema::Schema {
+    fn json_schema(gen: &mut schemars::SchemaGenerator) -> schemars::Schema {
         // HACK: this is both more and less permissive than `TomlValue` but its close
         gen.subschema_for::<serde_json::Value>().into()
     }
