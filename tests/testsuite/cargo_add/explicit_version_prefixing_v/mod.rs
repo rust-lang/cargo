b208cca0831c7a52f2afcfdfb2c1fed8768517b9
DeveloperC <DeveloperC@protonmail.com>
2025-03-08 12:08:54 +0000

    test: add v prefixed semver failing tests

---
+++ tests/testsuite/cargo_add/explicit_version_prefixing_v/mod.rs
@@ -0,0 +1,29 @@
+use cargo_test_support::compare::assert_ui;
+use cargo_test_support::current_dir;
+use cargo_test_support::file;
+use cargo_test_support::prelude::*;
+use cargo_test_support::str;
+use cargo_test_support::Project;
+
+#[cargo_test]
+fn case() {
+    cargo_test_support::registry::init();
+    for ver in ["0.1.1+my-package", "0.2.0+my-package", "0.2.3+my-package"] {
+        cargo_test_support::registry::Package::new("my-package", ver).publish();
+    }
+
+    let project = Project::from_template(current_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    snapbox::cmd::Command::cargo_ui()
+        .arg("add")
+        .arg_line("my-package@v0.2.0")
+        .current_dir(cwd)
+        .assert()
+        .failure()
+        .stdout_eq(str![""])
+        .stderr_eq(file!["stderr.term.svg"]);
+
+    assert_ui().subset_matches(current_dir!().join("out"), &project_root);
+}
