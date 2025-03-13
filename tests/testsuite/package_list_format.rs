a0a6d476d86b92dc449f6d2ad36a145089173205
Weihang Lo <me@weihanglo.tw>
2025-03-13 15:58:18 -0400

    test(package): add test for symlink copying

--- tests/testsuite/package_list_format.rs
+++ tests/testsuite/package_list_format.rs
@@ -3,6 +3,7 @@
 use cargo_test_support::prelude::*;
 use cargo_test_support::project;
 use cargo_test_support::str;
+use cargo_test_support::symlink_supported;
 
 #[cargo_test]
 fn gated() {
@@ -103,6 +104,51 @@ fn workspace() {
 {
   "path+[ROOTURL]/foo/gondor#0.0.0": "",
   "path+[ROOTURL]/foo/rohan#0.0.0": ""
+}
+"#]].is_json())
+        .run();
+}
+
+#[cargo_test]
+fn show_copied_files() {
+    if !symlink_supported() {
+        return;
+    }
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [workspace]
+                members = ["gondor"]
+            "#,
+        )
+        .file("lib.rs", "")
+        .file("LICENSE", "")
+        .file("README.md", "")
+        .file(
+            "gondor/Cargo.toml",
+            r#"
+                [package]
+                name = "gondor"
+                edition = "2015"
+                description = "foo"
+                documentation = "foo"
+                license-file = "../LICENSE"
+            "#,
+        )
+        .file("gondor/main.rs", "fn main() {}")
+        .symlink("lib.rs", "gondor/src/lib.rs")
+        .symlink("README.md", "gondor/README.md")
+        .file("original-dir/file", "")
+        .symlink_dir("original-dir", "gondor/symlink-dir")
+        .build();
+
+    p.cargo("package --list=json -Zunstable-options")
+        .masquerade_as_nightly_cargo(&["package --list=json"])
+        .with_stderr_data(str![""])
+        .with_stdout_data(str![[r#"
+{
+  "path+[ROOTURL]/foo/gondor#0.0.0": ""
 }
 "#]].is_json())
         .run();
