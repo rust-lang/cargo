5fedc01ef8d48fbc1ad186b02c8fb75ed2e2a0f4
Weihang Lo <me@weihanglo.tw>
2025-03-21 13:14:33 -0400

    fix: revert the behavior checking lockfile's VCS
    Lockfile might be gitignore'd,
    So it never shows up in `git status`.
    
    However, `cargo packag` vcs checks actually performs
    `git status --untracked --ignored`.
    It is a bit confusing that lockfile is ignored but still counts as dirty
    from the report of `cargo package`.
    
    There are some more nuances:
    We check lockfile's VCS status if the lockfile is ouside the current
    package root. That means a non-workspace Cargo package will not have
    this VCS check. I don't think it is good that we have diverged behaviors
    Hence this revert.
    
    We can always re-evaluate later,
    as we've reserved rooms for doing more dirty checks.
    
    https://github.com/rust-lang/cargo/issues/14967#issuecomment-2651329783

--- src/cargo/ops/cargo_package/vcs.rs
+++ src/cargo/ops/cargo_package/vcs.rs
@@ -11,7 +11,6 @@ use tracing::debug;
 
 use crate::core::Package;
 use crate::core::Workspace;
-use crate::ops::lockfile::LOCKFILE_NAME;
 use crate::sources::PathEntry;
 use crate::CargoResult;
 use crate::GlobalContext;
@@ -263,41 +262,12 @@ fn dirty_files_outside_pkg_root(
         .map(|path| paths::normalize_path(&pkg_root.join(path)))
         .collect();
 
-    // Unlike other files, lockfile is allowed to be missing,
-    // and can be generated during packaging.
-    // We skip checking when it is missing in both workdir and git index,
-    // otherwise cargo will fail with git2 not found error.
-    let lockfile_path = ws.lock_root().as_path_unlocked().join(LOCKFILE_NAME);
-    let lockfile_path = if lockfile_path.exists() {
-        Some(lockfile_path)
-    } else if let Ok(rel_path) = paths::normalize_path(&lockfile_path).strip_prefix(workdir) {
-        // We don't canonicalize here because non-existing path can't be canonicalized.
-        match repo.status_file(&rel_path) {
-            Ok(s) if s != git2::Status::CURRENT => {
-                dirty_files.insert(lockfile_path);
-            }
-            // Unmodified
-            Ok(_) => {}
-            Err(e) => {
-                debug!(
-                    "check git status failed for `{}` in workdir `{}`: {e}",
-                    rel_path.display(),
-                    workdir.display(),
-                );
-            }
-        }
-        None
-    } else {
-        None
-    };
-
     for rel_path in src_files
         .iter()
         .filter(|p| p.is_symlink_or_under_symlink())
         .map(|p| p.as_ref().as_path())
         .chain(metadata_paths.iter().map(AsRef::as_ref))
         .chain([ws.root_manifest()])
-        .chain(lockfile_path.as_deref().into_iter())
         // If inside package root. Don't bother checking git status.
         .filter(|p| paths::strip_prefix_canonical(p, pkg_root).is_err())
         // Handle files outside package root but under git workdir,
