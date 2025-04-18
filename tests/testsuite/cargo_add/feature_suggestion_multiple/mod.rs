16c5947a06e6d76aec6a1a4bb1222ab508a9ac1e
Jujumba <far77578@gmail.com>
2025-04-18 20:51:07 +0200

    test(add): add tests for feature suggestions

---
+++ tests/testsuite/cargo_add/feature_suggestion_multiple/mod.rs
@@ -0,0 +1,24 @@
+use cargo_test_support::current_dir;
+use cargo_test_support::file;
+use cargo_test_support::prelude::*;
+use cargo_test_support::Project;
+
+#[cargo_test]
+fn case() {
+    let project = Project::from_template(current_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    cargo_test_support::registry::Package::new("my-package", "0.1.0+my-package")
+        .feature("bar", &[])
+        .feature("foo", &[])
+        .publish();
+
+    snapbox::cmd::Command::cargo_ui()
+        .arg("add")
+        .arg_line("my-package --features baz --features feo")
+        .current_dir(cwd)
+        .assert()
+        .failure()
+        .stderr_eq(file!["stderr.term.svg"]);
+}
