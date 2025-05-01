54448a1a19aefa9a06748f1cc14e2e647e2f9c4a
Joe Neeman <joe.neeman@tweag.io>
2025-05-01 13:38:02 +0700

    Only check for the registry if package-workspace is turned on.
    Without this, the upcoming change to stop stripping versioned
    dev-dependencies breaks the unpublished_cyclic_dev_dependencies test
    because the test setup replaces the registry.

--- src/cargo/ops/cargo_package/mod.rs
+++ src/cargo/ops/cargo_package/mod.rs
@@ -239,18 +239,17 @@ fn do_package<'a>(
     let deps = local_deps(pkgs.iter().map(|(p, f)| ((*p).clone(), f.clone())));
     let just_pkgs: Vec<_> = pkgs.iter().map(|p| p.0).collect();
 
-    // The publish registry doesn't matter unless there are local dependencies,
-    // so only try to get one if we need it. If they explicitly passed a
-    // registry on the CLI, we check it no matter what.
-    let sid = if deps.has_no_dependencies() && opts.reg_or_index.is_none() {
-        None
-    } else {
-        let sid = get_registry(ws.gctx(), &just_pkgs, opts.reg_or_index.clone())?;
-        debug!("packaging for registry {}", sid);
-        Some(sid)
-    };
-
     let mut local_reg = if ws.gctx().cli_unstable().package_workspace {
+        // The publish registry doesn't matter unless there are local dependencies,
+        // so only try to get one if we need it. If they explicitly passed a
+        // registry on the CLI, we check it no matter what.
+        let sid = if deps.has_no_dependencies() && opts.reg_or_index.is_none() {
+            None
+        } else {
+            let sid = get_registry(ws.gctx(), &just_pkgs, opts.reg_or_index.clone())?;
+            debug!("packaging for registry {}", sid);
+            Some(sid)
+        };
         let reg_dir = ws.build_dir().join("package").join("tmp-registry");
         sid.map(|sid| TmpRegistry::new(ws.gctx(), reg_dir, sid))
             .transpose()?
