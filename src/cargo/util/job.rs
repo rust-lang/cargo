9d710a98a16f8d859cfec830043659bdc5748e1e
Eric Huss <eric@huss.org>
2025-07-05 19:29:43 -0700

    Apply unsafe_op_in_unsafe_fn

--- src/cargo/util/job.rs
+++ src/cargo/util/job.rs
@@ -36,7 +36,10 @@ mod imp {
         // ALLOWED: For testing cargo itself only.
         #[allow(clippy::disallowed_methods)]
         if env::var("__CARGO_TEST_SETSID_PLEASE_DONT_USE_ELSEWHERE").is_ok() {
-            libc::setsid();
+            // SAFETY: I'm unaware of any safety requirements for this function.
+            unsafe {
+                libc::setsid();
+            }
         }
         Some(())
     }
