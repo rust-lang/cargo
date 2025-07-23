8be51ef2d66e5febf7ef0c75968bb76f4bac448a
Naman Garg <ngarg1270@gmail.com>
2025-07-23 23:45:27 +0530

    Add test for multiple build scripts in different order

--- tests/testsuite/build_scripts_multiple.rs
+++ tests/testsuite/build_scripts_multiple.rs
@@ -609,6 +609,85 @@ Hello, from Build Script 2!
 }
 
 #[cargo_test]
+fn build_script_with_conflicts_reverse_sorted() {
+    // In this, multiple scripts create file with same name in their respective OUT_DIR.
+    // It is different from above because `package.build` is not sorted in this.
+
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                cargo-features = ["multiple-build-scripts"]
+
+                [package]
+                name = "foo"
+                version = "0.1.0"
+                edition = "2024"
+
+                build = ["build2.rs", "build1.rs"]
+            "#,
+        )
+        // OUT_DIR is set to the lexicographically largest build script's OUT_DIR by default
+        .file(
+            "src/main.rs",
+            r#"
+                include!(concat!(env!("OUT_DIR"), "/foo.rs"));
+                fn main() {
+                    println!("{}", message());
+                }
+            "#,
+        )
+        .file(
+            "build1.rs",
+            r#"
+            use std::env;
+            use std::fs;
+            use std::path::Path;
+
+            fn main() {
+                let out_dir = env::var_os("OUT_DIR").unwrap();
+                let dest_path = Path::new(&out_dir).join("foo.rs");
+                fs::write(
+                    &dest_path,
+                    "pub fn message() -> &'static str {
+                        \"Hello, from Build Script 1!\"
+                    }
+                    "
+                ).unwrap();
+             }"#,
+        )
+        .file(
+            "build2.rs",
+            r#"
+            use std::env;
+            use std::fs;
+            use std::path::Path;
+
+            fn main() {
+                let out_dir = env::var_os("OUT_DIR").unwrap();
+                let dest_path = Path::new(&out_dir).join("foo.rs");
+                fs::write(
+                    &dest_path,
+                    "pub fn message() -> &'static str {
+                        \"Hello, from Build Script 2!\"
+                    }
+                    "
+                ).unwrap();
+             }"#,
+        )
+        .build();
+
+    p.cargo("run -v")
+        .masquerade_as_nightly_cargo(&["multiple-build-scripts"])
+        .with_status(0)
+        .with_stdout_data(str![[r#"
+Hello, from Build Script 2!
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
 fn rerun_untracks_other_files() {
     let p = project()
         .file(
