b43404de7ef593b9f8e2ad4c4602988aa1122d70
Weihang Lo <me@weihanglo.tw>
2025-08-16 07:18:21 -0400

    feat(build-analysis): build.analysis configuration
    When enabled, set timing output format to HTML to pretend
    we are persisting timing data for now.

--- tests/testsuite/build_analysis.rs
+++ tests/testsuite/build_analysis.rs
@@ -17,6 +17,7 @@ fn gated() {
         .env("CARGO_BUILD_ANALYSIS_ENABLED", "true")
         .masquerade_as_nightly_cargo(&["build-analysis"])
         .with_stderr_data(str![[r#"
+[WARNING] ignoring 'build.analysis' config, pass `-Zbuild-analysis` to enable it
 [CHECKING] foo v0.0.0 ([ROOT]/foo)
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
 
@@ -36,6 +37,7 @@ fn simple() {
         .masquerade_as_nightly_cargo(&["build-analysis"])
         .with_stderr_data(str![[r#"
 [CHECKING] foo v0.0.0 ([ROOT]/foo)
+      Timing report saved to [ROOT]/foo/target/cargo-timings/cargo-timing-[..].html
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
 
 "#]])
