b54b92cc5f6f375423da3f1bc81af93daa0902ee
Ed Page <eopage@gmail.com>
2025-08-26 15:12:19 -0500

    test(add): Ensure an empty frontmatter is handled properly
    There are corner cases with empty frontmatters, so I wanted to ensure
    this case works now and going forward

--- tests/testsuite/cargo_add/mod.rs
+++ tests/testsuite/cargo_add/mod.rs
@@ -149,6 +149,7 @@ mod rustc_older;
 mod script_bare;
 mod script_escape;
 mod script_frontmatter;
+mod script_frontmatter_empty;
 mod script_shebang;
 mod sorted_table_with_dotted_item;
 mod symlink;
