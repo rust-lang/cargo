6bcb08c6cb2593af33cdd9deb5cc8f48e2ef1079
Mads Marquart <mads@marquart.dk>
2025-09-04 17:36:29 +0200

    feat: Don't check antivirus in cargo install

--- src/cargo/ops/cargo_compile/mod.rs
+++ src/cargo/ops/cargo_compile/mod.rs
@@ -561,9 +561,14 @@ where `<compatible-ver>` is the latest version supporting rustc {rustc_version}"
     }
 
     if build_config.detect_antivirus {
-        if let Err(err) = detect_antivirus::detect_and_report(gctx) {
-            // Errors in this detection are not fatal.
-            tracing::error!("failed detecting whether binaries may be slow to run: {err}");
+        // We don't want to do this check when installing, since there might
+        // be `cargo install` users who are not necessarily developers (and so
+        // the note will be irrelevant to them).
+        if build_config.intent != UserIntent::Install {
+            if let Err(err) = detect_antivirus::detect_and_report(gctx) {
+                // Errors in this detection are not fatal.
+                tracing::error!("failed detecting whether binaries may be slow to run: {err}");
+            }
         }
     }
 
