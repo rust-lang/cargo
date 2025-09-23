a5c8b96f824af5b7175b4fb21d817d8ff87fbf59
Weihang Lo <me@weihanglo.tw>
2025-09-23 19:56:49 -0400

    fix: use `host-tuple` for host target subsitution
    The "host" string is ambiguous
    
    * We have `-Zhost-config` that config `[host]` table applies to
      artifacts running on host, such as build scripts and proc macros.
    * `host` sounds like the default behavior, whereas `--target host`
      is in the cross-compilation mode: `target/<triple>/debug`.
    * We might want to reserve `host` for future use

--- src/cargo/util/command_prelude.rs
+++ src/cargo/util/command_prelude.rs
@@ -1263,10 +1263,12 @@ fn get_target_triples() -> Vec<clap_complete::CompletionCandidate> {
         }
     }
 
-    // Allow tab-completion for `host` as the desired target.
-    candidates.push(clap_complete::CompletionCandidate::new("host").help(Some(
-        concat!("alias for: ", env!("RUST_HOST_TARGET")).into(),
-    )));
+    // Allow tab-completion for `host-tuple` as the desired target.
+    candidates.push(
+        clap_complete::CompletionCandidate::new("host-tuple").help(Some(
+            concat!("alias for: ", env!("RUST_HOST_TARGET")).into(),
+        )),
+    );
 
     candidates
 }
