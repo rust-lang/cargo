f63461e986191784c9d4d0de7af6b80e7e81f409
omskscream <golovkov93@gmail.com>
2025-09-24 21:08:14 +0300

    test (publish): add cases for publishing without providing token explicitly when `login` is run before `publish`
    (login uses token creds for simplicity)

--- tests/testsuite/publish.rs
+++ tests/testsuite/publish.rs
@@ -4551,3 +4551,98 @@ See https://doc.rust-lang.org/cargo/reference/manifest.html#package-metadata for
 "##]])
         .run();
 }
+
+#[cargo_test]
+fn publish_works_without_token_specified_when_login_is_made_before() {
+    let registry = RegistryBuilder::new()
+        .alternative_named("i-need-token")
+        .http_api()
+        .token(registry::Token::Plaintext("TOKEN".to_string()))
+        .auth_required()
+        .build();
+
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "foo"
+                version = "0.0.1"
+                edition = "2024"
+            "#,
+        )
+        .file("src/lib.rs", "")
+        .build();
+    p.cargo("login -v")
+        .replace_crates_io(registry.index_url())
+        .with_status(0)
+        .with_stdin("TOKEN")
+        .run();
+
+    p.cargo("publish --no-verify")
+        .replace_crates_io(registry.index_url())
+        .with_status(0)
+        .with_stderr_data(str![[r##"
+[UPDATING] crates.io index
+[WARNING] manifest has no description, license, license-file, documentation, homepage or repository.
+See https://doc.rust-lang.org/cargo/reference/manifest.html#package-metadata for more info.
+[PACKAGING] foo v0.0.1 ([ROOT]/foo)
+[PACKAGED] 4 files, [FILE_SIZE]B ([FILE_SIZE]B compressed)
+[UPLOADING] foo v0.0.1 ([ROOT]/foo)
+[UPLOADED] foo v0.0.1 to registry `crates-io`
+[NOTE] waiting for foo v0.0.1 to be available at registry `crates-io`
+[HELP] you may press ctrl-c to skip waiting; the crate should be available shortly
+[PUBLISHED] foo v0.0.1 at registry `crates-io`
+
+"##]])
+        .run();
+}
+
+#[cargo_test]
+fn publish_fails_without_token_specified_when_login_failed_before() {
+    let registry = RegistryBuilder::new()
+        .alternative_named("i-need-token")
+        .http_api()
+        .token(registry::Token::Plaintext("TOKEN".to_string()))
+        .auth_required()
+        .build();
+
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "foo"
+                version = "0.0.1"
+                edition = "2024"
+            "#,
+        )
+        .file("src/lib.rs", "")
+        .build();
+    p.cargo("login -v")
+        .replace_crates_io(registry.index_url())
+        .with_status(101)
+        .with_stderr_data(str![[r##"
+[UPDATING] crates.io index
+[CREDENTIAL] cargo:token get crates-io
+[CREDENTIAL] cargo:token login crates-io
+please paste the token for crates-io below
+[ERROR] credential provider `cargo:token` failed action `login`
+
+Caused by:
+  please provide a non-empty token
+
+"##]])
+        .run();
+
+    p.cargo("publish --no-verify")
+        .replace_crates_io(registry.index_url())
+        .with_status(101)
+        .with_stderr_data(str![[r##"
+[UPDATING] crates.io index
+[ERROR] no token found, please run `cargo login`
+or use environment variable CARGO_REGISTRY_TOKEN
+
+"##]])
+        .run();
+}
