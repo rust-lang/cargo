dc0eefb706a159ff3b041db934b4ad53b24a295f
dawe <dawedawe@posteo.de>
2025-10-17 11:16:23 +0200

    fix(check): fix the suggested fix command by always using -p

--- tests/testsuite/check.rs
+++ tests/testsuite/check.rs
@@ -532,7 +532,7 @@ fn check_virtual_manifest_one_bin_project_not_in_default_members() {
         .build();
 
     p.cargo("check -p bar")
-        .with_stderr_contains("[..]run `cargo fix --bin \"bar\"` to apply[..]")
+        .with_stderr_contains("[..]run `cargo fix --bin \"bar\" -p bar` to apply[..]")
         .run();
 }
 
@@ -1421,7 +1421,7 @@ fn check_fixable_example() {
     p.cargo("check --all-targets")
         .with_stderr_data(str![[r#"
 ...
-[WARNING] `foo` (example "ex1") generated 1 warning (run `cargo fix --example "ex1"` to apply 1 suggestion)
+[WARNING] `foo` (example "ex1") generated 1 warning (run `cargo fix --example "ex1" -p foo` to apply 1 suggestion)
 ...
 "#]])
         .run();
@@ -1467,7 +1467,7 @@ fn check_fixable_bench() {
     p.cargo("check --all-targets")
         .with_stderr_data(str![[r#"
 ...
-[WARNING] `foo` (bench "bench") generated 1 warning (run `cargo fix --bench "bench"` to apply 1 suggestion)
+[WARNING] `foo` (bench "bench") generated 1 warning (run `cargo fix --bench "bench" -p foo` to apply 1 suggestion)
 ...
 "#]])
         .run();
@@ -1517,9 +1517,9 @@ fn check_fixable_mixed() {
         .build();
     p.cargo("check --all-targets")
         .with_stderr_data(str![[r#"
-[WARNING] `foo` (example "ex1") generated 1 warning (run `cargo fix --example "ex1"` to apply 1 suggestion)
-[WARNING] `foo` (bench "bench") generated 1 warning (run `cargo fix --bench "bench"` to apply 1 suggestion)
-[WARNING] `foo` (bin "foo" test) generated 2 warnings (run `cargo fix --bin "foo" --tests` to apply 2 suggestions)
+[WARNING] `foo` (example "ex1") generated 1 warning (run `cargo fix --example "ex1" -p foo` to apply 1 suggestion)
+[WARNING] `foo` (bench "bench") generated 1 warning (run `cargo fix --bench "bench" -p foo` to apply 1 suggestion)
+[WARNING] `foo` (bin "foo" test) generated 2 warnings (run `cargo fix --bin "foo" -p foo --tests` to apply 2 suggestions)
 ...
 "#]].unordered())
         .run();
