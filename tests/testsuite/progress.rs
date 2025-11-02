42933dba15b8867af0796d087f370fd367b032a5
Weihang Lo <me@weihanglo.tw>
2025-11-02 10:02:33 -0500

    test(term): `progress = "never"` is invalid
    This was claimed supported in
    https://github.com/rust-lang/cargo/pull/8165#issuecomment-693620188
    but actually it never is.
    
    Tested with 1.50 Cargo and it failed with the same reason.
    
    It never worked because our custom `Deserializer::deserialize_option`
    never called `visit_str`. See
    https://github.com/rust-lang/cargo/blob/c369b8c8d85a/src/cargo/util/config/de.rs#L135-L145

--- tests/testsuite/progress.rs
+++ tests/testsuite/progress.rs
@@ -156,3 +156,26 @@ fn never_progress() {
         .with_stderr_does_not_contain("[BUILDING] [..] [..]/4: [..]")
         .run();
 }
+
+#[cargo_test]
+fn plain_string_when_doesnt_work() {
+    let p = project()
+        .file(
+            ".cargo/config.toml",
+            r#"
+            [term]
+            progress = "never"
+            "#,
+        )
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check")
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+[ERROR] invalid configuration for key `term.progress`
+expected a table, but found a string for `term.progress` in [ROOT]/foo/.cargo/config.toml
+
+"#]])
+        .run();
+}
