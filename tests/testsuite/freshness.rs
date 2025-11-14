5fb4cb19bcf7d6f07fefe69d49b962fee8127ecb
Weihang Lo <me@weihanglo.tw>
2025-11-14 04:22:10 -0500

    fix(fingerprint): force update mtime of cargo-check artifacts
    This mtime shift for .rmeta is a workaround as rustc incremental build
    since rust-lang/rust 114669 (1.90.0) skips unnecessary rmeta generation.
    
    The situation is like this:
    
    1. When build script execution's external dependendies
       (rerun-if-changed, rerun-if-env-changed) got updated,
       the execution unit reran and got a newer mtime.
    2. rustc type-checked the associated crate, though with incremental
       compilation, no rmeta regeneration. Its `.rmeta` stays old.
    3. Run `cargo check` again. Cargo found build script execution had
       a new mtime than existing crate rmeta, so re-checking the crate.
       However the check is a no-op (input has no change), so stuck.

--- tests/testsuite/freshness.rs
+++ tests/testsuite/freshness.rs
@@ -3229,9 +3229,7 @@ fn incremental_build_script_execution_got_new_mtime_and_cargo_check() {
     p.cargo("check -v")
         .env("CARGO_INCREMENTAL", "1")
         .with_stderr_data(str![[r#"
-[DIRTY] foo v0.0.1 ([ROOT]/foo): the dependency build_script_build was rebuilt ([TIME_DIFF_AFTER_LAST_BUILD])
-[CHECKING] foo v0.0.1 ([ROOT]/foo)
-[RUNNING] `rustc --crate-name foo [..]`
+[FRESH] foo v0.0.1 ([ROOT]/foo)
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
 
 "#]])
@@ -3240,9 +3238,7 @@ fn incremental_build_script_execution_got_new_mtime_and_cargo_check() {
     p.cargo("check -v")
         .env("CARGO_INCREMENTAL", "1")
         .with_stderr_data(str![[r#"
-[DIRTY] foo v0.0.1 ([ROOT]/foo): the dependency build_script_build was rebuilt ([TIME_DIFF_AFTER_LAST_BUILD])
-[CHECKING] foo v0.0.1 ([ROOT]/foo)
-[RUNNING] `rustc --crate-name foo [..]`
+[FRESH] foo v0.0.1 ([ROOT]/foo)
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
 
 "#]])
