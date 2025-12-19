41c171bd35da5582abf9853d29498f7e6bcecad4
Ed Page <eopage@gmail.com>
2025-12-19 14:01:40 -0600

    refactor(compile): Move CARGO_BIN_EXE_ next to artifact deps
    Seeing as this is an implicit artifact
    
    At this new location,
    it will now run as part of `cargo rustdoc --test <name>`
    which technically fixes a bug
    but `--test` is not really supported (#13427)
    so I didn't mark this as a `fix` nor did I add tests.

--- src/cargo/core/compiler/mod.rs
+++ src/cargo/core/compiler/mod.rs
@@ -1478,32 +1478,6 @@ fn build_base_args(
             .env("RUSTC_BOOTSTRAP", "1");
     }
 
-    // Add `CARGO_BIN_EXE_` environment variables for building tests.
-    if unit.target.is_test() || unit.target.is_bench() {
-        for bin_target in unit
-            .pkg
-            .manifest()
-            .targets()
-            .iter()
-            .filter(|target| target.is_bin())
-        {
-            let name = bin_target
-                .binary_filename()
-                .unwrap_or_else(|| bin_target.name().to_string());
-
-            // For `cargo check` builds we do not uplift the CARGO_BIN_EXE_ artifacts to the
-            // artifact-dir. We do not want to provide a path to a non-existent binary but we still
-            // need to provide *something* so `env!("CARGO_BIN_EXE_...")` macros will compile.
-            let exe_path = build_runner
-                .files()
-                .bin_link_for_target(bin_target, unit.kind, build_runner.bcx)?
-                .map(|path| path.as_os_str().to_os_string())
-                .unwrap_or_else(|| OsString::from(format!("placeholder:{name}")));
-
-            let key = format!("CARGO_BIN_EXE_{name}");
-            cmd.env(&key, exe_path);
-        }
-    }
     Ok(())
 }
 
@@ -1805,6 +1779,33 @@ fn build_deps_args(
     }
     for arg in extern_args(build_runner, unit, &mut unstable_opts)? {
         cmd.arg(arg);
+    }
+
+    // Add `CARGO_BIN_EXE_` environment variables for building tests.
+    if unit.target.is_test() || unit.target.is_bench() {
+        for bin_target in unit
+            .pkg
+            .manifest()
+            .targets()
+            .iter()
+            .filter(|target| target.is_bin())
+        {
+            let name = bin_target
+                .binary_filename()
+                .unwrap_or_else(|| bin_target.name().to_string());
+
+            // For `cargo check` builds we do not uplift the CARGO_BIN_EXE_ artifacts to the
+            // artifact-dir. We do not want to provide a path to a non-existent binary but we still
+            // need to provide *something* so `env!("CARGO_BIN_EXE_...")` macros will compile.
+            let exe_path = build_runner
+                .files()
+                .bin_link_for_target(bin_target, unit.kind, build_runner.bcx)?
+                .map(|path| path.as_os_str().to_os_string())
+                .unwrap_or_else(|| OsString::from(format!("placeholder:{name}")));
+
+            let key = format!("CARGO_BIN_EXE_{name}");
+            cmd.env(&key, exe_path);
+        }
     }
 
     for (var, env) in artifact::get_env(build_runner, deps)? {
