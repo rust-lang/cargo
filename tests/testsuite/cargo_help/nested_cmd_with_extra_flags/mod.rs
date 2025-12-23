4d23857a06643855bd168a91d4a4bdb6edcc02a2
Weihang Lo <me@weihanglo.tw>
2025-12-23 18:52:16 -0500

    test(help): snapshot `cargo help` tests

---
+++ tests/testsuite/cargo_help/nested_cmd_with_extra_flags/mod.rs
@@ -0,0 +1,16 @@
+use crate::prelude::*;
+use cargo_test_support::file;
+
+#[cargo_test]
+fn case() {
+    snapbox::cmd::Command::cargo_ui()
+        .env("__CARGO_TEST_FORCE_HELP_TXT", "1")
+        .arg("help")
+        .arg("report")
+        .arg("future-incompatibilities")
+        .arg("--id")
+        .assert()
+        .code(1)
+        .stdout_eq(file!["stdout.term.txt"])
+        .stderr_eq(file!["stderr.term.svg"]);
+}
