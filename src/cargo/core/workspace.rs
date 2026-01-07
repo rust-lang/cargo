ed5bf3c9dcc674e41dbe9f811ace5919e54b2b6a
enthropy7 <221884178+enthropy7@users.noreply.github.com>
2026-01-07 20:27:16 +0300

    Implement text_direction_codepoint lint

--- src/cargo/core/workspace.rs
+++ src/cargo/core/workspace.rs
@@ -26,6 +26,7 @@ use crate::lints::rules::blanket_hint_mostly_unused;
 use crate::lints::rules::check_im_a_teapot;
 use crate::lints::rules::implicit_minimum_version_req;
 use crate::lints::rules::non_kebab_case_bin;
+use crate::lints::rules::text_direction_codepoint;
 use crate::ops;
 use crate::ops::lockfile::LOCKFILE_NAME;
 use crate::sources::{CRATES_IO_INDEX, CRATES_IO_REGISTRY, PathSource, SourceConfigMap};
@@ -1357,6 +1358,13 @@ impl<'gctx> Workspace<'gctx> {
             )?;
             non_kebab_case_bin(
                 self,
+                pkg,
+                &path,
+                &cargo_lints,
+                &mut run_error_count,
+                self.gctx,
+            )?;
+            text_direction_codepoint(
                 pkg.into(),
                 &path,
                 &cargo_lints,
@@ -1414,6 +1422,13 @@ impl<'gctx> Workspace<'gctx> {
             }
 
             implicit_minimum_version_req(
+                self.root_maybe().into(),
+                self.root_manifest(),
+                &cargo_lints,
+                &mut run_error_count,
+                self.gctx,
+            )?;
+            text_direction_codepoint(
                 self.root_maybe().into(),
                 self.root_manifest(),
                 &cargo_lints,
--- tests/testsuite/lints/text_direction_codepoint.rs
+++ tests/testsuite/lints/text_direction_codepoint.rs
@@ -3,8 +3,8 @@ use cargo_test_support::project;
 use cargo_test_support::str;
 
 #[cargo_test]
-fn bidi_in_description() {
-    // Manifest with a RIGHT-TO-LEFT OVERRIDE (U+202E) in the description
+fn bidi_in_description_allowed() {
+    // BiDi in description is allowed (legitimate RTL language use case)
     let manifest = "
 [package]
 name = \"foo\"
@@ -28,14 +28,121 @@ description = \"A \u{202E}test package\"
 }
 
 #[cargo_test]
-fn multiple_bidi() {
-    // Manifest with multiple BiDi codepoints
+fn bidi_in_comment_denied() {
+    // BiDi in comments is denied - can be used to hide malicious content
     let manifest = "
 [package]
 name = \"foo\"
 version = \"0.0.1\"
 edition = \"2015\"
-description = \"A \u{202E}test\u{202D} package\"
+# This is a \u{202E}tricky comment
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+[ERROR] unicode codepoint changing visible direction of text present in manifest
+ --> Cargo.toml:6:13
+  |
+6 | # This is a �tricky comment
+  |             ^ `/u{202E}` (RIGHT-TO-LEFT OVERRIDE)
+  |
+  = [NOTE] `cargo::text_direction_codepoint` is set to `deny` by default
+  = [NOTE] these kinds of unicode codepoints change the way text flows on screen, but can cause confusion because they change the order of characters
+  = [HELP] if their presence wasn't intentional, you can remove them, or use their escape sequence (e.g., /u{202E}) in double-quoted strings
+[ERROR] encountered 1 error while running lints
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn bidi_in_non_description_value_denied() {
+    // BiDi in regular values (not description/metadata) is denied
+    let manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+authors = [\"Test \u{202E}Author\"]
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+[ERROR] unicode codepoint changing visible direction of text present in manifest
+ --> Cargo.toml:6:18
+  |
+6 | authors = ["Test �Author"]
+  |                  ^ `/u{202E}` (RIGHT-TO-LEFT OVERRIDE)
+  |
+  = [NOTE] `cargo::text_direction_codepoint` is set to `deny` by default
+  = [NOTE] these kinds of unicode codepoints change the way text flows on screen, but can cause confusion because they change the order of characters
+  = [HELP] if their presence wasn't intentional, you can remove them, or use their escape sequence (e.g., /u{202E}) in double-quoted strings
+[ERROR] encountered 1 error while running lints
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn multiple_bidi_same_line() {
+    // Multiple BiDi codepoints on the same line in a denied location
+    let manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+authors = [\"A \u{202E}test\u{202D} author\"]
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+[ERROR] unicode codepoint changing visible direction of text present in manifest
+ --> Cargo.toml:6:15
+  |
+6 | authors = ["A �test� author"]
+  |               ^    ^ `/u{202D}` (LEFT-TO-RIGHT OVERRIDE)
+  |               |
+  |               `/u{202E}` (RIGHT-TO-LEFT OVERRIDE)
+  |
+  = [NOTE] `cargo::text_direction_codepoint` is set to `deny` by default
+  = [NOTE] these kinds of unicode codepoints change the way text flows on screen, but can cause confusion because they change the order of characters
+  = [HELP] if their presence wasn't intentional, you can remove them, or use their escape sequence (e.g., /u{202E}) in double-quoted strings
+[ERROR] encountered 1 error while running lints
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn allow_lint() {
+    // Test that the lint can be allowed even for denied locations
+    let manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+authors = [\"Test \u{202E}Author\"]
+
+[lints.cargo]
+text_direction_codepoint = \"allow\"
 ";
     let p = project()
         .file("Cargo.toml", manifest)
@@ -45,6 +152,43 @@ description = \"A \u{202E}test\u{202D} package\"
     p.cargo("check -Zcargo-lints")
         .masquerade_as_nightly_cargo(&["cargo-lints"])
         .with_stderr_data(str![[r#"
+[CHECKING] foo v0.0.1 ([ROOT]/foo)
+[FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn warn_lint() {
+    // Test that the lint can be set to warn
+    let manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+authors = [\"Test \u{202E}Author\"]
+
+[lints.cargo]
+text_direction_codepoint = \"warn\"
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_stderr_data(str![[r#"
+[WARNING] unicode codepoint changing visible direction of text present in manifest
+ --> Cargo.toml:6:18
+  |
+6 | authors = ["Test �Author"]
+  |                  ^ `/u{202E}` (RIGHT-TO-LEFT OVERRIDE)
+  |
+  = [NOTE] `cargo::text_direction_codepoint` is set to `warn` in `[lints]`
+  = [NOTE] these kinds of unicode codepoints change the way text flows on screen, but can cause confusion because they change the order of characters
+  = [HELP] if their presence wasn't intentional, you can remove them, or use their escape sequence (e.g., /u{202E}) in double-quoted strings
 [CHECKING] foo v0.0.1 ([ROOT]/foo)
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
 
@@ -79,38 +223,8 @@ edition = "2015"
 }
 
 #[cargo_test]
-fn workspace_member_bidi() {
-    // Workspace with BiDi in member package
-    let manifest = "
-[workspace]
-members = [\"foo\"]
-";
-    let foo_manifest = "
-[package]
-name = \"foo\"
-version = \"0.0.1\"
-edition = \"2015\"
-description = \"A \u{202E}test package\"
-";
-    let p = project()
-        .file("Cargo.toml", manifest)
-        .file("foo/Cargo.toml", foo_manifest)
-        .file("foo/src/lib.rs", "")
-        .build();
-
-    p.cargo("check -Zcargo-lints")
-        .masquerade_as_nightly_cargo(&["cargo-lints"])
-        .with_stderr_data(str![[r#"
-[CHECKING] foo v0.0.1 ([ROOT]/foo/foo)
-[FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
-
-"#]])
-        .run();
-}
-
-#[cargo_test]
-fn virtual_workspace_bidi() {
-    // Virtual workspace with BiDi in metadata
+fn workspace_metadata_allowed() {
+    // BiDi in workspace.metadata is allowed (legitimate use case)
     let manifest = "
 [workspace]
 members = [\"foo\"]
@@ -135,6 +249,105 @@ edition = "2015"
         .with_stderr_data(str![[r#"
 [CHECKING] foo v0.0.1 ([ROOT]/foo/foo)
 [FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn package_metadata_allowed() {
+    // BiDi in package.metadata values is allowed
+    let manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+
+[package.metadata]
+info = \"test \u{202E}info\"
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_stderr_data(str![[r#"
+[CHECKING] foo v0.0.1 ([ROOT]/foo)
+[FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn workspace_inherited_allow() {
+    // Workspace-level lint configuration with member package
+    let manifest = "
+[workspace]
+members = [\"foo\"]
+
+[workspace.lints.cargo]
+text_direction_codepoint = \"allow\"
+";
+    let foo_manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+authors = [\"Test \u{202E}Author\"]
+
+[lints]
+workspace = true
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("foo/Cargo.toml", foo_manifest)
+        .file("foo/src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_stderr_data(str![[r#"
+[CHECKING] foo v0.0.1 ([ROOT]/foo/foo)
+[FINISHED] `dev` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
+
+"#]])
+        .run();
+}
+
+#[cargo_test]
+fn bidi_in_key_denied() {
+    // BiDi in keys (inside quoted key) is denied - security concern
+    let manifest = "
+[package]
+name = \"foo\"
+version = \"0.0.1\"
+edition = \"2015\"
+
+[package.metadata]
+\"\u{202E}evil\" = \"value\"
+";
+    let p = project()
+        .file("Cargo.toml", manifest)
+        .file("src/lib.rs", "")
+        .build();
+
+    p.cargo("check -Zcargo-lints")
+        .masquerade_as_nightly_cargo(&["cargo-lints"])
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+[ERROR] unicode codepoint changing visible direction of text present in manifest
+ --> Cargo.toml:8:2
+  |
+8 | "�evil" = "value"
+  |  ^ `/u{202E}` (RIGHT-TO-LEFT OVERRIDE)
+  |
+  = [NOTE] `cargo::text_direction_codepoint` is set to `deny` by default
+  = [NOTE] these kinds of unicode codepoints change the way text flows on screen, but can cause confusion because they change the order of characters
+  = [HELP] if their presence wasn't intentional, you can remove them, or use their escape sequence (e.g., /u{202E}) in double-quoted strings
+[ERROR] encountered 1 error while running lints
 
 "#]])
         .run();
