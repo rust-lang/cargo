30ae8bcf4f56e94bb8b2ffd7ba653b213ee50b70
Ed Page <eopage@gmail.com>
2026-01-20 12:38:12 -0600

    test(rm): Show no-flag behavior

---
+++ tests/testsuite/cargo_remove/invalid_section_missing_flags/mod.rs
@@ -0,0 +1,29 @@
+use crate::prelude::*;
+use cargo_test_support::Project;
+use cargo_test_support::compare::assert_ui;
+use cargo_test_support::current_dir;
+use cargo_test_support::file;
+use cargo_test_support::str;
+
+#[cargo_test]
+fn case() {
+    cargo_test_support::registry::init();
+    cargo_test_support::registry::Package::new("semver", "0.1.1")
+        .feature("std", &[])
+        .publish();
+
+    let project = Project::from_template(current_dir!().join("in"));
+    let project_root = project.root();
+    let cwd = &project_root;
+
+    snapbox::cmd::Command::cargo_ui()
+        .arg("remove")
+        .args(["semver"])
+        .current_dir(cwd)
+        .assert()
+        .code(101)
+        .stdout_eq(str![""])
+        .stderr_eq(file!["stderr.term.svg"]);
+
+    assert_ui().subset_matches(current_dir!().join("out"), &project_root);
+}
