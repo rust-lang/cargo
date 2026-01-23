d8aeadea9cd20c2835318e244ac608203cfb1f54
Eric Huss <eric@huss.org>
2026-01-23 11:04:18 -0800

    Don't check the specific build-std output
    This particular test was failing occasionally because the order of the
    output is not deterministic. Sometimes `test` would run first, and
    sometimes `rustc-std-workspace-std` would run first. This is because
    they start in parallel, and it is a race which one prints first.
    
    The exact output here isn't particularly interesting. I would prefer
    to not check the particular structure of the standard library, so
    this just elides all the output.

--- tests/build-std/main.rs
+++ tests/build-std/main.rs
@@ -165,7 +165,6 @@ fn basic() {
         .build_std_isolated()
         .target_host()
         .with_stderr_data(str![[r#"
-[COMPILING] test v0.0.0 ([..])
 ...
 [COMPILING] foo v0.0.1 ([ROOT]/foo)
 [FINISHED] `test` profile [unoptimized + debuginfo] target(s) in [ELAPSED]s
