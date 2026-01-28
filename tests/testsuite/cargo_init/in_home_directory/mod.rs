baf35a0b2a28185f3ed2298cf4c3a7a4c6244326
Liam <liam@scalzulli.com>
2026-01-28 00:56:31 -0500

    test(init): Add test for init in home directory

---
+++ tests/testsuite/cargo_init/in_home_directory/mod.rs
@@ -0,0 +1,15 @@
+use crate::prelude::*;
+use cargo_test_support::file;
+use cargo_test_support::paths;
+use cargo_test_support::str;
+
+#[cargo_test]
+fn case() {
+    snapbox::cmd::Command::cargo_ui()
+        .arg_line("init --vcs none")
+        .current_dir(paths::home())
+        .assert()
+        .success()
+        .stdout_eq(str![""])
+        .stderr_eq(file!["stderr.term.svg"]);
+}
