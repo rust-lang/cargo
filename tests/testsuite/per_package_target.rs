fe8ec37dd8e2d503a07924bad55bacf2b3f29f5a
Raushan Kumar <raushansinghrajpoot687@gmail.com>
2026-02-09 11:05:44 +0000

    Fix cross-compilation target in regression test

--- tests/testsuite/per_package_target.rs
+++ tests/testsuite/per_package_target.rs
@@ -4,8 +4,37 @@ use crate::prelude::*;
 use cargo_test_support::project;
 use cargo_test_support::registry::Package;
 
+use cargo_test_support::cross_compile;
+use std::process::Command;
+
 #[cargo_test]
 fn forced_target_with_artifact_dep() {
+    let target = cross_compile::alternate();
+
+    // Check if target is installed by trying to compile a dummy file.
+    let mut child = Command::new("rustc")
+        .arg("--target")
+        .arg(target)
+        .arg("-")
+        .arg("--crate-type=bin")
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::null())
+        .stderr(std::process::Stdio::null())
+        .spawn()
+        .unwrap();
+
+    use std::io::Write;
+    child
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(b"fn main() {}")
+        .unwrap();
+
+    if !child.wait().unwrap().success() {
+        return;
+    }
+
     Package::new("bar", "1.0.0")
         .file("src/lib.rs", "pub fn bar() {}")
         .publish();
@@ -13,21 +42,25 @@ fn forced_target_with_artifact_dep() {
     let p = project()
         .file(
             "Cargo.toml",
-            r#"
-            cargo-features = ["per-package-target"]
-            [package]
-            name = "foo"
-            version = "0.1.0"
-            forced-target = "x86_64-unknown-linux-gnu"
+            &format!(
+                r#"
+                cargo-features = ["per-package-target"]
+                [package]
+                name = "foo"
+                version = "0.1.0"
+                forced-target = "{target}"
 
-            [build-dependencies]
-            bar = { version = "1.0", artifact = "bin", target = "target" }
-            "#,
+                [build-dependencies]
+                bar = {{ version = "1.0", artifact = "bin", target = "target" }}
+                "#,
+                target = target
+            ),
         )
         .file("src/main.rs", "fn main() {}")
         .build();
 
     p.cargo("build -Z bindeps")
         .masquerade_as_nightly_cargo(&["per-package-target"])
+        .env("RUSTC_BOOTSTRAP", "1")
         .run();
 }
