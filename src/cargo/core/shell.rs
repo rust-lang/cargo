87eebbbf8c448022a1843575ec31f09409d32726
Ed Page <eopage@gmail.com>
2026-03-13 10:57:16 -0500

    refactor(shell): Pull out hyperlink logic into anstyle-hyperlink
    After seeing
    - annotate-snippets needing a `Hyperlink`
    - people directly editing Cargo for ansi escape code progress detection
    
    I figured it would be good to pull this general logic out.

--- src/cargo/core/shell.rs
+++ src/cargo/core/shell.rs
@@ -8,8 +8,9 @@ use anstream::AutoStream;
 use anstyle::Style;
 
 use crate::util::errors::CargoResult;
-use crate::util::hostname;
 use crate::util::style::*;
+
+pub use anstyle_hyperlink::Hyperlink;
 
 /// An abstraction around console output that remembers preferences for output
 /// verbosity and color.
@@ -22,7 +23,6 @@ pub struct Shell {
     /// Flag that indicates the current line needs to be cleared before
     /// printing. Used when a progress bar is currently displayed.
     needs_clear: bool,
-    hostname: Option<String>,
 }
 
 impl fmt::Debug for Shell {
@@ -62,7 +62,6 @@ impl Shell {
             },
             verbosity: Verbosity::Verbose,
             needs_clear: false,
-            hostname: None,
         }
     }
 
@@ -72,7 +71,6 @@ impl Shell {
             output: ShellOut::Write(AutoStream::never(out)), // strip all formatting on write
             verbosity: Verbosity::Verbose,
             needs_clear: false,
-            hostname: None,
         }
     }
 
@@ -334,8 +332,10 @@ impl Shell {
                 stdout, hyperlinks, ..
             } => stdout.current_choice() == anstream::ColorChoice::AlwaysAnsi && *hyperlinks,
         };
-        Hyperlink {
-            url: supports_hyperlinks.then_some(url),
+        if supports_hyperlinks {
+            Hyperlink::with_url(url)
+        } else {
+            Hyperlink::default()
         }
     }
 
@@ -347,39 +347,20 @@ impl Shell {
             } => stderr.current_choice() == anstream::ColorChoice::AlwaysAnsi && *hyperlinks,
         };
         if supports_hyperlinks {
-            Hyperlink { url: Some(url) }
+            Hyperlink::with_url(url)
         } else {
-            Hyperlink { url: None }
+            Hyperlink::default()
         }
     }
 
-    pub fn out_file_hyperlink(&mut self, path: &std::path::Path) -> Hyperlink<url::Url> {
-        let url = self.file_hyperlink(path);
+    pub fn out_file_hyperlink(&mut self, path: &std::path::Path) -> Hyperlink<String> {
+        let url = anstyle_hyperlink::path_to_url(path);
         url.map(|u| self.out_hyperlink(u)).unwrap_or_default()
     }
 
-    pub fn err_file_hyperlink(&mut self, path: &std::path::Path) -> Hyperlink<url::Url> {
-        let url = self.file_hyperlink(path);
+    pub fn err_file_hyperlink(&mut self, path: &std::path::Path) -> Hyperlink<String> {
+        let url = anstyle_hyperlink::path_to_url(path);
         url.map(|u| self.err_hyperlink(u)).unwrap_or_default()
-    }
-
-    fn file_hyperlink(&mut self, path: &std::path::Path) -> Option<url::Url> {
-        let mut url = url::Url::from_file_path(path).ok()?;
-        // Do a best-effort of setting the host in the URL to avoid issues with opening a link
-        // scoped to the computer you've SSH'ed into
-        let hostname = if cfg!(windows) {
-            // Not supported correctly on windows
-            None
-        } else {
-            if let Some(hostname) = self.hostname.as_deref() {
-                Some(hostname)
-            } else {
-                self.hostname = hostname().ok().and_then(|h| h.into_string().ok());
-                self.hostname.as_deref()
-            }
-        };
-        let _ = url.set_host(hostname);
-        Some(url)
     }
 
     fn unstable_flags_rustc_unicode(&self) -> bool {
@@ -689,29 +670,6 @@ mod tests {
 
         // Without PROGRESS feature
         assert!(!term_features_has_progress("MBT2Sc"));
-    }
-}
-
-pub struct Hyperlink<D: fmt::Display> {
-    url: Option<D>,
-}
-
-impl<D: fmt::Display> Default for Hyperlink<D> {
-    fn default() -> Self {
-        Self { url: None }
-    }
-}
-
-impl<D: fmt::Display> fmt::Display for Hyperlink<D> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Some(url) = self.url.as_ref() else {
-            return Ok(());
-        };
-        if f.alternate() {
-            write!(f, "\x1B]8;;\x1B\\")
-        } else {
-            write!(f, "\x1B]8;;{url}\x1B\\")
-        }
     }
 }
 
