229246220dc1cf7fc447fdf11fb1f2c82f4e22f2
Raushan Kumar <raushansinghrajpoot687@gmail.com>
2026-03-27 13:45:17 +0000

    fix(ops): improve warning for --broken-code when fixes were applied

--- tests/testsuite/fix.rs
+++ tests/testsuite/fix.rs
@@ -1428,15 +1428,14 @@ fn fix_to_broken_code() {
     p.cargo("fix --allow-no-vcs --broken-code")
         .cwd("bar")
         .env("RUSTC", p.root().join("foo/target/debug/foo"))
-        .with_stderr_data(str![[r#"
+        .with_stderr_data(        str![[r#"
 [COMPILING] bar v0.1.0 ([ROOT]/foo/bar)
 [ERROR] errors present after applying fixes to crate `bar`
   |
   = cause: thread 'main' ([..]) panicked at src/main.rs:23:29:
            explicit panic
            [NOTE] run with `RUST_BACKTRACE=1` environment variable to display a backtrace
-[HELP] to report this as a bug, open an issue at https://github.com/rust-lang/rust/issues, quoting the full output of this command
-[HELP] to possibly apply more fixes, pass in the `--broken-code` flag
+[WARNING] fixes were applied but the code still does not compile; partially-fixed code was saved due to `--broken-code`
 [NOTE] original diagnostics will follow:
 ...
 
