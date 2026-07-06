38e1e62341beea85565bb8fc8f951089a15c2ac9
Bryanskiy <ivakin.kir@gmail.com>
2026-07-06 16:47:08 +0300

    test(build-std): test for duplicate lang items with abort panic strategy

--- tests/build-std/main.rs
+++ tests/build-std/main.rs
@@ -505,6 +505,32 @@ fn default_features_still_included_with_extra_build_std_features() {
         .run();
 }
 
+#[cargo_test(build_std_real)]
+fn duplicate_lang_item_with_panic_abort() {
+    // This is a regression test to ensure that rustc doesn't load `panic_abort`
+    // from the sysroot. See rust-lang/cargo#7359
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [package]
+                name = "foo"
+                edition = "2021"
+
+                [profile.dev]
+                panic = 'abort'
+            "#,
+        )
+        .file("src/main.rs", "fn main() {}")
+        .build();
+
+    p.cargo("check")
+        .build_std_arg("std")
+        .with_stderr_contains("error[E0152]: duplicate lang item in crate `core`: `sized`")
+        .with_status(101)
+        .run();
+}
+
 pub trait CargoProjectExt {
     /// Creates a `ProcessBuilder` to run cargo.
     ///
