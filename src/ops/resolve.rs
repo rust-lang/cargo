0d9e6b592a7e1f2c1a3fb3fc6c9cc1b305ab170f
Andrew Liu <24590316+aliu@users.noreply.github.com>
2026-09-13 22:32:09 +0900

    fix: de-duplicate no-lib bindeps warnings

--- src/ops/resolve.rs
+++ src/ops/resolve.rs
@@ -329,21 +329,21 @@ pub fn resolve_ws_with_opts<'gctx>(
             feature_opts,
         )?;
 
-        pkg_set.warn_no_lib_packages_and_artifact_libs_overlapping_deps(
-            ws,
-            &resolved_with_overrides,
-            &member_ids,
-            has_dev_units,
-            requested_targets,
-            target_data,
-            force_all_targets,
-        )?;
-
         specs_and_features.push(SpecsAndResolvedFeatures {
             specs,
             resolved_features,
         });
     }
+
+    pkg_set.warn_no_lib_packages_and_artifact_libs_overlapping_deps(
+        ws,
+        &resolved_with_overrides,
+        &member_ids,
+        has_dev_units,
+        requested_targets,
+        target_data,
+        force_all_targets,
+    )?;
 
     Ok(WorkspaceResolve {
         pkg_set,
