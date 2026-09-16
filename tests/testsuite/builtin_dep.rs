f87af572d97d2b2d7a2812d2c8c5476141bc6989
Adam Gemmell <adam.gemmell@arm.com>
2026-09-16 16:35:59 +0100

    test(builtin-deps): Builtin dep with version specifier should be rejected

--- tests/testsuite/builtin_dep.rs
+++ tests/testsuite/builtin_dep.rs
@@ -334,3 +334,35 @@ Caused by:
             .run();
     }
 }
+
+#[cargo_test]
+fn builtin_combined_with_version_specifier() {
+    let p = project()
+        .file("src/lib.rs", "")
+        .file(
+            "Cargo.toml",
+            r#"
+                cargo-features = ["builtin-dependencies"]
+
+                [package]
+                name = "foo"
+                version = "0.1.0"
+                [dependencies]
+
+                core = { builtin = true, version = "0.0.0" }
+                "#,
+        )
+        .build();
+
+    p.cargo("check")
+        .masquerade_as_nightly_cargo(&["builtin-dependencies"])
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+
+thread [..] panicked at [..]
+not yet implemented: SourceKind::Builtin
+[NOTE] run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+
+"#]])
+        .run();
+}
