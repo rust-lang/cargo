55db40092db8746ad7b86245befd530b733e7def
Adam Gemmell <adam.gemmell@arm.com>
2026-09-21 15:11:19 +0100

    test(builtin-deps): Add regression test for inherited build dependency

--- tests/testsuite/builtin_dep.rs
+++ tests/testsuite/builtin_dep.rs
@@ -467,3 +467,39 @@ Caused by:
 "#]])
         .run();
 }
+
+#[cargo_test]
+fn inherited_build_dependencies() {
+    let p = project()
+        .file("src/lib.rs", "")
+        .file(
+            "Cargo.toml",
+            r#"
+                cargo-features = ["builtin-dependencies"]
+
+                [package]
+                name = "foo"
+                version = "0.1.0"
+                edition = "2021"
+
+                [workspace.dependencies]
+                core = { builtin = true }
+
+                [build-dependencies]
+                core.workspace = true
+            "#,
+        )
+        .build();
+
+    p.cargo("check")
+        .masquerade_as_nightly_cargo(&["builtin-dependencies"])
+        .with_status(101)
+        .with_stderr_data(str![[r#"
+[ERROR] failed to parse manifest at `[ROOT]/foo/Cargo.toml`
+
+Caused by:
+  builtin dependency `core` cannot be used as a build dependency
+
+"#]])
+        .run();
+}
